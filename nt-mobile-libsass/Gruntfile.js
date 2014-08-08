745d8865aa83bbd33f123312a16883fcb10036f4
Ray Hatfield <ray.hatfield@nextthought.com>
2014-08-07 23:06:40 -0500

    setting up again, using foundation with libsass instead of compass.

---
+++ nt-mobile-libsass/Gruntfile.js
@@ -0,0 +1,151 @@
+'use strict';
+
+var mountFolder = function(connect, dir) {
+	return connect.static(require('path').resolve(dir));
+};
+
+var webpackDistConfig = require('./webpack.dist.config.js'),
+		webpackDevConfig = require('./webpack.config.js');
+
+module.exports = function(grunt) {
+
+	// Let *load-grunt-tasks* require everything
+	require('load-grunt-tasks')(grunt);
+
+	var pkgConfig = grunt.file.readJSON('package.json');
+
+	grunt.initConfig({
+
+		pkg: pkgConfig,
+
+		webpack: {
+			options: webpackDistConfig,
+
+			dist: {
+				cache: false
+			}
+		},
+
+		'webpack-dev-server': {
+			options: {
+				port: 9000,
+				webpack: webpackDevConfig,
+				publicPath: '/',
+				contentBase: './<%= pkg.src %>/'
+			},
+
+			start: {
+				keepAlive: true
+			}
+		},
+
+		connect: {
+			options: {
+				port: 9000
+			},
+
+			dist: {
+				options: {
+					keepalive: true,
+					middleware: function(connect) {
+						return [
+						mountFolder(connect, pkgConfig.dist)
+						];
+					}
+				}
+			}
+		},
+
+		open: {
+			options: {
+				delay: 500
+			},
+			dev: {
+				path: 'http://localhost:<%= connect.options.port %>/webpack-dev-server/'
+			},
+			dist: {
+				path: 'http://localhost:<%= connect.options.port %>/'
+			}
+		},
+
+		copy: {
+			dist: {
+				files: [
+				// includes files within path
+					{
+						flatten: true,
+						expand: true,
+						src: ['<%= pkgConfig.src %>/*'],
+						dest: '<%= pkgConfig.dist %>/',
+						filter: 'isFile'
+					},
+					{
+						flatten: true,
+						expand: true,
+						src: ['<%= pkgConfig.src %>/images/*'],
+						dest: '<%= pkgConfig.dist %>/images/'
+					}
+				]
+			}
+		},
+
+		clean: {
+			dist: {
+				files: [{
+					dot: true,
+					src: [
+					'<%= pkgConfig.dist %>'
+					]
+				}]
+			}
+		},
+
+		sass: {
+			options: {
+				includePaths: ['src/main/resources/vendor/foundation/scss']
+			},
+			dist: {
+				options: {
+					outputStyle: 'compressed'
+				},
+				files: {
+					'src/main/resources/css/app.css': 'src/main/resources/scss/app.scss'
+				}
+			}
+		},
+
+		watch: {
+			grunt: { files: ['Gruntfile.js'] },
+
+			sass: {
+				files: 'scss/**/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('build', ['sass']);
+	grunt.registerTask('default', ['build', 'watch']);
+
+	grunt.registerTask('serve', function(target) {
+		if (target === 'dist') {
+			return grunt.task.run(['build', 'open:dist', 'connect:dist']);
+		}
+
+		grunt.task.run([
+			'sass',
+			'open:dev',
+			'webpack-dev-server'
+			]);
+	});
+
+	grunt.registerTask('test', ['karma']);
+
+	grunt.registerTask('build', ['clean', 'sass', 'copy', 'webpack']);
+
+	grunt.registerTask('default', []);
+
+};
--- nt-mobile/config.rb
+++ nt-mobile/config.rb
@@ -3,13 +3,16 @@ add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "scss"
-images_dir = "images"
-javascripts_dir = "js"
+css_dir = "src/main/resources/stylesheets"
+sass_dir = "src/main/resources/scss"
+images_dir = "src/main/resources/images"
+javascripts_dir = "src/main/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+
+sass_options = {:sourcemap => true,}
+enable_sourcemaps = true
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
