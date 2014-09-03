84e24d6eed18dd1cc879d0a239346c67c5a90c02
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-09-03 13:29:14 -0500

    add a new handler in the dispatcher

--- src/main/js/common/dispatcher/AppDispatcher.js
+++ src/main/js/common/dispatcher/AppDispatcher.js
@@ -17,18 +17,32 @@ var copyProperties = require('react/lib/copyProperties');
 
 var AppDispatcher = copyProperties(new Dispatcher(), {
 
-  /**
-   * A bridge function between the views and the dispatcher, marking the action
-   * as a view action.  Another variant here could be handleServerAction.
-   * @param  {object} action The data coming from the view.
-   */
-  handleViewAction: function(action) {
-    console.log('AppDispatcher::handleViewAction: %s', action.actionType);
-    this.dispatch({
-      source: 'VIEW_ACTION',
-      action: action
-    });
-  }
+    /**
+     * A bridge function between the views and the dispatcher, marking the action
+     * as a view action.
+     * @param  {object} action The data coming from the view.
+     */
+    handleViewAction: function(action) {
+        console.log('AppDispatcher::handleViewAction: %s', action.actionType);
+        this.dispatch({
+            source: 'VIEW_ACTION',
+            action: action
+        });
+    },
+
+
+    /**
+     * A bridge function between the views and the dispatcher, marking the action
+     * as a request action.
+     * @param  {object} action The data coming from the view.
+     */
+    handleRequestAction: function(action) {
+        console.log('AppDispatcher::handleRequestAction: %s', action.actionType);
+        this.dispatch({
+            source: 'REQUEST_ACTION',
+            action: action
+        });
+    }
 
 });
 
