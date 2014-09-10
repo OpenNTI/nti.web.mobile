5c49c2fd23e0fd1dd388ba644efeaa6865f003dd
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-09-10 18:13:28 -0500

    fix potential glitch

--- src/main/js/common/locale/index.js
+++ src/main/js/common/locale/index.js
@@ -2,7 +2,7 @@ var counterpart = require('counterpart');
 counterpart.registerTranslations('en',require('./en.js'));
 
 function translate(key,options) {
-	return counterpart(key,options);
+	return counterpart.apply(this, arguments);
 }
 
 module.exports = translate;
