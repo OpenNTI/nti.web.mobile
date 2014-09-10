286a9d2ff731789d0c5f36a65c4c69b130efdd18
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-09-09 19:11:49 -0500

    restore routes that got lost.

--- src/main/js/navigation/components/Router.jsx
+++ src/main/js/navigation/components/Router.jsx
@@ -5,10 +5,11 @@ var React = require('react/addons');
 var Router = require('react-router-component');
 
 //Main Views
-var LoginView = require('../../login').LoginView;
-var HomeView = require('../../home').View;
-var LibraryView = require('../../library').View;
-var NotFoundView = require('../../notfound').View;
+var Login = require('../../login');
+var Home = require('../../home');
+var Library = require('../../library');
+var Course = require('../../course');
+var NotFound = require('../../notfound');
 
 module.exports = React.createClass({
 
@@ -17,10 +18,12 @@ module.exports = React.createClass({
 		console.log('basePath: %s', basePath);
 		return (
 			<Router.Locations path={this.props.path}>
-				<Router.Location path={basePath + 'login/*'} handler={LoginView} basePath={basePath}/>
-				<Router.Location path={basePath + 'library/*'} handler={LibraryView} basePath={basePath} />
-				<Router.Location path={basePath} handler={HomeView} />
-				<Router.NotFound handler={NotFoundView} />
+				<Router.Location path={basePath + 'login/*'} handler={Login.LoginView} basePath={basePath}/>
+				<Router.Location path={basePath + 'library/*'} handler={Library.View} basePath={basePath} />
+				<Router.Location path={basePath + 'catalog(/:entry)(/*)'} handler={Home.View} basePath={basePath} />
+				<Router.Location path={basePath + 'course(/:course)(/*)'} handler={Course.View} basePath={basePath} />
+				<Router.Location path={basePath} handler={Home.View} />
+				<Router.NotFound handler={NotFound.View} />
 			</Router.Locations>
 		);
 	}
