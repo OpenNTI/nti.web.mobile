c54b477a418d4ea33035e5484c3cf11f9c4c178d
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-09-11 18:08:20 -0500

    make the Unknown type more helpfull.

--- src/main/js/notifications/components/kinds/Unknown.jsx
+++ src/main/js/notifications/components/kinds/Unknown.jsx
@@ -5,11 +5,17 @@ var React = require('react/addons');
 var NoteableMixin = require('../mixins/Noteable');
 
 module.exports = React.createClass({
+	displayName: 'UnknownNotificationType',
 	mixins: [NoteableMixin],
 
 	render: function() {
+		var change = this.props.item;
+		var item = change.Item || change;
+		var type = item.MimeType.replace('application/vnd.nextthought.', '');
+		console.debug('Unhandled Notification Kind: %o', change);
+
 		return (
-			<li className="notification-item">Unknown</li>
+			<li className="notification-item">Unknown {type}</li>
 		);
 	}
 });
