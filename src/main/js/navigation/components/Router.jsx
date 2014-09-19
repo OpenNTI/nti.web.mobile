0dd83c396dcf4636d86c80d5e6ec145954a2d2ba
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-09-18 20:17:44 -0500

    reset scroll pos on nav.

--- src/main/js/navigation/components/Router.jsx
+++ src/main/js/navigation/components/Router.jsx
@@ -15,11 +15,24 @@ var NotFound = require('../../notfound');
 module.exports = React.createClass({
 	displayName: 'Router',
 
+
+	onNavigation: function() {
+		if (global.scrollTo) {
+			global.scrollTo(0,0);
+		}
+
+		var action = this.props.onNavigation;
+		if (action) {
+			action();
+		}
+	},
+
+
 	render: function() {
 		var basePath = this.props.basePath;
 		console.log('basePath: %s', basePath);
 		return (
-			<Router.Locations path={this.props.path} onNavigation={this.props.onNavigation}>
+			<Router.Locations path={this.props.path} onNavigation={this.onNavigation}>
 				<Router.Location path={basePath + 'login(/*)'} handler={Login.LoginView} basePath={basePath}/>
 				<Router.Location path={basePath + 'library(/*)'} handler={Library.View} basePath={basePath} />
 				<Router.Location path={basePath + 'catalog(/*)'} handler={Catalog.View} basePath={basePath} />
