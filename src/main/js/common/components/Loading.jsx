2c3ba1d2deae5a6f6882581f9c90cdc81757ad11
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-09-24 16:55:52 -0500

    Add a handy trick the to loading component.

--- src/main/js/common/components/Loading.jsx
+++ src/main/js/common/components/Loading.jsx
@@ -2,10 +2,16 @@
 'use strict';
 
 var React = require('react/addons');
+var isEmpty = require('dataserverinterface/utils/isempty');
 
 module.exports = React.createClass({
 	displayName: 'Loading',
 	render: function() {
+
+		if (!isEmpty(this.props.children) && !this.props.loading) {
+			return React.DOM.div(null, this.props.children);
+		}
+
 		return (
 			<figure className="loading">
 				<div className="m spinner"></div>
