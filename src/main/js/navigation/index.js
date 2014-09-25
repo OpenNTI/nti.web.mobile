5ddb699cb4755960f2c1fd19f1eab81d6a5ba8d6
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-09-25 11:24:54 -0500

    expose the router as a prop of the package.

--- src/main/js/navigation/index.js
+++ src/main/js/navigation/index.js
@@ -6,6 +6,7 @@ var store = require('./Store');
 var navrecord = require('./NavRecord');
 
 module.exports = {
+	Router: require('./components/Router'),
 	Actions: actions,
 	Constants: constants,
 	Store: store,
