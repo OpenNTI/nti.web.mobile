c53cf7c32cee6d4588aafdd54765ba706834befc
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-09-27 12:26:12 -0500

    Begin the content "Overlay" equiv for the mobile app... React-Style

---
+++ src/main/js/content/components/widgets/Unknown.jsx
@@ -0,0 +1,28 @@
+/** @jsx React.DOM */
+'use strict';
+
+var React = require('react/addons');
+
+module.exports = React.createClass({
+	displayName: 'ContentWidgetUnknown',
+
+	componentDidMount: function() {
+		console.log('Component Mounted');
+	},
+
+	componentWillUnmount: function() {
+		console.log('Component Unmounting...');
+	},
+
+
+	render: function() {
+		var type = this.props.data['attribute-type'];
+		return (
+			<div onClick={this._onClick}>Unknown Type: {type}</div>
+		);
+	},
+
+	_onClick: function () {
+		this._owner.setState({foobar: true})
+	},
+});
--- src/main/js/content/components/widgets/index.js
+++ src/main/js/content/components/widgets/index.js
@@ -1,1 +1,31 @@
 'use strict';
+
+var merge = require('react/lib/merge');
+
+var Unknown = require('./Unknown');
+
+exports = module.exports = {
+	Unknown: Unknown, //Unknown for future items.
+
+	select: function getItemHandler(part) {
+		var Item = exports.Unknown;
+		var key, Type;
+
+		for (key in exports) {
+			if (exports.hasOwnProperty(key)) {
+				Type = exports[key];
+				if (Type !== Unknown && Type.handles && Type.handles(part)) {
+					Item = Type;
+					break;
+				}
+			}
+		}
+
+		return Item(
+			{
+				key: 'widget-' + part.guid,
+				data: part
+			});
+	}
+
+};
