59da2ac7caef8000cbc18cf811573c11fc641e07
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-09-27 12:27:27 -0500

    first iteration of using body "parts" and react in-line components instead of "Overlaying"

--- src/main/js/content/components/View.jsx
+++ src/main/js/content/components/View.jsx
@@ -15,21 +15,55 @@ module.exports = React.createClass({
 
 	getInitialState: function() {
 		return {
-			loading: true
+			loading: true,
+			pageWidgets: {}
 		};
 	},
 
 
 	componentDidMount: function() {
+		//The getDOMNode() will always be the loading dom at his point...
+		//we wait for the re-render of the actual data in componentDidUpdate()
 		Store.addChangeListener(this._onChange);
 		this.getDataIfNeeded(this.props);
 	},
 
 
 	componentWillUnmount: function() {
+		console.debug('Content View: Unmounting...');
 		Store.removeChangeListener(this._onChange);
+		//Cleanup our components...
+		var guid, el, w,
+			widgets = this.getPageWidgets();
+
+		for(guid in widgets) {
+			el = document.getElementById(guid);
+			React.unmountComponentAtNode(el);
+			el.removeAttribute('mounted');
+		}
 	},
 
+
+	componentDidUpdate: function () {
+		//See if we need to re-mount/render our components...
+		var guid, el, w,
+			widgets = this.getPageWidgets();
+		console.debug('Content View: Did Update... %o', widgets);
+
+		for(guid in widgets) {
+			el = document.getElementById(guid);
+			w = widgets[guid];
+			if (el && !el.hasAttribute('mounted')) {
+				console.debug('Content View: Mounting Widget...');
+				w = React.renderComponent(w, el);
+				el.setAttribute('mounted', 'true');
+			}
+		}
+	},
+
+	componentWillUpdate: function () {
+		//Do bookkeeping for "out-of-flow" components...
+	},
 
 	componentWillReceiveProps: function(props) {
 		if (this.props.pageId !== props.pageId) {
@@ -40,30 +74,69 @@ module.exports = React.createClass({
 
 	getDataIfNeeded: function(props) {
 		this.setState(this.getInitialState());
-		Actions.loadPage(decodeURIComponent(props.pageId));
+		Actions.loadPage(this.getPageID());
+	},
+
+
+	getPageID: function () {
+		return decodeURIComponent(this.props.pageId);
+	},
+
+
+	getPageWidgets: function() {
+		var o = this.state.pageWidgets;
+		var id = this.getPageID();
+		if (!o[id]) {
+			console.debug('Content View: Creating bin for PageWidgets for %s', id);
+			o[id] = {};
+		}
+		return o[id];
+	},
+
+
+	maybeCreateWidget: function(widgetData) {
+		var widgets = this.getPageWidgets();
+		if (!widgets[widgetData.guid]) {
+			console.debug('Content View: Creating widget for %s', widgetData.guid);
+			widgets[widgetData.guid] = Widgets.select(widgetData);
+		}
 	},
 
 
 	_onChange: function() {
-		var id = decodeURIComponent(this.props.pageId);
-		var data = Store.getPageData(id);
+		var id = this.getPageID();
+		var data = Store.getPageData(this.getPageID());
 
 		this.setState({
 			data: data,
-			content: data.content,
+			body: data.body,
 			styles: data.styles
 		});
 	},
 
 
 	render: function() {
-		var content = this.state.content;
+		var body = this.state.body || [];
 		return (
 			<div className="content-view">
 				{this._applyStyle()}
-				<div id="NTIContent" dangerouslySetInnerHTML={{__html: content}}/>
+				<div id="NTIContent" dangerouslySetInnerHTML={{
+					__html: body.map(this._buildBody).join('')
+				}}/>
 			</div>
 		);
+	},
+
+
+	_buildBody: function(part) {
+
+		if (typeof part === 'string') {
+			return part;
+		}
+
+		this.maybeCreateWidget(part);
+
+		return '<div id="'+ part.guid +'">Component Placeholder</div>';
 	},
 
 
