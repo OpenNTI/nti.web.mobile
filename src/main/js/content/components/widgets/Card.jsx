a75b6bda6d29e6dca13e62d11a1fdd0a8ef77950
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-09-27 16:46:20 -0500

    new content widget "card"

---
+++ src/main/js/content/components/widgets/Card.jsx
@@ -0,0 +1,31 @@
+/** @jsx React.DOM */
+'use strict';
+
+var Card = require('common/components/Card');
+
+module.exports = React.createClass({
+	displayName: 'NTICard',
+
+	statics: {
+		mimeTest: /^application\/vnd\.nextthought\.relatedworkref/i,
+		handles: function(item) {
+			debugger;
+			return this.mimeTest.test(item.MimeType);
+		}
+	},
+
+
+	render: function() {
+
+		var props = this.props;
+		var basePath = path.join(
+			props.basePath,
+			'course', //encodeURIComponent(props.course.getID()),
+			'o', props.outlineId
+		)
+
+		return this.transferPropsTo(<Card
+			basePath={basePath} pathname="c"
+			package={this.props.course}/>);
+	}
+});
