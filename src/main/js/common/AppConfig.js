e38f085070b98afb1a8198d76d66bfd138fcd7c3
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-09-30 20:54:55 -0500

    do not reference $AppConfig at define time.

--- src/main/js/common/AppConfig.js
+++ src/main/js/common/AppConfig.js
@@ -1,17 +1,31 @@
 'use strict';
 
+
 /**
 * Application configuration/settings
 * @class AppConfig
 */
-module.exports = {
+module.exports = exports = {
 	/**
 	* Where to go upon successful login.
 	* @property login_success_url
 	* @static
 	* @final
 	*/
-	login_success_url: '/loginsuccess',
-	basePath: $AppConfig.basepath
+	login_success_url: '/loginsuccess'
 };
 
+/**
+ * We cannot access $AppConfig at define time, because it is undefined on the
+ * server side. So, it must be hidden in a function call.
+ */
+Object.defineProperty(exports, 'basePath', {
+
+	// enumerable: true,
+	// writable: false,
+	// configurable: false,
+	set: function() {},
+	get: function() {
+		return $AppConfig.basepath;
+	}
+});
