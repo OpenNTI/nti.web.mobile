95314b0588f546d5136c902cec74e80478b84e21
Ray Hatfield <ray.hatfield@nextthought.com>
2014-10-07 18:18:20 -0500

    initial work on filtering courses (current, upcoming, archived)

--- src/main/js/library/components/Collection.jsx
+++ src/main/js/library/components/Collection.jsx
@@ -8,13 +8,48 @@ var Package = require('./Package');
 var Bundle = require('./Bundle');
 var Course = require('./Course');
 
+
 module.exports = React.createClass({
 	displayName: 'Collection',
 	mixins: [OwnerQuery],
 
 	propTypes: {
 		title: React.PropTypes.string.isRequired,
-		list: React.PropTypes.array.isRequired
+		list: React.PropTypes.array.isRequired,
+		filters: React.PropTypes.object
+	},
+
+	getInitialState: function() {
+		return {
+			activeFilter:null
+		};
+	},
+
+	_onFilterClick: function(filterName) {
+		this.setState({
+			activeFilter: filterName
+		});
+	},
+
+	componentWillMount: function() {
+		if (this.props.filters && Object.keys(this.props.filters).length > 0) {
+			this.setState({activeFilter: Object.keys(this.props.filters)[0]});
+		}
+	},
+
+	filterBar: function() {
+		var filterItems = Object.keys(this.props.filters).map(function(key,index,array) {
+			var isActive = this.state.activeFilter === key;
+			return <dd className={isActive ? 'active' : null}><a href="#" onClick={this._onFilterClick.bind(this,key)}>{key}</a></dd>
+		}.bind(this));
+
+		var filterBar = filterItems.length === 0 ? null : (
+			<dl className="sub-nav">
+				{filterItems}
+			</dl>
+		);
+
+		return filterBar;
 	},
 
 	render: function() {
@@ -22,22 +57,31 @@ module.exports = React.createClass({
 		var basePath = this.props.basePath;
 
 		var size = this.getStateFromParent('orientation') === 'landscape' ? 2 : 1;
+		var fbar = this.filterBar();
+
+		if (this.state.activeFilter) {
+			list = list.filter(this.props.filters[this.state.activeFilter]);
+		}
 
 		return (
-		<div className="grid-container">
-			<h2>{this.props.title}</h2>
-			<ul className={'small-block-grid-' + size + ' medium-block-grid-3 large-block-grid-4'}>
-			{list.map(function(o) {
-				var Item = o.isBundle ?
-					Bundle :
-					o.isCourse ?
-						Course :
-						Package;
+			<div>
+				{fbar}
+				<h2>{this.props.title}</h2>
+				<div className="grid-container">
+					<ul className={'small-block-grid-' + size + ' medium-block-grid-3 large-block-grid-4'}>
+					{list.map(function(o) {
+						var Item = o.isBundle ?
+							Bundle :
+							o.isCourse ?
+								Course :
+								Package;
 
-				return <Item key={o.NTIID} item={o} basePath={basePath}/>;
-			})}
-			</ul>
-		</div>
+						return <Item key={o.NTIID} item={o} basePath={basePath}/>;
+					})}
+					</ul>
+				</div>
+			</div>
+			
 		);
 	}
 });
