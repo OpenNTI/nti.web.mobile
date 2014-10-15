e492f2df18f6e0fddc4a31cff44fa2a3d6d8dbd8
Ray Hatfield <ray.hatfield@nextthought.com>
2014-10-15 16:19:48 -0500

    trello #25 ( https://trello.com/c/4FlWPKBR ) : Guard against being handed invalid data; try/catch/log in filter methods, return false.

--- src/main/js/library/CourseFilters.js
+++ src/main/js/library/CourseFilters.js
@@ -2,20 +2,38 @@
 
 var CourseFilters = {
 	'Current': function(item,index,array) {
-		var startDate = new Date(item.CourseInstance.CatalogEntry.StartDate);
-		var endDate = new Date(item.CourseInstance.CatalogEntry.EndDate);
-		var now = new Date();
-		return startDate < now && endDate > now;
+		try {
+			var startDate = new Date(item.CourseInstance.CatalogEntry.StartDate);
+			var endDate = new Date(item.CourseInstance.CatalogEntry.EndDate);
+			var now = new Date();
+			return startDate < now && endDate > now;
+		}
+		catch(e) {
+			console.error(e);
+			return false;
+		}
 	},
 	'Upcoming': function(item,index,array) {
-		var startDate = new Date(item.CourseInstance.CatalogEntry.StartDate);
-		var now = new Date();
-		return startDate > now;
+		try {
+			var startDate = new Date(item.CourseInstance.CatalogEntry.StartDate);
+			var now = new Date();
+			return startDate > now;
+		}
+		catch(e) {
+			console.error(e);
+			return false;
+		}
 	},
 	'Archived': function(item,index,array) {
-		var endDate = new Date(item.CourseInstance.CatalogEntry.EndDate);
-		var now = new Date();
-		return endDate < now;
+		try {
+			var endDate = new Date(item.CourseInstance.CatalogEntry.EndDate);
+			var now = new Date();
+			return endDate < now;	
+		}
+		catch(e) {
+			console.error(e);
+			return false;
+		}
 	}
 };
 
