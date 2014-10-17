aea343c9e0b951ae4848cbee8b75ac5e9310bab0
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-10-16 20:41:10 -0500

    lint

--- src/main/js/common/components/video-services/iframe/index.jsx
+++ src/main/js/common/components/video-services/iframe/index.jsx
@@ -2,14 +2,14 @@
 'use strict';
 
 var React = require('react/addons');
-var Error = require('common/components/Error');
+var ErrorWidget = require('common/components/Error');
 
 module.exports = React.createClass({
 	displayName: 'IFrame-Video',
 
 	render: function() {
 		if (!this.props.src) {
-			return (<Error error="No source"/>)
+			return (<ErrorWidget error="No source"/>);
 		}
 		return this.transferPropsTo(<iframe frameBorder="0" seemless allowfullscreen webkitallowfullscreen mozAllowFullScreen />);
 	}
