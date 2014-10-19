2cd182190174e7041a42ab2ad2d2d29f5a994b3c
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-10-19 15:13:29 -0500

    fill in discussion data on overviews

--- src/main/js/course/components/widgets/Discussion.jsx
+++ src/main/js/course/components/widgets/Discussion.jsx
@@ -2,6 +2,7 @@
 'use strict';
 
 var React = require('react/addons');
+var getService = require('common/Utils').getService;
 
 module.exports = React.createClass({
 	displayName: 'CourseOverviewDiscussion',
@@ -20,12 +21,15 @@ module.exports = React.createClass({
 	getInitialState: function(){
 		var i = this.props.item;
 		return {
-			icon: null
+			count: 0,
+			icon: null,
+			title: ''
 		};
 	},
 
 	componentDidMount: function() {
 		this.resolveIcon(this.props);
+		this.resolveCommentCount();
 	},
 
 	resolveIcon: function(props) {
@@ -43,17 +47,37 @@ module.exports = React.createClass({
 			}.bind(this));
 	},
 
+
+	resolveCommentCount: function() {
+		var item = this.props.item;
+		var id = item && item.NTIID;
+
+		getService().then(function(service){
+			return service.getObject(id)
+				.then(function(o) {
+					if (this.isMounted()) {
+						this.setState({
+							title: o.title,
+							count: o.PostCount || 0
+						});
+					}
+				}.bind(this));
+		}.bind(this));
+	},
+
+
 	render: function() {
 		var props = this.props;
 		var item = props.item;
-		var title = item.title||'Discussion';
-		var numCommments = "[Number of]";
+		var title = item.title || this.state.title || 'Discussion';
+
+
 		return (
 			<div className={'overview-discussion'}>
 				<img src={this.state.icon}></img>
-				<div className={'wrap'}>
-					<div className='title'>{title}</div>
-					<div className='comments'>{"[Number of] Comments"}</div>
+				<div className="wrap">
+					<div className="title">{title}</div>
+					<div className="comments">{this.state.count + " Comments"}</div>
 				</div>
 			</div>
 		);
