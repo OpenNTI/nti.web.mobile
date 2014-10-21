d5b96a5d1f707dc45f2bfa1b6f329bf85a6746fa
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-10-20 20:23:49 -0500

    cleanup

--- src/server/api.js
+++ src/server/api.js
@@ -6,18 +6,22 @@ var merge = require('merge');
 var request = require('dataserverinterface/utils/request');
 var autoBind = require('dataserverinterface/utils/autobind');
 
+function throwError(msg) {
+	throw new Error(msg);
+}
+
+
 var api = module.exports = autoBind({
 
 	register: function(express, config) {
 		merge(this, config);
-		console.log(config);
 		express.get(/^\/api\/user-agreement/, api.serveUserAgreement);
 	},
 
 
 	serveUserAgreement: function(req, res) {
 		var BODY_REGEX = /<body[^>]*>(.*)<\/body/i;
-		var url = this['user-agreement'] || 'http://nextthought.com';
+		var url = this['user-agreement'] || throwError('No user-agreement url set');
 
 		request(url, function(error, r, response) {
 			var body = BODY_REGEX.exec(response);
