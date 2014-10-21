170e0912cf581aff06674c99b4f68fd68ece79e7
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-10-20 19:43:16 -0500

    don't do this stuff if we're not going to use it.

--- src/main/js/home/components/View.jsx
+++ src/main/js/home/components/View.jsx
@@ -3,49 +3,13 @@
 
 var React = require('react/addons');
 
-var getService = require('common/Utils').getService;
-
-var Loading = require('common/components/Loading');
-var DisplayName = require('common/components/DisplayName');
-var Avatar = require('common/components/Avatar');
 var Redirect = require('common/components/Redirect');
 
 
 module.exports = React.createClass({
 	displayName: 'HomeView',
 
-	getInitialState: function() {
-		return {
-			appUser: null
-		};
-	},
-
-
-	componentDidMount: function() {
-		this._resolve();
-	},
-
-
-	componentWillReceiveProps: function(nextProps) {
-		this._resolve();
-	},
-
-
-	_resolve: function() {
-		if (!this.state.appUser) {
-			getService()
-				.then(function(service){return service.getAppUser();})
-				.then(function(user) {
-					this.setState({appUser: user});
-				}.bind(this));
-		}
-	},
-
-
 	render: function() {
-		if (!this.state.appUser) {
-			return (<Loading/>);
-		}
 		return (<Redirect location='courseware/' />);
 	}
 });
