60f2e60bd4089a51ad90110461d8fe32989d078f
Ray Hatfield <ray.hatfield@nextthought.com>
2014-10-21 22:49:52 -0500

    Cosmetic tweaks to the OU 4+4 signup confirmation view.

--- src/main/js/login/components/SignupConfirm.jsx
+++ src/main/js/login/components/SignupConfirm.jsx
@@ -12,12 +12,14 @@ var SignupConfirm = React.createClass({
 
 	render: function() {
 		return (
-			<div className="medium-6 medium-centered columns">
+			<div>
 				<div className="notice">
 					If you are a current student at the University of Oklahoma, you don't need to create an account. <Link href="/">Log in with your OUNet ID (4+4)</Link>
 				</div>
-				<Button href="/signup/">Create Account <i className="fi-arrow-right" /></Button>
-				<Button href="/" className="fi-arrow-left"> Return to Login</Button>
+				<div className="medium-6 medium-centered columns">
+					<Button href="/signup/">Create Account <i className="fi-arrow-right" /></Button>
+					<Button href="/" className="fi-arrow-left"> Return to Login</Button>
+				</div>
 			</div>
 		);
 	}
