755a370022fedcfb6452cc5b517fa5dec80c2357
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-10-24 22:25:40 -0500

    wire up vimeo & events

--- src/main/js/common/components/video-services/vimeo/index.jsx
+++ src/main/js/common/components/video-services/vimeo/index.jsx
@@ -21,13 +21,52 @@ var Source = module.exports = React.createClass({
 		}
 	},
 
+	propTypes: {
+		id: React.PropTypes.string,
+		source: React.PropTypes.any.isRequired
+	},
+
+
+
+	getInitialState: function() {
+		return {
+			playerURL: this.buildURL()
+		};
+	},
+
+
+
+	getDefaultProps: function() {
+		return {
+			id: guid()
+		};
+	},
+
+
+	componentDidMount: function() {
+		this.updateURL();
+		window.addEventListener('message', this.onMessage, false);
+	},
+
+
+	componentWillUnmount: function() {
+		window.removeEventListener('message', this.onMessage, false);
+	},
+
+
+
+	componentWillReceiveProps: function() {
+		this.updateURL();
+	},
+
+
+	buildURL: function () {
 		var mediaSource = this.props.source;
-		var videoId = mediaSource.source[0];
-		var playerId = '';
+		var videoId = typeof mediaSource === 'string' ? Source.getId(mediaSource) : mediaSource.source[0];
 
 		var args = {
 			api: 1,
-			player_id: playerId,
+			player_id: this.props.id,
 			//autopause: 0, //we handle this for other videos, but its nice we only have to do this for cross-provider videos.
 			autoplay: 0,
 			badge: 0,
@@ -37,10 +76,72 @@ var Source = module.exports = React.createClass({
 			title: 0
 		};
 
-		var src = '//player.vimeo.com/video/' + videoId + '?' + asQueryString(args);
+		return location.protocol + '//player.vimeo.com/video/' + videoId + '?' + asQueryString(args);
+	},
 
 
-		return this.transferPropsTo(<iframe frameBorder="0" src={src}
-			seemless allowFullScreen allowTransparency />);
+	updateURL: function() {
+		var url = this.buildURL();
+		this.setState({
+			scope: url.split('?')[0],
+			playerURL: url
+		});
+	},
+
+
+
+	getPlayerContext: function() {
+		var iframe = this.getDOMNode();
+		return iframe && (iframe.contentWindow || window.frames[iframe.name]);
+	},
+
+
+	onMessage: function(event) {
+		var data = JSON.parse(event.data);
+
+		event = data.event;
+
+		if (data.player_id !== this.props.id) {
+			return;
+		}
+
+		console.debug('Vimeo Event: %s: %o', event, data);
+
+		if (event === 'ready') {
+			this.postMessage('addEventListener', 'playProgress');
+			this.postMessage('addEventListener', 'play');
+			this.postMessage('addEventListener', 'pause');
+			this.postMessage('addEventListener', 'finish');
+			this.postMessage('addEventListener', 'seek');
+		}
+	},
+
+
+	postMessage: function(method, params) {
+		var context = this.getPlayerContext(), data;
+		if (!context) {
+			console.warn(this.props.id, ' No Player Context!');
+			return;
+		}
+
+		data = {
+			method: method,
+			value: params
+		};
+
+		//console.log('%s Posting message to vimeo player %o', this.props.id, data);
+
+		context.postMessage(JSON.stringify(data), this.state.scope);
+	},
+
+
+	render: function() {
+		if (!this.state.playerURL) {
+			return (<ErrorWidget error="No source"/>);
+		}
+
+		return this.transferPropsTo(
+			<iframe src={this.state.playerURL} frameBorder="0" seemless allowFullScreen allowTransparency />
+		);
 	}
 });
