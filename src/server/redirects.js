586db08fba5b66a738cd9579328435b3a451b273
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-10-26 20:59:59 -0500

    only redirect webapp rewrites

--- src/server/redirects.js
+++ src/server/redirects.js
@@ -2,7 +2,7 @@
 
 module.exports = function(_, res, next) {
 	var url = _.originalUrl || _.url;
-	var index = (url && url.indexOf('?')) || 0;
+	var index = (url && url.indexOf('?q=')) || 0;
 
 	if (index > 0) {
 		res.redirect(url.substr(0, index) + '#notifications');
