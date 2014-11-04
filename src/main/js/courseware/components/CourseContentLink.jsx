f148d88fb1cab020ab0fb8b3b6d8eba0f87e7470
Ray Hatfield <ray.hatfield@nextthought.com>
2014-11-04 15:33:59 -0600

    remove NTIID.encodeForURI call as it (sometimes) resulted in double-escaped urls.

--- src/main/js/courseware/components/CourseContentLink.jsx
+++ src/main/js/courseware/components/CourseContentLink.jsx
@@ -6,7 +6,6 @@
 
 var React = require('react/addons');
 var Utils = require('common/Utils');
-var NTIID = require('dataserverinterface/utils/ntiids');
 
 var CourseContentLink = React.createClass({
 
@@ -16,10 +15,8 @@ var CourseContentLink = React.createClass({
 
 	render: function() {
 
-		var courseUrl = NTIID.encodeForURI(this.props.courseId);
-		
 		return (
-			<a href={Utils.getBasePath() + 'course/' + courseUrl + '/#nav'}>{this.props.children}</a>
+			<a href={Utils.getBasePath() + 'course/' + this.props.courseId + '/#nav'}>{this.props.children}</a>
 		);
 	}
 
