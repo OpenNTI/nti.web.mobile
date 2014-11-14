6fc628a7f0571b01ead38da5ccc8aaf8dd66057c
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-11-14 13:29:55 -0600

    OSU workarounds

--- src/main/js/login/Store.js
+++ src/main/js/login/Store.js
@@ -61,7 +61,7 @@ var LoginStore = merge(EventEmitter.prototype, {
 			{
 				ref: 'username',
 				type: 'text',
-				placeholder: 'Username or OU 4+4'
+				placeholder: 'Username'
 			},
 			{
 				ref: 'password',
