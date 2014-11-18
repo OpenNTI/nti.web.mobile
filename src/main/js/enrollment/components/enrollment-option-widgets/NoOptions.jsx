66ff89efe44eca2ba6070b245bf020b5490b2e78
Ray Hatfield <ray.hatfield@nextthought.com>
2014-11-18 17:12:40 -0600

    use locale strings for 'okay' button.

--- src/main/js/enrollment/components/enrollment-option-widgets/NoOptions.jsx
+++ src/main/js/enrollment/components/enrollment-option-widgets/NoOptions.jsx
@@ -6,12 +6,13 @@
 
 var React = require('react/addons');
 var PanelButton = require('common/components/PanelButton');
+var t = require('common/locale').translate;
 
 var NoOptions = React.createClass({
 
 	render: function() {
 		return (
-			<PanelButton href="../"className="column" buttonText='Okay'>
+			<PanelButton href="../" linkText={t('BUTTONS.OK')}>
 				This course is not currently available for enrollment.
 			</PanelButton>
 		);
