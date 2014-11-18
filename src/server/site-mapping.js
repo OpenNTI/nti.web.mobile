4c7b8be77847575e1fc589cf3d4defc3dc58a899
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-11-18 17:37:16 -0600

    typo

--- src/server/site-mapping.js
+++ src/server/site-mapping.js
@@ -1,14 +1,14 @@
 'use strict';
 
 var map = {
-	'ou-alpha.nextthouht.com': 'platform.ou.edu',
-	'ou-test.nextthouht.com': 'platform.ou.edu',
+	'ou-alpha.nextthought.com': 'platform.ou.edu',
+	'ou-test.nextthought.com': 'platform.ou.edu',
 	'janux.ou.edu': 'platform.ou.edu'
 };
 
 
 
 module.exports = function getSite(site) {
-	
+
 	return map[site] || site;
 };
