9ae5fce40fab644fab0cda948cca3fdc05ac9378
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-11-19 15:48:00 -0600

    do not re-inject scripts.

--- src/main/js/common/mixins/ScriptInjectorMixin.js
+++ src/main/js/common/mixins/ScriptInjectorMixin.js
@@ -1,36 +1,41 @@
 'use strict';
 
+var injected = {};
 
 module.exports = {
 
 	injectScript: function(scriptUrl, shouldDefineSymbole) {
 
-		return new Promise(function(fullfill,reject) {
+		if (injected[shouldDefineSymbole]) {
+			injected[shouldDefineSymbole] = new Promise(function(fullfill,reject) {
 
-			var script = document.createElement('script');
+				var script = document.createElement('script');
 
-            script.async = true;//Do not block the UI thread while loading.
-            script.defer = true;//legacy version of async
-            script.charset = 'utf-8'; //Be explicit
-			script.type = 'text/javascript'; //Be explicit
-			script.src = scriptUrl;
+	            script.async = true;//Do not block the UI thread while loading.
+	            script.defer = true;//legacy version of async
+	            script.charset = 'utf-8'; //Be explicit
+				script.type = 'text/javascript'; //Be explicit
+				script.src = scriptUrl;
 
-			//Some browsers may not fire an error... so we mush check in the 'load' event
-			//for an expected symbol to be defined.
-			script.onerror = reject;
+				//Some browsers may not fire an error... so we mush check in the 'load' event
+				//for an expected symbol to be defined.
+				script.onerror = reject;
 
-			script.onload = function() {
+				script.onload = function() {
 
-				if (shouldDefineSymbole && !global[shouldDefineSymbole]) {
-					return reject('Loaded, but expected interface was not found: "%s"', shouldDefineSymbole);
-				}
+					if (shouldDefineSymbole && !global[shouldDefineSymbole]) {
+						return reject('Loaded, but expected interface was not found: "%s"', shouldDefineSymbole);
+					}
 
-				fullfill(script);
-			};
+					fullfill(script);
+				};
 
-			//don't inject the element until all handlers are registered. If the src is cached,
-			//the handlers may fire before they're registered.
-			document.body.appendChild(script);
-		});
+				//don't inject the element until all handlers are registered. If the src is cached,
+				//the handlers may fire before they're registered.
+				document.body.appendChild(script);
+			});
+		}
+
+		return injected[shouldDefineSymbole];
 	}
 };
