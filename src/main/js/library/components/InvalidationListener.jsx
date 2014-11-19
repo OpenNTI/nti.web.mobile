762b2df82804f529f608ee4a97d155ea39897fb2
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-11-19 14:33:50 -0600

    inspect the event and only flush catalog/library if it matches my criteria.

--- src/main/js/library/components/InvalidationListener.jsx
+++ src/main/js/library/components/InvalidationListener.jsx
@@ -1,6 +1,4 @@
-/**
- * @jsx React.DOM
- */
+/** @jsx React.DOM */
 
 'use strict';
 
@@ -10,7 +8,29 @@ var StoreEnrollmentStore = require('enrollment/store-enrollment').Store;
 var Actions = require('../Actions');
 var CatalogActions = require('../catalog/Actions');
 
+
+var INVALIDATION_EVENTS = {
+	DROP_COURSE: true,
+	ENROLL_5M: true,
+	ENROLL_OPEN: true,
+	ENROLL_STORE: true
+};
+
+
+
 function _flush(event) {
+	if (!event || !event.action) {
+		console.debug('InvalidationListener: ignoring unknown event: %o', event);
+		return;
+	}
+
+	var act = INVALIDATION_EVENTS[event.action.type];
+
+	if (!act) {
+		console.debug('InvalidationListener: ignoring non-invalidation event: %o', event);
+		return;
+	}
+
 	console.log('InvalidationListener: reloading library in response to event: %O', event);
 	Actions.reload();
 	CatalogActions.reload();
