89c1198645faaa4d20b5fb1d030ec35941932dbd
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-11-21 12:39:38 -0600

    make it not render P or H2 if they're empty. Fix the missed classname for unstyled.

--- src/main/js/common/components/forms/FormPanel.jsx
+++ src/main/js/common/components/forms/FormPanel.jsx
@@ -14,15 +14,19 @@ var FormPanel = React.createClass({
 	getDefaultProps: function() {
 		return {
 			noValidate: true,
-			styled: true
+			styled: true,
+			subhead: null,
+			title: null
 		};
 	},
 
 	render: function() {
 		var cssClasses = [];
 		var formClasses = '';
+		var headingClasses = '';
 
 		if (this.props.styled) {
+			headingClasses = 'column';
 			formClasses = 'medium-6 medium-centered columns';
 			cssClasses = ['row'];
 		}
@@ -35,9 +39,9 @@ var FormPanel = React.createClass({
 		return (
 			<div className={cssClasses.join(' ')}>
 				<form className={formClasses} onSubmit={this.props.onSubmit} noValidate={this.props.noValidate}>
-					<div className="column">
-						<h2>{this.props.title}</h2>
-						<p>{this.props.subhead}</p>
+					<div className={headingClasses}>
+						{this.props.title && (<h2>{this.props.title}</h2>)}
+						{this.props.subhead && (<p>{this.props.subhead}</p>)}
 					</div>
 					{this.props.children}
 				</form>
