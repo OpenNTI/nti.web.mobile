5f78bab183b2d73c9afbcb5851405440794685c9
Ray Hatfield <ray.hatfield@nextthought.com>
2014-11-21 18:24:14 -0600

    Display a specific error message if available on payment rejection (e.g. 'Your card was declined')

--- src/main/js/enrollment/store-enrollment/components/PaymentError.jsx
+++ src/main/js/enrollment/store-enrollment/components/PaymentError.jsx
@@ -7,16 +7,37 @@
 var React = require('react/addons');
 var PanelButton = require('common/components/PanelButton');
 var NavigatableMixin = require('common/mixins/NavigatableMixin');
+var Store = require('../Store');
 
 var PaymentError = React.createClass({
 
 	mixins: [NavigatableMixin],
 
+	getInitialState: function() {
+		return {};
+	},
+
+	componentWillMount: function() {
+		var error = (Store.getPaymentResult()||{}).Error;
+		var message = (error||{}).Message;
+		if (message) {
+			this.setState({
+				message: message
+			});
+		}
+	},
+
+	errorMessage: function() {
+		return this.state.message ? <p>{this.state.message}</p> : null;
+	},
+
 	render: function() {
+
 		return (
 			<div className="small-12 columns">
 				<PanelButton className="error" href='../../../'>
 					<p>We were unable to process your enrollment for '{this.props.courseTitle}'.</p>
+					{this.errorMessage()}
 					<p>Please try again. If this issue persists contact support.</p>
 				</PanelButton>
 			</div>
