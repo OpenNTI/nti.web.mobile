6554d12ef16f21eb993ee159e347d16c8f1def09
Ray Hatfield <ray.hatfield@nextthought.com>
2014-11-23 14:59:02 -0600

    payment error okay button invokes Actions.resetProcess instead of linking to '../../../'. (Which previously ended up linking to janux.ou.edu from the history channel iframe widget.)

--- src/main/js/enrollment/store-enrollment/components/PaymentError.jsx
+++ src/main/js/enrollment/store-enrollment/components/PaymentError.jsx
@@ -8,6 +8,7 @@ var React = require('react/addons');
 var PanelButton = require('common/components/PanelButton');
 var NavigatableMixin = require('common/mixins/NavigatableMixin');
 var Store = require('../Store');
+var Actions = require('../Actions');
 
 var PaymentError = React.createClass({
 
@@ -31,13 +32,17 @@ var PaymentError = React.createClass({
 		return this.state.message ? <p>{this.state.message}</p> : null;
 	},
 
+	_buttonClick: function() {
+		Actions.resetProcess();
+	},
+
 	render: function() {
 
 		var courseTitle = this.props.courseTitle;
 
 		return (
 			<div className="small-12 columns">
-				<PanelButton className="error" href='../../../'>
+				<PanelButton className="error" buttonClick={this._buttonClick}>
 					<p>We were unable to process your enrollment for '{courseTitle}'.</p>
 					{this.errorMessage()}
 					<p>Please try again. If this issue persists contact support.</p>
