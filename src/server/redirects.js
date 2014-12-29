b5dee4fecf78ebb41a967f104df39cfabf8213e9
Ray Hatfield <ray.hatfield@nextthought.com>
2014-12-29 17:37:13 -0600

    Add redirection support for for-credit enrollment links.
    (trello #192: https://trello.com/c/I9IQlCv9)

--- src/server/redirects.js
+++ src/server/redirects.js
@@ -10,11 +10,42 @@ module.exports = autoBind({
 		express.use(route, this.handleRedirects);
 	},
 
+	translateCatalogId: function(input) {
+		var catalogId = input
+					.replace(/-/g, '+')
+					.replace(/_/g, '/');
+
+		catalogId = new Buffer(catalogId, 'base64').toString();
+		catalogId = catalogId.replace(/^!@/,'');//strip off the WebApp's 'salt'
+		catalogId = NTIIDs.encodeForURI(catalogId);
+
+		return catalogId;
+	},
+
+	translateTrailingPath: function(trailingPath) {
+
+		var segments = (trailingPath || '').split('/');
+		var translated = '';
+
+		switch(segments[0]) {
+			case 'redeem':
+				translated = path.join('enrollment','store','gift', segments.slice(0,2).join('/'));
+				break;
+
+			case 'forcredit':
+				translated = path.join('enrollment','credit','/');
+				break;
+		}
+
+		return translated;
+	},
+
 	handleRedirects: function(_, res, next) {
+
 		var url = _.originalUrl || _.url;
 		var index = (url && url.indexOf('?q=')) || 0;
 		var catalog = /library\/availablecourses\/([^\/]*)\/?(.*)/;
-		var redUrl, encoded, redeem;
+		var redUrl, parts, trailingPath;
 
 		/* From:
 		 * ?q=library/availablecourses/IUB0YWc6bmV4dHRob3VnaHQuY29tLDIwMTEtMTA6TlRJLUNvdXJzZUluZm8tU3ByaW5nMjAxNV9MU1REXzExNTM/redeem/code
@@ -23,30 +54,19 @@ module.exports = autoBind({
 		 * <basepath>/library/catalog/item/NTI-CourseInfo-Spring2015_LSTD_1153/enrollment/store/gift/redeem/code
 		 */
 		if (index > 0) {
+			// default to root view with notification drawer open:
+			// '/mobile/#notifications'
 			redUrl = url.substr(0, index) + '#notifications';
 
-			catalog = url.match(catalog);
+			// [full match, catalog item id (encoded), trailing path]
+			parts = url.match(catalog);
 
-			if (catalog) {
-				encoded = catalog[1]
-							.replace(/-/g, '+')
-							.replace(/_/g, '/');
-
-				redeem = (catalog[2] || '').split('/');
-				if (!/^redeem$/i.test(redeem[0])) {
-					redeem = '';
-				} else {
-					redeem = path.join('enrollment','store','gift', redeem.slice(0,2).join('/'));
-				}
-
-				catalog = new Buffer(encoded, 'base64').toString();
-				catalog = catalog.replace(/^!@/,'');//strip off the WebApps 'salt'
-				catalog = NTIIDs.encodeForURI(catalog);
-
-				redUrl = path.join(this.basepath, 'library', 'catalog', 'item', catalog) + '/' + redeem;
+			if (parts) {
+				var catalogId = this.translateCatalogId(parts[1]);
+				trailingPath = this.translateTrailingPath(parts[2]);
+				redUrl = path.join(this.basepath, 'library', 'catalog', 'item', catalogId, trailingPath);
 			}
-
-
+			console.log('\n\nredUrl: ', redUrl, '\n\n');
 			res.redirect(redUrl);
 			return;
 		}
