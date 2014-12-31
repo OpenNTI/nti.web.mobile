53be73db92b0492416860220f0564787ae5c5c0d
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-12-31 11:08:48 -0600

    add OkState site mappings in the node service

--- src/server/site-mapping.js
+++ src/server/site-mapping.js
@@ -3,7 +3,10 @@
 var map = {
 	'ou-alpha.nextthought.com': 'platform.ou.edu',
 	'ou-test.nextthought.com': 'platform.ou.edu',
-	'janux.ou.edu': 'platform.ou.edu'
+	'janux.ou.edu': 'platform.ou.edu',
+	'okstate-alpha.nextthought.com': 'okstate.nextthought.com',
+	'okstate-test.nextthought.com': 'okstate.nextthought.com',
+	'learnonline.okstate.edu': 'okstate.nextthought.com'
 };
 
 
