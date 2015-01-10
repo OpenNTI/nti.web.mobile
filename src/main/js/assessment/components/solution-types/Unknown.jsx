4c3ecc0f6dc6f1d022fb96c21a766391b61e9a79
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-01-09 18:38:50 -0600

    Style Unknown widget and Free Response.

--- src/main/js/assessment/components/solution-types/Unknown.jsx
+++ src/main/js/assessment/components/solution-types/Unknown.jsx
@@ -6,10 +6,13 @@ module.exports = React.createClass({
 	displayName: 'Unknown',
 
 	render: function() {
-		console.debug(this.props.item);
+		console.debug('Input Type Missing: %s', this.props.item.MimeType);
 		return (
-			<div>
-				{this.props.item.MimeType}
+			<div className="unknown solution">
+
+				<h4>This solution type is currently not available on mobile.<br/>
+				Please use the iPad app or your desktop browser.</h4>
+
 			</div>
 		);
 	}
