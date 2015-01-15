487228daed04cfe9266baae1bac4b36691a594a6
Ray Hatfield <ray.hatfield@nextthought.com>
2015-01-14 22:33:03 -0600

    finding the right forum bin now looks for the forum id instead of the board id; the boards (Parent and Section) are the same under each bin. fixes trello 295.

--- src/main/js/forums/components/FindBin.jsx
+++ src/main/js/forums/components/FindBin.jsx
@@ -10,8 +10,17 @@ var NavigatableMixin = require('common/mixins/NavigatableMixin');
 var NotFoundView = require('notfound/components/NotFoundView');
 var NTIID = require('dataserverinterface/utils/ntiids');
 
-function binHasBoard(bin, boardId) {
-	return Object.keys(bin).some(board => (bin[board].id === boardId));
+
+function binHasForum(bin, forumId) {
+	// keys in bin: Parent, Section
+	return Object.keys(bin).some(boardName => {
+		
+		// Parent or Section with a 'forums' array property
+		var b = bin[boardName];
+
+		// does one of the forums have a matching id?
+		return (b.forums||[]).some(forum => (forum.getID() === forumId));
+	});
 }
 
 var FindBin = React.createClass({
@@ -25,7 +34,7 @@ var FindBin = React.createClass({
 	},
 
 	componentWillMount: function() {
-		var bin = this.findBin(this.props.boardId);
+		var bin = this.findBin(this.props.forumId);
 		if (bin) {
 			var p = this.getPath();
 			var newPath = p.replace(/^\/jump/,'/'+bin);
@@ -35,13 +44,13 @@ var FindBin = React.createClass({
 		}
 	},
 
-	findBin: function(boardId) {
-		boardId = NTIID.decodeFromURI(boardId);
+	findBin: function(forumId) {
+		forumId = NTIID.decodeFromURI(forumId);
 		var d = this.props.discussions;
 		var result = null;
 		Object.keys(d||{}).some(binName => {
 			var bin = d[binName];
-			if(binHasBoard(bin,boardId)) {
+			if(binHasForum(bin,forumId)) {
 				result = binName;
 				return true;
 			}
