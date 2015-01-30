9f96d83a7f1c3b49d6f64d18d5781e0649657629
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-01-29 19:26:54 -0600

    analytics video event key was missing.

--- src/main/js/analytics/Constants.js
+++ src/main/js/analytics/Constants.js
@@ -3,5 +3,6 @@
 var nsKeyMirror = require('dataserverinterface/utils/namespaced-key-mirror');
 
 module.exports = nsKeyMirror('analytics', {
-	NEW_EVENT: null
+	NEW_EVENT: null,
+	VIDEO_PLAYER_EVENT: null
 });
