ae9d0873b1f068df4a83afc31772455048064866
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-02-19 14:25:53 -0600

    dispatch an event for beginning to load a course

--- src/main/js/course/Actions.js
+++ src/main/js/course/Actions.js
@@ -2,7 +2,11 @@ import AppDispatcher from 'dispatcher/AppDispatcher';
 
 import {getLibrary} from 'library/Api';
 
-import {SET_ACTIVE_COURSE, NOT_FOUND} from './Constants';
+import {
+	SET_ACTIVE_COURSE,
+	SET_ACTIVE_COURSE_BEGIN,
+	NOT_FOUND
+} from './Constants';
 
 /**
  * Actions available to views for course-related functionality.
@@ -13,18 +17,15 @@ export function setCourse (courseId) {
 		return;
 	}
 
+	dispatch(SET_ACTIVE_COURSE_BEGIN, courseId);
+
 	getLibrary()
 		.then(library => library.getCourse(courseId) || Promise.reject(NOT_FOUND))
-		.then(courseEnrollment => {
-			dispatch(SET_ACTIVE_COURSE, courseEnrollment);
-		})
-		.catch(reason => {
-			dispatch(SET_ACTIVE_COURSE, new Error(reason));
-			//Failure
-			//TODO: Display error
-		});
+		.then(
+			courseEnrollment => dispatch(SET_ACTIVE_COURSE, courseEnrollment),
+			reason => dispatch(SET_ACTIVE_COURSE, new Error(reason))
+			);
 }
-
 
 
 function dispatch(key, data) {
--- src/main/js/course/Constants.js
+++ src/main/js/course/Constants.js
@@ -1,3 +1,4 @@
 export const COURSE_NAV_KEY = Symbol('course:Course Navigation Key');
 export const NOT_FOUND = Symbol('course:Not Found');
 export const SET_ACTIVE_COURSE = Symbol('course:Set Active Course');
+export const SET_ACTIVE_COURSE_BEGIN = Symbol('course:Setting Active Course now in progress');
