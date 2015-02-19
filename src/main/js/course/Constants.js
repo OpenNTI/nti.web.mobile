302e7395cc61c71b87b2b1808e8528840943b444
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-02-19 14:25:22 -0600

    Lets use Symbols for "Constants".

--- src/main/js/course/Constants.js
+++ src/main/js/course/Constants.js
@@ -1,3 +1,3 @@
-export const COURSE_NAV_KEY = 'course:Course Navigation Key';
-export const NOT_FOUND = 'course:Not Found';
-export const SET_ACTIVE_COURSE = 'course:Set Active Course';
+export const COURSE_NAV_KEY = Symbol('course:Course Navigation Key');
+export const NOT_FOUND = Symbol('course:Not Found');
+export const SET_ACTIVE_COURSE = Symbol('course:Set Active Course');
