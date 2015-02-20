a5a390eaf7d06ca64508667a0e5e37dcbb8a203b
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-02-19 19:55:18 -0600

    fix module

--- src/main/js/common/utils/viewport.js
+++ src/main/js/common/utils/viewport.js
@@ -1,27 +1,22 @@
-'use strict';
-
-
 function getDocument() {
 	return typeof document === 'undefined' ? {} :
 		document.documentElement || {};
 }
 
-module.exports = {
-	getHeight: function() {
-		return global.innerHeight || getDocument().clientHeight;
-	},
+export function getHeight () {
+	return global.innerHeight || getDocument().clientHeight;
+}
 
-	getWidth: function () {
-		return global.innerWidth || getDocument().clientWidth;
-	},
+export function getWidth  () {
+	return global.innerWidth || getDocument().clientWidth;
+}
 
-	getScreenWidth: function() {
-		var fallback = this.getWidth();
-		return (global.screen || {}).width || fallback;
-	},
+export function getScreenWidth () {
+	var fallback = getWidth();
+	return (global.screen || {}).width || fallback;
+}
 
-	getScreenHeight: function() {
-		var fallback = this.getHeight();
-		return (global.screen || {}).height || fallback;
-	}
-};
+export function getScreenHeight () {
+	var fallback = getHeight();
+	return (global.screen || {}).height || fallback;
+}
