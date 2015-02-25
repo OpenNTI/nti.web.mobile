c12f8a70a720a57d7c73b7fc9d1d105846493a15
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-02-25 16:17:18 -0600

    Nabber work

--- src/main/js/AppFrame.jsx
+++ src/main/js/AppFrame.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-import Avatar from 'common/components/Avatar';
 import Session from 'common/components/Session';
 import Footer from 'common/components/Footer';
 
 import RouteAware from 'common/mixins/NavigatableMixin';
 
-import MessageDisplay from 'messages/components/Display';
 import NavigationBar from 'navigation/components/Bar';
 import Navigation from 'navigation/components/View';
+
 import Notifications from 'notifications/components/View';
 
 import {getAppUsername} from 'common/utils';
@@ -43,9 +42,7 @@ export default React.createClass({
 	},
 
 
-	getOverlayState () {
-		return (this.state || {}).overlay;
-	},
+	getOverlayState () { return (this.state || {}).overlay;	},
 
 
 	componentDidUpdate () {
@@ -68,19 +65,7 @@ export default React.createClass({
 
 				<div className={`off-canvas-wrap ${state}`} data-offcanvas>
 					<div className="inner-wrap">
-
-						<NavigationBar title="NextThought">
-
-							<a className="left-off-canvas-toggle hamburger"
-									onClick={this.onLeftMenuClick}
-									href="#"><span/></a>
-
-							<a	className="right-off-canvas-toggle"
-									onClick={this.onRightMenuClick}
-									href="#"><Avatar username={username} /></a>
-
-						</NavigationBar>
-
+						<NavigationBar title="next thought" branding/>
 
 						<aside className="left-off-canvas-menu" style={height}>
 							<Navigation/>
@@ -92,7 +77,6 @@ export default React.createClass({
 						</aside>
 
 						<section className="main-section">
-							<MessageDisplay />
 							{this.props.children}
 							<Footer />
 						</section>
@@ -113,20 +97,12 @@ export default React.createClass({
 	},
 
 
-	onLeftMenuClick (e) {
-		if (e) {
-			e.preventDefault();
-			e.stopPropagation();
-		}
+	onLeftMenuClick () {
 		this.setState({overlay: LEFT_MENU_OPEN});
 	},
 
 
-	onRightMenuClick (e) {
-		if (e) {
-			e.preventDefault();
-			e.stopPropagation();
-		}
+	onRightMenuClick () {
 		this.setState({overlay: RIGHT_MENU_OPEN});
 	}
 });
--- src/main/js/navigation/components/Bar.jsx
+++ src/main/js/navigation/components/Bar.jsx
@@ -1,25 +1,85 @@
 import React from 'react';
+import cx from 'react/lib/cx';
+import PureRenderMixin from 'react/lib/ReactComponentWithPureRenderMixin';
+
+import {getAppUsername} from 'common/utils';
+
+import Avatar from 'common/components/Avatar';
 
 import BasePathAware from 'common/mixins/BasePath';
+
+var Hamburger = React.createClass({
+	mixins: [PureRenderMixin],
+
+	onClick (e) {
+		e.preventDefault();
+		e.stopPropagation();
+		this.props.onClick();
+	},
+
+	render () {
+		let props = {
+			className: 'left-off-canvas-toggle hamburger',
+			onClick: this.onClick,
+			href: '#'
+		};
+
+		return <a {...props}><span/></a>;
+	}
+});
+
+var UserMenu = React.createClass({
+	mixins: [PureRenderMixin],
+
+	onClick (e) {
+		e.preventDefault();
+		e.stopPropagation();
+		this.props.onClick();
+	},
+
+	render () {
+		let props = {
+			className: 'right-off-canvas-toggle',
+			onClick: this.onClick,
+			href: '#'
+		};
+		let username = getAppUsername();
+		return <a {...props}><Avatar username={username} /></a>;
+	}
+});
+
 
 export default React.createClass({
 	displayName: 'NavigationBar',
 	mixins: [BasePathAware],
 
-	onMenuTap () {
+	contextTypes: {
+		triggerLeftMenu: React.PropTypes.func.isRequired,
+		triggerRightMenu: React.PropTypes.func.isRequired
+	},
 
+	onMenuTap () {
 	},
 
 
 	render () {
-		let [left, right] = this.props.children;
+		let {triggerLeftMenu, triggerRightMenu} = this.context;
+
+		let left = <Hamburger onClick={triggerLeftMenu}/>;
+		let right = <UserMenu onClick={triggerRightMenu}/>;
+
+		let css = cx({
+			'title': true,
+			'branding': !!this.props.branding
+		});
 
 		return (
 			<nav className="tab-bar">
 				<section className="left-small">{left}</section>
 
 				<section className="middle tab-bar-section">
-					<a href={this.getBasePath()} onClick={this.onMenuTap}><h1 className="title">{this.props.title}</h1></a>
+					<a href={this.getBasePath()} onClick={this.onMenuTap}>
+						<h1 className={css}>{this.props.title}</h1></a>
 				</section>
 
 				<section className="right-small">{right}</section>
