51ce804bb215333f0d0a4f9b6560745ad375678b
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-02-25 11:18:51 -0600

    Example Usage of Master Detail

--- src/main/js/course/components/Overview.jsx
+++ src/main/js/course/components/Overview.jsx
@@ -7,7 +7,10 @@ import Loading from 'common/components/Loading';
 import ErrorWidget from 'common/components/Error';
 import AnalyticsStore from 'analytics/Store';
 
-// This is an example of the correct way to aquire a reference to 
+import OutlineView from './OutlineView';
+import MasterDetail from 'navigation/components/MasterDetail';
+
+// This is an example of the correct way to aquire a reference to
 // this mixin from outside of the `widgets` package. If this comment
 // strikes you odd, see the comment block with the `./widgets/Mixin.js`
 import {Mixin} from './widgets';
@@ -97,23 +100,27 @@ export default React.createClass({
 
 
 	render () {
-		var {data,node, loading, error} = this.state;
-		var pages = node && node.getPageSource();
-		var currentPage = this.getOutlineID();
+		let {data, node, loading, error} = this.state;
+		let {course} = this.props;
+		let pages = node && node.getPageSource();
+		let currentPage = this.getOutlineID();
 
 		if (loading) { return (<Loading/>); }
 		if (error) { return (<ErrorWidget error={error}/>); }
 
-		var title = (data || {}).title;
-		var items = (data || {}).Items || [];
+		let title = (data || {}).title;
+		let items = (data || {}).Items || [];
 
 		return (
-			<div className="course-overview row">
-				<Pager pageSource={pages} current={currentPage}/>
-				<DateTime date={node.AvailableBeginning} className="label" format="dddd, MMMM Do"/>
-				<h1 dangerouslySetInnerHTML={{__html: title}}/>
-				{this._renderItems(items, {node: node})}
-			</div>
+			<MasterDetail>
+				<OutlineView item={course}/>
+				<div className="course-overview row">
+					<Pager pageSource={pages} current={currentPage}/>
+					<DateTime date={node.AvailableBeginning} className="label" format="dddd, MMMM Do"/>
+					<h1 dangerouslySetInnerHTML={{__html: title}}/>
+					{this._renderItems(items, {node: node})}
+				</div>
+			</MasterDetail>
 		);
 	}
 });
