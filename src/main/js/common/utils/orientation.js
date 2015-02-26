0fe4df545b1098dc92f11afeecc2eb3e3e02cf60
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-02-26 14:41:33 -0600

    do not `document.body.className = ""`  use add/removeClass instead.

--- src/main/js/common/utils/orientation.js
+++ src/main/js/common/utils/orientation.js
@@ -1,34 +1,35 @@
-'use strict';
+import {addClass, removeClass} from './dom';
 
-module.exports = exports = {
+function handler (component) {
+	let {body} = document;
+	var state = 'portrait';
+	var w = global; //window
+	if(Math.abs(w.orientation) === 90 || w.innerWidth > w.innerHeight) {
+		state ='landscape';
+	}
 
+	removeClass(body, 'portrait');
+	removeClass(body, 'landscape');
+	addClass(body, state);
 
-	init: function(component) {
+	//console.debug('Window is now: %s', state);
+	if (component && component.isMounted()) {
+		component.setState({orientation: state});
+	}
+}
+
+export default {
+
+	init (component) {
 		global.addEventListener('orientationchange', this.changeHandler.bind(this, component));
 		this.changeHandler(component);
 	},
 
 
-	changeHandler: function OnOrientationChange(component) {
+	changeHandler (component) {
 		// delay this handler because on android the innerWidth and innerHeight
 		// properties may not be updated yet, and because on android the height
 		// of the nav drawer doesn't update if we fire immediately.
-		setTimeout(function() {
-			this._handler(component);
-		}.bind(this), 500);
-	},
-
-	_handler: function(component) {
-		var state = 'portrait';
-		var w = global; //window
-		if(Math.abs(w.orientation) === 90 || w.innerWidth > w.innerHeight) {
-			state ='landscape';
-		}
-		document.body.className = state;
-		//console.debug('Window is now: %s', state);
-		if (component && component.isMounted()) {
-			component.setState({orientation: state});
-		}
+		setTimeout(()=>handler(component), 500);
 	}
-
 };
