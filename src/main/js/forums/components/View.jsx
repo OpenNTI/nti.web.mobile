17edcbc25680f123e9d5c002087151d736ed8561
Ray Hatfield <ray.hatfield@nextthought.com>
2015-02-26 16:17:23 -0600

    add root discussions context/breadcrumb

--- src/main/js/forums/components/View.jsx
+++ src/main/js/forums/components/View.jsx
@@ -3,11 +3,25 @@ import React from 'react';
 import CourseBanner from './widgets/CourseBanner';
 import ForumListView from './ForumListView';
 import ForumView from './ForumView';
-
+import Breadcrumb from 'common/components/Breadcrumb';
 import Router from 'react-router-component';
+import Navigatable from 'common/mixins/NavigatableMixin';
 
 export default React.createClass({
 	displayName: 'ForumsView',
+
+	mixins: [Navigatable],
+
+	__getContext: function() {
+		var href = this.makeHref('/', true);
+		return Breadcrumb.noContextProvider().then(context => {
+			context.push({
+				label: "Discussions",
+				href: href
+			});
+			return context;
+		});
+	},
 
 	render () {
 		let {course} = this.props;
@@ -16,7 +30,7 @@ export default React.createClass({
 				<CourseBanner course={course} />
 				<div className="forums-wrapper">
 					<Router.Locations contextual>
-						<Router.Location path="/(:forumId)/*" handler={ForumView} course={course} />
+						<Router.Location path="/(:forumId)/*" handler={ForumView} course={course} contextProvider={this.__getContext}/>
 						<Router.Location path="/" handler={ForumListView} course={course} />
 					</Router.Locations>
 				</div>
