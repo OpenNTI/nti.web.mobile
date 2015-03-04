251cbeb8c83f7b516707d0309fc76eb0c6e81313
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-03-04 01:15:45 -0600

    make these routes more data driven

--- src/main/js/course/components/Lessons.jsx
+++ src/main/js/course/components/Lessons.jsx
@@ -10,43 +10,34 @@ import Overview from './Overview';
 
 import ContentViewer from 'content/components/Viewer';
 
+const ROUTES = [
+	{path: '/:outlineId/c/:rootId(/*)',		handler: Page, pageContent: ContentViewer,	slug: 'c'},
+	{path: '/:outlineId/v/:videoId(/*)',	handler: Page, pageContent: Media,			slug: 'v'},
+	{path: '/:outlineId(/*)',				handler: Page, pageContent: Overview },
+	{}//
+];
 
 export default React.createClass({
 	displayName: 'Lessons',
 
 	render () {
 		let {course, contextProvider} = this.props;
-		return (
-				<Router.Locations contextual>
-					<Router.Location path="/:outlineId/c/:rootId(/*)"
-							handler={Page}
-							pageContent={ContentViewer}
-							contentPackage={course}
-							contextProvider={contextProvider}
-							slug="c"
-							/>
 
-					<Router.Location path="/:outlineId/v/:videoId(/*)"
-							handler={Page}
-							pageContent={Media}
-							course={course}
-							contextProvider={contextProvider}
-							slug="v"
-							/>
+		return React.createElement(Router.Locations, {contextual: true},
+			...ROUTES.map(route=>
+				route.path ?
+				<Router.Location {...route}
 
-					<Router.Location path="/:outlineId(/*)"
-							handler={Page}
-							pageContent={Overview}
-							course={course}
-							contextProvider={contextProvider}
-							/>
-
-					<Router.NotFound handler={Page}
-							pageContent={Outline}
-							item={course}
-							contextProvider={contextProvider}
-							/>
-				</Router.Locations>
-		);
+					sectionPathPrefix="../"
+					contentPackage={course}
+					contextProvider={contextProvider}
+					course={course}
+					/> :
+				<Router.NotFound handler={Page} pageContent={Outline}
+						sectionPathPrefix="../"
+						contextProvider={contextProvider}
+						item={course}
+						/>
+			));
 	}
 });
