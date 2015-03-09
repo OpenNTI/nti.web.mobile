3d3bd3e633cdf4ad87f4992f2416c04be3aaa5d2
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-03-09 12:36:03 -0500

    quick fix to put the nav bar on books. (last 10% ... getting the content title in the bar... is significant, so I'll do it later)

--- src/main/js/content/components/View.jsx
+++ src/main/js/content/components/View.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Router from 'react-router-component';
 
+import Page from 'common/components/Page';
+import BasePathAware from 'common/mixins/BasePath';
+
 import Viewer from './Viewer';
 
 export default React.createClass({
 	displayName: 'ContentView',
+	mixins: [BasePathAware],
 
 	propTypes: {
 		packageId: React.PropTypes.string
@@ -17,8 +21,10 @@ export default React.createClass({
 			<Router.Locations contextual>
 				<Router.Location path="/*"
 									rootId={this.props.pkgId}
-									handler={Viewer}
+									handler={Page}
+									pageContent={Viewer}
 									slug="/content/"
+									title="Content"
 									contextProvider={this.__getContext}/>
 			</Router.Locations>
 		);
@@ -32,7 +38,15 @@ export default React.createClass({
 	* @param {Object} props The props set from the handler of the route.
 	*/
 	__getContext (/*props*/) {
-
-		return Promise.resolve([]);
+		return Promise.resolve([
+			{
+				label: 'Books',
+				href: this.getBasePath()
+			}, {
+				// ntiid: course.getID(),
+				// label: presentation.title,
+				// href: path.join(this.getBasePath(), 'course', this.props.course, '/o/')
+			}
+		]);
 	}
 });
