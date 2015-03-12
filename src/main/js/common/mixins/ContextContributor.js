b27ada57d91318f827db00faa18746615bbc292a
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-03-12 16:50:33 -0500

    split out the context contribution portion form the end-point portion. This will let you add the mixin to something that is not itself a destination and allow it to add to the context (like adding a back link to the library)

---
+++ src/main/js/common/mixins/ContextContributor.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const GetContext = 'context:provider:get-local';
+
+export default {
+
+	contextTypes: {
+		contextProvider: React.PropTypes.func
+	},
+
+
+	childContextTypes: {
+		contextProvider: React.PropTypes.func
+	},
+
+
+	getChildContext () {
+		return {
+			contextProvider: this.resolveContext
+		};
+	},
+
+
+	[GetContext] () {
+		return this.getContext ?
+			this.getContext() :
+			Promise.resolve([]);
+	},
+
+
+	resolveContext () {
+		let getParentContext = this.context.contextProvider;
+		let getContext = this[GetContext];
+
+		if (getParentContext) {
+			return Promise.all([
+				getParentContext(),
+				getContext()]).then(x=>{
+					let [parent, ours] = x;
+
+					return [].concat(parent).concat(ours);
+				});
+		}
+
+		return getContext();
+	}
+};
--- src/main/js/common/mixins/ContextSender.js
+++ src/main/js/common/mixins/ContextSender.js
@@ -1,50 +1,12 @@
 import React from 'react';
-
-const GetContext = 'context:provider:get-local';
+import Contributor from './ContextContributor';
 
 export default {
+	mixins: [Contributor],
 
 	contextTypes: {
 		navigationContext: React.PropTypes.func.isRequired,
-		hasPages: React.PropTypes.func.isRequired,
-		contextProvider: React.PropTypes.func
-	},
-
-
-	childContextTypes: {
-		contextProvider: React.PropTypes.func
-	},
-
-
-	getChildContext () {
-		return {
-			contextProvider: this.resolveContext
-		};
-	},
-
-
-	[GetContext] () {
-		return this.getContext ?
-			this.getContext() :
-			Promise.resolve([]);
-	},
-
-
-	resolveContext () {
-		let getParentContext = this.context.contextProvider;
-		let getContext = this[GetContext];
-
-		if (getParentContext) {
-			return Promise.all([
-				getParentContext(),
-				getContext()]).then(x=>{
-					let [parent, ours] = x;
-
-					return [].concat(parent).concat(ours);
-				});
-		}
-
-		return getContext();
+		hasPages: React.PropTypes.func.isRequired
 	},
 
 
