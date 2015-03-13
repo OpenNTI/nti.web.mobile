6e02d92bf50abcf8fde7b743905aab3d74dd647d
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-03-12 21:46:26 -0500

    fix missing menu

--- src/main/js/common/components/Page.jsx
+++ src/main/js/common/components/Page.jsx
@@ -22,7 +22,7 @@ export default React.createClass({
 
 		return (
 			<div>
-				<NavigationBar title={title} {...props}/>
+				<NavigationBar title={title} {...this.props}/>
 				{Content ?
 					<Content {...props}/>
 					:
