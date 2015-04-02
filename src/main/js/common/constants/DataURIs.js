be3646fe3eded871e90bd4ad7d2cef28c6e96ae5
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-04-02 14:47:47 -0500

    ES6'ify

--- src/main/js/common/constants/DataURIs.js
+++ src/main/js/common/constants/DataURIs.js
@@ -1,4 +1,2 @@
-'use strict';
-
-exports.BLANK_IMAGE = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
-exports.BLANK_AVATAR = require('../../../resources/images/icons/unresolved-user.png');
+export const BLANK_IMAGE = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+export const BLANK_AVATAR = require('../../../resources/images/icons/unresolved-user.png');
