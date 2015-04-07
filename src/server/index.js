ba384b0bca9235b4d1083718ce7c93cb12009240
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-04-07 14:45:22 -0500

    allow config to be passed as a url

--- src/server/index.js
+++ src/server/index.js
@@ -19,39 +19,44 @@ var common = require('./lib/common');
 var server = require('./lib/app-server');
 var logger = require('./lib/logger');
 
-var config = common.config();
-var protocol = config.protocol === 'proxy' ? proxiedHttp : http;
-var address = config.address || '0.0.0.0';
-var port = config.port || 9000;
+common.loadConfig().then(function(config) {
 
-//WWW Server
-var app = express();
+	var protocol = config.protocol === 'proxy' ? proxiedHttp : http;
+	var address = config.address || '0.0.0.0';
+	var port = config.port || 9000;
 
-var mobileapp = express();
-mobileapp.use(config.basepath, app);//re-root the app to /mobile/
-mobileapp.all('/', function(_, res) { res.redirect('/mobile/'); });
+	//WWW Server
+	var app = express();
 
-var port = server.setupApplication(app, config);
+	var mobileapp = express();
+	mobileapp.use(config.basepath, app);//re-root the app to /mobile/
+	mobileapp.all('/', function(_, res) { res.redirect('/mobile/'); });
 
-//Errors
-// We need the signature to be 4 args long
-// for express to treat it as a error handler
-app.use(function(err, req, res, next){// eslint-disable-line no-unused-vars
-	if (!err) {
-		err = 'Unknown Error';
-	}
-	else if (err.toJSON) {
-		err = err.toJSON();
-	}
-	else if (err.stack) {
-		err = err.stack;
-	}
+	port = server.setupApplication(app, config);
 
-	logger.error('%o', err);
+	//Errors
+	// We need the signature to be 4 args long
+	// for express to treat it as a error handler
+	app.use(function(err, req, res, next){// eslint-disable-line no-unused-vars
+		if (!err) {
+			err = 'Unknown Error';
+		}
+		else if (err.toJSON) {
+			err = err.toJSON();
+		}
+		else if (err.stack) {
+			err = err.stack;
+		}
 
-	res.status(err.statusCode || 500).send(err.body || 'Oops! Something broke!');
+		logger.error('%o', err);
+
+		res.status(err.statusCode || 500).send(err.body || 'Oops! Something broke!');
+	});
+
+	//Go!
+	protocol.createServer(mobileapp || app).listen(port, address, function() {
+		logger.info('Listening on port %d', port); });
+
+}, function (error) {
+	logger.error('Failed to load config: ', error);
 });
-
-//Go!
-protocol.createServer(mobileapp || app).listen(port, address, function() {
-	logger.info('Listening on port %d', port); });
--- src/server/lib/common.js
+++ src/server/lib/common.js
@@ -1,14 +1,12 @@
+import fs from 'fs';
+import url from 'url';
+import path from 'path';
+import request from 'request';
 import getSite from './site-mapping';
+import logger from './logger';
 import {SiteName, ServiceStash} from 'nti.lib.interfaces/CommonSymbols';
 
-let env = {};
-
-try {
-	env = require('../config/env.json');
-} catch(e) {
-	console.error('You do not have an environment config file. ' +
-					'See: %s/config/env.json.example', __dirname);
-}
+let env = null; //until this loads, calling config() should blow up.
 
 //Use native node require() for optimist since it defines a 'default' property on the exports.
 let opt = require('optimist').usage('WebApp Instance')
@@ -23,7 +21,6 @@ let opt = require('optimist').usage('WebApp Instance')
 			})
 			.options('p', {
 				alias: 'port',
-				default: env.port || undefined,
 				desc: 'Liston on port'
 			})
 			.options('protocol', {
@@ -31,15 +28,51 @@ let opt = require('optimist').usage('WebApp Instance')
 				default: 'proxy',
 				desc: 'Protocol to use (proxy or http)'
 			})
+			.options('config', {
+				demand: true,
+				default: '../config/env.json',
+				desc: 'URI/path to config file (http/https/file/path)'
+			})
 			/*eslint no-throw-literal:0*/
 			.check(v => {if (v.hasOwnProperty('h')){ throw false; }})
 			.argv;
 
-let baseOverrides = {
-	protocol: opt.protocol,
-	address: opt.l,
-	port: opt.p
-};
+
+export function loadConfig () {
+	if (!opt.config) {
+		return Promise.reject('No config file specified');
+	}
+
+	return new Promise((pass, fail)=> {
+
+		let uri = url.parse(opt.config);
+		if (uri.protocol == null || uri.protocol === 'file:') {
+			uri = opt.config.replace(/^file:\/\//i, '');
+			uri = path.resolve(__dirname, uri);
+			if (!fs.existsSync(uri)) {
+				logger.error('You do not have an environment config file at: ', uri,
+					`See: ${path.resolve(__dirname, '../config/env.json.example')}`);
+			}
+			env = JSON.parse(fs.readFileSync(uri));
+			return pass(config());
+		}
+
+		request(opt.config, function (error, response, body) {
+			if (!error && response.statusCode === 200) {
+				try {
+					env = JSON.parse(body);
+					pass(config());
+				} catch (e) {
+					logger.error(e);
+					fail(e);
+				}
+			} else {
+				fail(error || response);
+			}
+		});
+	});
+}
+
 
 
 function override(dest, overrides) {
@@ -56,8 +89,11 @@ export function config() {
 	let base = 'development';
 
 	return override(
-		Object.assign({}, env[base], env[process.env.NODE_ENV] || {}),
-		baseOverrides);
+		Object.assign({}, env[base], env[process.env.NODE_ENV] || {}), {
+			protocol: opt.protocol,
+			address: opt.l,
+			port: opt.p || env.port
+		});
 }
 
 export function clientConfig (username, context) {
