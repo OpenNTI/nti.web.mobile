e56c59a4cf3e42529d70590dca2e763b8bd7cba2
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-04-16 15:44:52 -0500

    Make the InsertImageButton do all the work leading up to inserting the canvas ...

--- src/main/js/modeled-content/components/InsertImageButton.jsx
+++ src/main/js/modeled-content/components/InsertImageButton.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import {createFromImage} from 'nti.lib.whiteboardjs/lib/utils';
+
 export default React.createClass({
 	displayName: 'InsertImageButton',
 
 	render () {
+		if (typeof FileReader === 'undefined') {
+			return null;//don't render the button.
+		}
+
 		return (
 			<button className="insert-whiteboard">
 				Insert Whiteboard
@@ -13,7 +19,32 @@ export default React.createClass({
 	},
 
 
+	insertWhiteboard (data) {
+		console.log(data);
+	},
+
+
+	onError () {
+		console.debug('Oops...');
+	},
+
+
 	onSelect (e) {
-		console.log(e.target.files);
+		let reader = new FileReader();
+		let img = new Image();
+		let file = e.target.files[0];
+
+		img.onerror = this.onError;
+		img.onload = ()=> createFromImage(img).then(this.insertWhiteboard, this.onError);
+
+		//file.size
+		if (!file || !(/image\/.*/i).test(file.type)) {
+			alert('Please Select an Image');//eslint-disable-line no-alert
+			return;
+		}
+
+
+		reader.onload = event => img.src = event.target.result;
+		reader.readAsDataURL(file);
 	}
 });
