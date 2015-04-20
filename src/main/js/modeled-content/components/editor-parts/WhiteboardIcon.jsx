7b4902bd7f17d5f543ac0115774a344c949c51ee
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-04-20 12:34:14 -0500

    restore  unselectable="on" to whiteboard icons. (for IE)

--- src/main/js/modeled-content/components/editor-parts/WhiteboardIcon.jsx
+++ src/main/js/modeled-content/components/editor-parts/WhiteboardIcon.jsx
@@ -14,14 +14,14 @@ export default React.createClass({
 		scene = JSON.stringify(scene) || '';
 
 		return (
-			<div contentEditable={false} className="body-divider whiteboard">
-				<div className="whiteboard-wrapper">
+			<div contentEditable={false} className="body-divider whiteboard" unselectable="on">
+				<div className="whiteboard-wrapper" unselectable="on">
 					<script type="application/json" dangerouslySetInnerHTML={{__html: scene}}/>
-					<img src={thumbnail} className="thumbnail" alt="Whiteboard Thumbnail" border="0"/>
-					<div className="fill"/>
+					<img src={thumbnail} className="thumbnail" alt="Whiteboard Thumbnail" border="0" unselectable="no"/>
+					<div className="fill" unselectable="no"/>
 					{/*}
-					<div className="centerer">
-						<div className="edit">Edit</div>
+					<div className="centerer" unselectable="no">
+						<div className="edit" unselectable="no">Edit</div>
 					</div>
 					{*/}
 				</div>
