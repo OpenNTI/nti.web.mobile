4da4fd880b9d3742be5ef2003fab705e565430d4
Ray Hatfield <ray.hatfield@nextthought.com>
2015-04-20 16:55:29 -0500

    Declare event handler constants for RenderFormConfigMixin.

--- src/main/js/common/forms/Constants.js
+++ src/main/js/common/forms/Constants.js
@@ -7,3 +7,22 @@ export const URL_RETRIEVED = 'forms:URL Retrieved';
 export const FIELD_VALUE_CHANGE = 'forms:Field Value Changed';
 export const FIELD_VALUES_REMOVED = 'forms:Field Value Removed';
 export const AVAILABLE_FIELDS_CHANGED = 'forms:Available Fields Changed';
+
+/**
+	Constants for users of the RenderFormConfix mixin to declare event handlers, as in:
+	[ON_CHANGE]: function(event) { ... }
+	[ON_BLUR]: function(event) { ... }
+	[ON_FOCUS]: function(event) { ... }
+*/
+const prefix = 'FormConstants:';
+const ON_BLUR = prefix + 'ON_BLUR';
+const ON_CHANGE = prefix + 'ON_CHANGE';
+const ON_FOCUS = prefix + 'ON_FOCUS';
+const RADIO_CHANGED = prefix + 'RADIO_CHANGED';
+
+export const RENDERED_FORM_EVENT_HANDLERS = {
+	ON_BLUR,
+	ON_CHANGE,
+	ON_FOCUS,
+	RADIO_CHANGED
+};
