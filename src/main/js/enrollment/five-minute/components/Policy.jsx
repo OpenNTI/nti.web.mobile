3652f6a6f4f978b14cc796c3a333d35d36781f1f
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-04-30 15:54:26 -0500

    Add context to new Policy view.

--- src/main/js/enrollment/five-minute/components/Policy.jsx
+++ src/main/js/enrollment/five-minute/components/Policy.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 
 import PanelButton from 'common/components/PanelButton';
 
+import ContextSender from 'common/mixins/ContextSender';
+import NavigatableMixin from 'common/mixins/NavigatableMixin';
+
+
 import {scoped} from 'common/locale';
 let t = scoped('ENROLLMENT.forms.fiveminute');
 
 export default React.createClass({
 	displayName: 'Policy',
+	mixins: [NavigatableMixin, ContextSender],
+
+
+	getContext () {
+		let href = this.makeHref('');
+		return Promise.resolve([ { label: 'Policy', href } ]);
+	},
+
 
 	render () {
 		return (
