f8054dd81c2f90fcf63be7f43ca249793ac30b04
Ray Hatfield <ray.hatfield@nextthought.com>
2015-05-01 17:18:11 -0500

    getInitialState shouldn't query the Store for data.

--- src/main/js/enrollment/store-enrollment/components/GiftView.jsx
+++ src/main/js/enrollment/store-enrollment/components/GiftView.jsx
@@ -53,19 +53,19 @@ export default React.createClass({
 	},
 
 	getInitialState () {
-		//FIXME: Re-write this:
-		// See: http://facebook.github.io/react/tips/props-in-getInitialState-as-anti-pattern.html
-		// Additional Note: On Mount and Recieve Props fill state (this is ment to be called one per CLASS lifetime not Instance lifetime)
-
-		let formData = Store.getPaymentFormData();
-
 		return {
 			agreed: false,
 			loading: true,
-			fieldValues: formData,
 			submitEnabled: true,
 			errors: {}
 		};
+	},
+
+	componentWillMount() {
+		let formData = Store.getPaymentFormData();
+		this.setState({
+			fieldValues: formData
+		});
 	},
 
 	componentDidMount () {
