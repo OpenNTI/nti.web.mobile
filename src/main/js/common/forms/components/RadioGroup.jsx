691149728c93f8944d115a5705d80da9c889a810
Ray Hatfield <ray.hatfield@nextthought.com>
2015-05-01 14:11:52 -0500

    provide larger hit target for radio button labels. fix unclickability problem with radio labels on iOS.

--- src/main/js/common/forms/components/RadioGroup.jsx
+++ src/main/js/common/forms/components/RadioGroup.jsx
@@ -7,14 +7,15 @@ export default React.createClass({
 		return (this.props.options || []).map((option, index) => {
 			let value = this.props.value || this.props.defaultValue;
 			let checked = value && value === option.value;
-
+			let id = [this.props.name, option.value].join(':');
 			return (
-				<label key={'option' + index}>
+				<label className="radio" key={'option' + index} htmlFor={id}>
 					<input {...this.props} type="radio"
+								id={id}
 								name={this.props.name}
 								checked={checked}
 								value={option.value} />
-					<span>{option.label}</span>
+					<span className="htmlLabel">{option.label}</span>
 				</label>
 			);
 		});
