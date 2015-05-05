4082e4393a826e78f1f0024c4d8a79898773f709
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-05-05 11:09:41 -0500

    reword errors. rename isNode to noConfig

--- src/main/js/common/utils/index.js
+++ src/main/js/common/utils/index.js
@@ -18,22 +18,22 @@ function exposeGlobaly (...fns) {
 }
 
 
-function isNode() {
-	return typeof $AppConfig === 'undefined' && typeof global.process !== 'undefined';
+function noConfig() {
+	return typeof $AppConfig === 'undefined';
 }
 
 
 export function getAppUsername () {
-	if (isNode()) {
-		console.error('utils:getAppUsername() was called in global scope.');
+	if (noConfig()) {
+		console.error('utils:getAppUsername() was called before config was defined.');
 	}
 	return $AppConfig.username;
 }
 
 
 export function getBasePath () {
-	if (isNode()) {
-		console.error('utils:getBasePath() was called in global scope.');
+	if (noConfig()) {
+		console.error('utils:getBasePath() was called before config was defined.');
 	} else {
 		console.error('[DEPRECATED] utils:getBasePath() is replaced with the BasePath Mixin');
 	}
@@ -42,8 +42,8 @@ export function getBasePath () {
 
 
 export function getServerURI () {
-	if (isNode()) {
-		console.error('utils:getServerURI() was called in global scope.');
+	if (noConfig()) {
+		console.error('utils:getServerURI() was called before config was defined.');
 	}
 	return $AppConfig.server;
 }
@@ -58,8 +58,8 @@ export function getSiteName () {
 
 
 export function isFlag (flagName) {
-	if (isNode()) {
-		console.error('utils:isFlag() was called in global scope.');
+	if (noConfig()) {
+		console.error('utils:isFlag() was called before config was defined.');
 	}
 	let flags = $AppConfig.flags || {};
 	return !!flags[flagName];
@@ -81,8 +81,8 @@ export function discussionsConfig () {
  * NOTICE: This is for low-level (or anonymous/non-authenticated) work ONLY.
  */
 export function getServer() {
-	if (isNode()) {
-		console.error('utils:getServer() was called in global scope.');
+	if (noConfig()) {
+		console.error('utils:getServer() was called before config was defined.');
 	}
 	let fn = getServer;
 
@@ -106,18 +106,20 @@ export function getServer() {
  * Returns a promise that fulfills with the service descriptor.
  */
 export function getService () {
-	if (isNode()) {
-		console.error('utils:getService() was called in global scope.');
+	if (noConfig()) {
+		console.error('utils:getService() was called before config was defined.');
 	}
 	return $AppConfig.nodeService ?
 		Promise.resolve($AppConfig.nodeService) :
 		getServer().getServiceDocument();
 }
 
+
 exposeGlobaly(getServer, getService);
 
+
 export function installAnonymousService () {
-	if ($AppConfig.nodeInterface || isNode()) {
+	if (noConfig() || $AppConfig.nodeInterface) {
 		return;
 	}
 
@@ -130,15 +132,15 @@ export function installAnonymousService () {
 
 
 export function overrideAppUsername (str) {
-	if (isNode()) {
-		console.error('utils:overrideAppUsername() was called in global scope.');
+	if (noConfig()) {
+		console.error('utils:overrideAppUsername() was called before config was defined.');
 	}
 	$AppConfig.username = str;
 }
 
 export function overrideConfigAndForceCurrentHost(){
-	if (isNode()) {
-		console.error('utils:overrideConfigAndForceCurrentHost() was called in global scope.');
+	if (noConfig()) {
+		console.error('utils:overrideConfigAndForceCurrentHost() was called before config was defined.');
 	}
 	$AppConfig.server = forceCurrentHost($AppConfig.server);
 }
