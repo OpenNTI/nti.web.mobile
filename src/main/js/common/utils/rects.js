36673056d7879b05876d4785ffe62f749c04b4d6
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-05-22 14:24:49 -0500

    moved rect utils from range utils into our rect utils.

--- src/main/js/common/utils/rects.js
+++ src/main/js/common/utils/rects.js
@@ -1,3 +1,4 @@
+import {isTextNode} from 'nti.lib.dom';
 import {getWidth as getViewportWidth, getHeight as getViewportHeight} from './viewport';
 
 export function getElementRect (el) {
@@ -27,6 +28,50 @@ export function getElementRect (el) {
 		// 		height: el.offsetHeight
 		// 	};
 		// }
+	}
+
+	return rect;
+}
+
+export const isZeroRect = r => !r || ((r.top + r.left + r.height + r.width) === 0);
+
+/**
+ * Gets the bounding rect of the provided range.  If the rect is zero
+ * but the range is not collapsed we will attempt to get the bounding box
+ * based on the ranges contents.  We do this because IE sucks.
+ * @param {Range} range range
+ * @returns {Rect} Bounding Rect
+ */
+export function safeBoundingBoxForRange (range) {
+	let {collapsed, commonAncestorContainer, startContainer, startOffset, endContainer, endOffset} = range || {};
+	let rect = range ? range.getBoundingClientRect(range) : null;
+
+	try {
+		if (rect && !collapsed && isZeroRect(rect) && range.toString() === '' && !isTextNode(startContainer)) {
+			console.log('No rect information...attempting to get selected node rect instead');
+			let node = startContainer.childNodes[startOffset];
+			rect = node.getBoundingClientRect();
+		}
+		else if (rect && !collapsed && isZeroRect(rect)) {
+			if (startContainer === endContainer && startContainer.nodeType !== Node.TEXT_NODE) {
+				if (startOffset + 1 === endOffset) {
+					console.debug('No rect information... the range contains just one node, use that instead.');
+					rect = startContainer.childNodes[startOffset].getBoundingClientRect();
+				}
+				else if (startOffset === 0 && endOffset === endContainer.childNodes.length) {
+					console.debug('No rect information... the range contains all all contents of the object/doc, use the startContainer.');
+					rect = startContainer.getBoundingClientRect();
+				}
+			}
+		}
+
+		//if it is still the zero rect and the common ancestor is an object, use the ancestors bounding rect
+		if (isZeroRect(rect) && commonAncestorContainer && commonAncestorContainer.tagName === 'OBJECT') {
+			rect = commonAncestorContainer.getBoundingClientRect();
+		}
+	}
+	catch (er) {
+		console.warn(er.stack || er.message || er);
 	}
 
 	return rect;
