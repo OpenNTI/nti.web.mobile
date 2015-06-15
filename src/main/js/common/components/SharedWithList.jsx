fe3bae6e43b68d1dfb9ff18e6c2bd4b248c971f2
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-06-15 16:16:16 -0500

    try to hide unresolved names in shared with.

--- src/main/js/common/components/SharedWithList.jsx
+++ src/main/js/common/components/SharedWithList.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-import DisplayName from './DisplayName';
+
+import DisplayName, {resolve} from './DisplayName';
+import Loading from './TinyLoader';
 
 const isEmpty = x => !Array.isArray(x) || x.length === 0;
 
@@ -28,7 +30,7 @@ export default React.createClass({
 
 
 	getInitialState () {
-		return {};
+		return {loading: true};
 	},
 
 
@@ -41,24 +43,62 @@ export default React.createClass({
 	},
 
 
-	render () {
-		let {item/*, short*/} = this.props;
+	componentDidMount () {
+		this.fill(this.props.item);
+	},
+
+	componentWillReceiveProps (props) {
+		if (this.props.item !== props.item) {
+			this.fill(props.item);
+		}
+	},
+
+
+	fill (item) {
+		this.setState({loading: true});
+
 		let {sharedWith=[]} = item;
-		//TODO: implement short format.
+		let pending = sharedWith
+			.filter(x => x && x !== EVERYONE)
+			.map(username=> resolve(this, {username}).catch(()=>null));
+
+
+		Promise.all(pending)
+			.then(list=> {
+
+				let users = list.filter(x=>x);
+				let others = list.length - users.length;
+
+				this.setState({users, others, loading: false});
+			});
+	},
+
+
+	render () {
+		let {short, item} = this.props;
+		let {loading, users = [], others = 0} = this.state;
+		let {sharedWith = []} = item;
+
+		if (loading) {
+			return (<Loading/>);
+		}
+
+		if (short && users.length > 1) {
+			others += users.length - 1;
+			users = users.slice(0, 1);
+		}
 
 
 		let state = this.isPublic(sharedWith) ?
 			'Public' :
 			sharedWith.length ?
-				'' :
+				(users.length === 0 ? 'Shared' : '') :
 				'Only Me';
 
-		let names = (state ? [state] : []).concat(
-				sharedWith
-					.filter(x => x && x !== EVERYONE)
-					.map(x =>
-						<DisplayName username={x}/>
-					));
+		let names = (state ? [state] : [])
+			.concat(users.map(x => <DisplayName user={x}/> ))
+			//Only show " N Others" when there are more than 0 others, AND we have at least one resolved name.
+			.concat(others === 0 || users.length === 0 ? [] : (`${others} Others`));
 
 		return React.createElement('span', {className: 'shared-with-list'}, ...names);
 	}
