1cd847ef48137870641d3a4abafeabd520dbe8c2
Ray Hatfield <ray.hatfield@nextthought.com>
2015-06-18 15:43:58 -0500

    Added comments

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,11 @@ var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('css', function() {
 	return gulp.src('src/main/resources/scss/app.scss')
-		.pipe(sourcemaps.init())
-		.pipe(sass({
+		.pipe(sourcemaps.init()) // init sourcemaps before invoking sass
+		.pipe(sass({ // compile scscc to css
 			includePaths: ['src/main/resources/vendor/foundation/scss/']
 		}).on('error', sass.logError))
-		.pipe(autoprefixer())
-		.pipe(sourcemaps.write('./'))
+		.pipe(autoprefixer()) // add browser-specific prefixes (e.g. -webkit)
+		.pipe(sourcemaps.write('./')) // write sourcemaps to the same directory as the output.
 		.pipe(gulp.dest('src/main/resources/css'))
 });
