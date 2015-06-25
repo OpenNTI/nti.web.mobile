d73e00678ca9b4c8ff6fe464292adf5d22d0b45c
Ray Hatfield <ray.hatfield@nextthought.com>
2015-06-25 18:09:08 -0500

    Disable content editable for now on user profile.

--- src/main/js/profile/components/About.jsx
+++ src/main/js/profile/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Loading from 'common/components/TinyLoader';
 import {scoped} from 'common/locale';
 import Card from './Card';
-import Editable from './Editable';
+// import Editable from './Editable';
 import renderItemsMixin from './widgets/Mixin';
 
 const t = scoped('PROFILE.ABOUT.SECTIONTITLES');
@@ -30,7 +30,7 @@ export default React.createClass({
 			<ul className="profile-cards">
 				{sections.map((s, index) => {
 					return (
-						<Card className={s} title={t(s)}><Editable>{this.renderItems(user[s], index)}</Editable></Card>
+						<Card className={s} title={t(s)}><div>{this.renderItems(user[s], index)}</div></Card>
 					);
 				})}
 			</ul>
