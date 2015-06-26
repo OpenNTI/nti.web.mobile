3479ffa75e713d016001c93dea1a45d33256d730
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-06-25 23:01:14 -0500

    call getAchievements

--- src/main/js/profile/components/Achievements.jsx
+++ src/main/js/profile/components/Achievements.jsx
@@ -4,6 +4,18 @@ import Card from './Card';
 export default React.createClass({
 	displayName: 'Achievements',
 
+	propTypes: {
+		user: React.PropTypes.object.isRequired
+	},
+
+
+	componentDidMount () {
+		let {user} = this.props;
+		if (user) {
+			user.getAchievements().then(achievements=> console.debug(achievements));
+		}
+	},
+
 	render () {
 		return (
 			<ul className="profile-cards">
