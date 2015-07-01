e491bab8ca3020d1e80443e1011a0b09d6bee5c7
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-07-01 18:18:08 -0500

    hide author line if blank.

--- src/main/js/common/components/Card.jsx
+++ src/main/js/common/components/Card.jsx
@@ -318,7 +318,7 @@ export default React.createClass({
 
 				<h5 dangerouslySetInnerHTML={{__html: label}}/>
 				<hr className="break hide-for-medium-up"/>
-				<div className="label" dangerouslySetInnerHTML={{__html: 'By ' + creator}/*TODO: localize*/}/>
+				<div className="label" dangerouslySetInnerHTML={creator ? {__html: 'By ' + creator} : null /*TODO: localize*/}/>
 				<div className="description" dangerouslySetInnerHTML={{__html: desc}}/>
 				<div className="comment-count" href="/discussions/" onClick={this.onClickDiscussion}>
 					{commentCount == null
