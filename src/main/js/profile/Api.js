b6760eb0ceafc501be1b4e90b35ffe053de22245
Ray Hatfield <ray.hatfield@nextthought.com>
2015-07-07 17:26:52 -0500

    Added API::leaveGroup

--- src/main/js/profile/Api.js
+++ src/main/js/profile/Api.js
@@ -1,3 +1,5 @@
+import {getService} from 'common/utils';
+
 export function getBreadcrumb(item) {
 	return (item || {}).getContextPath ? item.getContextPath() : Promise.reject('item doesn\'t have a getContextPath method.');
 }
@@ -9,5 +11,15 @@ export function getThumbnail(item) {
 			return (breadcrumb[0][0].getPresentationProperties() || {}).thumb;
 		}
 		return Promise.reject('No thumbnail found.');
+	});
+}
+
+export function leaveGroup(entity) {
+	if (!entity || !entity.getLink) {
+		console.error('Group entity with getLink method is required for leaveGroup.');
+	}
+	let link = entity.getLink('my_membership');
+	return getService().then(service => {
+		return service.delete(link);
 	});
 }
