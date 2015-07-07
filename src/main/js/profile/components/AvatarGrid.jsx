7a6f1d19192f1ada16b9e7289ceb72da0b33b4a5
Ray Hatfield <ray.hatfield@nextthought.com>
2015-07-07 11:59:38 -0500

    link avatars to profiles.

--- src/main/js/profile/components/AvatarGrid.jsx
+++ src/main/js/profile/components/AvatarGrid.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Avatar from 'common/components/Avatar';
 import DisplayName from 'common/components/DisplayName';
+import BasePathAware from 'common/mixins/BasePath';
+import {join} from 'path';
+import {encode} from 'common/utils/user';
 
 export default React.createClass({
 	displayName: 'AvatarGrid',
@@ -9,21 +12,26 @@ export default React.createClass({
 		entities: React.PropTypes.array.isRequired
 	},
 
+	mixins: [BasePathAware],
+
 	render () {
 
 		let {entities} = this.props;
 
+		let base = this.getBasePath();
+
 		return (
 			<ul className="avatar-grid">
-				{(entities || []).map((e, i) =>
-					<li key={'avatar' + i}>
-						<Avatar entity={e} />
-						<div className="body">
-							<DisplayName entity={e} />
-							<span className="location">{e.location}</span>
-						</div>
-					</li>
-				)}
+				{(entities || []).map((e, i) => {
+					let profile = join(base, 'profile', encode(e.Username));
+					return (<li key={'avatar' + i}>
+								<a href={profile}><Avatar entity={e} /></a>
+								<div className="body">
+									<DisplayName entity={e} />
+									<span className="location">{e.location}</span>
+								</div>
+							</li>);
+				})}
 			</ul>
 		);
 	}
