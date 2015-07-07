c14f48c8fd990e4b45a667257af53446b281800f
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-07-07 14:18:22 -0500

    use the profile link

--- src/main/js/profile/components/AvatarGrid.jsx
+++ src/main/js/profile/components/AvatarGrid.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import Avatar from 'common/components/Avatar';
 import DisplayName from 'common/components/DisplayName';
-import BasePathAware from 'common/mixins/BasePath';
-import {join} from 'path';
-import {encode} from 'common/utils/user';
+
+import ProfileLink from './ProfileLink';
 
 export default React.createClass({
 	displayName: 'AvatarGrid',
@@ -12,26 +11,21 @@ export default React.createClass({
 		entities: React.PropTypes.array.isRequired
 	},
 
-	mixins: [BasePathAware],
 
 	render () {
-
 		let {entities} = this.props;
-
-		let base = this.getBasePath();
 
 		return (
 			<ul className="avatar-grid">
-				{(entities || []).map((e, i) => {
-					let profile = join(base, 'profile', encode(e.Username));
-					return (<li key={'avatar' + i}>
-								<a href={profile}><Avatar entity={e} /></a>
-								<div className="body">
-									<DisplayName entity={e} />
-									<span className="location">{e.location}</span>
-								</div>
-							</li>);
-				})}
+				{(entities || []).map((e, i) => (
+					<li key={'avatar' + i}>
+						<ProfileLink entity={e}><Avatar entity={e} /></ProfileLink>
+						<div className="body">
+							<DisplayName entity={e} />
+							<span className="location">{e.location}</span>
+						</div>
+					</li>
+				))}
 			</ul>
 		);
 	}
