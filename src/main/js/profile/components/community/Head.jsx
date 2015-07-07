926488c9b5e5a0c01375d753ddb74c0196a7efd8
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-07-06 21:27:10 -0500

    incr cmty

--- src/main/js/profile/components/community/Head.jsx
+++ src/main/js/profile/components/community/Head.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+
+import {Link} from 'react-router-component';
+
+import cx from 'classnames';
+
 import Avatar from 'common/components/Avatar';
+import DisplayName from 'common/components/DisplayName';
 
 export default React.createClass({
 	displayName: 'Community:Head',
@@ -11,12 +17,27 @@ export default React.createClass({
 	},
 
 	render () {
-		let {entity} = this.props;
+		let {entity, narrow} = this.props;
+		let role = 'admin';
 
 		return (
 			<div className="community head">
-				<div className="avatar-wrapper">
-					<Avatar entity={entity}/>
+				<div className="coordinate-root">
+					<div className="avatar-wrapper">
+						<Avatar entity={entity}/>
+					</div>
+					{ narrow && (
+
+						<div className="meta">
+							<Link href="/info/" className="info-icon" />
+							<DisplayName entity={entity} tag="h1"/>
+							<ul>
+								<li>Community</li>
+								{role && ( <li className={cx('role', role)}>{role}</li> ) }
+							</ul>
+						</div>
+
+					) }
 				</div>
 			</div>
 		);
