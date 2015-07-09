5dcfbc4659bfab26e535de842324e1c0e6829a5b
Ray Hatfield <ray.hatfield@nextthought.com>
2015-07-09 17:22:03 -0500

    Cosmetic tweaks to profile cards.

--- src/main/js/profile/components/Activity.jsx
+++ src/main/js/profile/components/Activity.jsx
@@ -109,7 +109,7 @@ export default React.createClass({
 
 		return (
 			<ul className="profile-cards activity">
-				{canPost && <Card key="editor"><WriteSomething /></Card> }
+				{canPost && <Card key="editor" className="card-write-something"><WriteSomething /></Card> }
 				{store.length === 0 && <Card key='emptyList'><EmptyList type="activity"/></Card>}
 				{store.map((a, index) => {
 
