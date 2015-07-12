a2a64f843555e779c2888b31c9ce4eb744c60789
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-07-11 22:12:14 -0500

    support generating redirect paths for PDF/external content notes. Currently LibraryPath's from the server have an extra PageInfo at the end... we should probably stop returning that since the ContainerID of the external content note is the RelatedWorkRef.

--- src/main/js/object-resolver/resolvers/LibraryPath.js
+++ src/main/js/object-resolver/resolvers/LibraryPath.js
@@ -5,12 +5,33 @@ import {encodeForURI as encode} from 'nti.lib.interfaces/utils/ntiids';
 
 import {join} from 'path';
 
+const IGNORE = Symbol();
+
 const MIME_TYPES = {
 	'application/vnd.nextthought.courses.courseinstance': (o) => `/course/${encode(o.getID())}/`,
 	'application/vnd.nextthought.courses.courseoutlinecontentnode': (o) => `/lessons/${encode(o.getID())}/`,
-	'application/vnd.nextthought.relatedworkref': (o) => `/content/${encode(o.target)}/`,
+	'application/vnd.nextthought.relatedworkref': (o, prev, next) => {
+		let c;
+
+		if (next && /pageinfo$/i.test(next.MimeType)) {
+			if (!prev || prev.ContentNTIID !== next.NTIID) {
+				c = `/content/${encode(o.target)}/`;
+			} else {
+				next[IGNORE] = true;
+			}
+
+		}
+
+		if (!c) {
+			c = `/external-content/${encode(o.getID())}`;
+		}
+
+		return c;
+	},
 
 	'application/vnd.nextthought.pageinfo': (o, prev, next) => {
+		if (o[IGNORE]) { return ''; }
+
 		let c = `/content/${encode(o.getID())}`;
 		if (prev && /relatedworkref$/i.test(prev.MimeType)) {
 			c = encode(o.getID());
