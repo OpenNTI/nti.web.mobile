4d7d6a9f533754b8c274e7b7349d52d21538aaa4
Ray Hatfield <ray.hatfield@nextthought.com>
2015-07-12 01:56:06 -0500

    colored svg avatars.

--- src/main/js/common/components/Avatar.jsx
+++ src/main/js/common/components/Avatar.jsx
@@ -4,7 +4,10 @@ import {BLANK_AVATAR, BLANK_GROUP_AVATAR} from '../constants/DataURIs';
 import {resolve, getDebugUsernameString} from '../utils/user';
 
 const DEFAULT = { entity: {avatarURL: BLANK_AVATAR }};
-const GROUP_DEFAULT = { entity: {avatarURL: BLANK_GROUP_AVATAR }};
+// const GROUP_DEFAULT = { entity: {avatarURL: BLANK_GROUP_AVATAR }};
+
+import cx from 'classnames';
+
 
 const isGroup = RegExp.prototype.test.bind(/\.(friendslist|community)/i);
 
@@ -48,17 +51,15 @@ export default React.createClass({
 	render () {
 		let {entity} = this.state;
 		let {username, className} = this.props;
-		let css = className || '';
+		let css = cx('avatar', avatarColorClass(entity || username), className);
 
 		let {avatarURL, initials, displayName} = entity || {};
 
 		let props = Object.assign({}, this.props, {
 			'data-for': getDebugUsernameString(username),
 			alt: 'Avatar for ' + displayName,
-			className: `avatar ${css}`
+			className: css
 		});
-
-
 
 		return avatarURL ? (
 				<img {...props} src={avatarURL} onError={this.setUnknown}/>
@@ -72,7 +73,26 @@ export default React.createClass({
 	}
 });
 
+const NUM_COLORS = 13;
+function avatarColorClass(entity) {
+	let username = typeof entity === 'string' ? entity : (entity || {}).Username || 'unknown';
+	let idx = Math.abs(hash(username)) % NUM_COLORS; // % NextThought.util.Format.DEFAULT_AVATAR_BG_COLORS.length;
+	let cssClass = `avatar-color-${idx}`; // NextThought.util.Format.DEFAULT_AVATAR_BG_COLORS[idx];
+	return cssClass;
+}
 
+function hash(str) {
+	let h = 0, c;
+	if (str.length === 0) {
+		return h;
+	}
+	for (let i = 0; i < str.length; i++) {
+		c = str.charCodeAt(i);
+		h = ((h << 5) - h) + c;
+		h = h & h; // Convert to 32bit integer
+	}
+	return h;
+}
 
 function fillIn (cmp, props) {
 	let {entity} = props;
