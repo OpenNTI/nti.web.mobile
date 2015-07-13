bf9661c07003e3832058a61582fc5a09a0d704e9
Ray Hatfield <ray.hatfield@nextthought.com>
2015-07-13 01:28:09 -0500

    Start fleshing out follow as a PromiseButton.

--- src/main/js/profile/components/user/FollowButton.jsx
+++ src/main/js/profile/components/user/FollowButton.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
-import Button from 'common/forms/components/Button';
+import Loader from 'common/components/TinyLoader';
+import PromiseButton from 'common/components/PromiseButton';
+import {resolve} from 'common/utils/user';
 
 export default React.createClass({
 	displayName: 'FollowButton',
 
+	propTypes: {
+		username: React.PropTypes.string.isRequired
+	},
+
+	getInitialState () {
+		return {
+			busy: true
+		};
+	},
+
+	componentDidMount () {
+		this.getStatus();
+	},
+
+	getStatus () {
+		resolve({username: this.props.username})
+			.then(user => {
+				this.setState({
+					user: user,
+					busy: false
+				});
+			});
+	},
+
 	render () {
+
+		if (this.state.busy) {
+			return <Loader />;
+		}
+
 		return (
-			<Button className="follow-button">Follow</Button>
+			<PromiseButton className="follow-button" text="Follow" />
 		);
 	}
 });
