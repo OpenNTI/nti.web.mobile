02f2134f4084c3e54f862af9040766071a42faf5
Ray Hatfield <ray.hatfield@nextthought.com>
2015-07-21 14:28:03 -0500

    Added fixed-position img element to GradientBackground; removed functionality that added the image as a background to body.

--- src/main/js/common/components/GradientBackground.jsx
+++ src/main/js/common/components/GradientBackground.jsx
@@ -11,35 +11,13 @@ export default React.createClass({
 		children: React.PropTypes.any
 	},
 
-	componentDidMount () {
-		this.setBackground();
-	},
-
-	componentWillReceiveProps (nextProps) {
-		this.setBackground(nextProps);
-	},
-
-	componentWillUnmount () {
-		this.removeBackground();
-	},
-
-	setBackground (props=this.props) {
-		if (props.imgUrl) {
-			document.body.style.background = `url('${props.imgUrl}') 0px 100vh fixed`;
-		}
-		else {
-			this.removeBackground();
-		}
-	},
-
-	removeBackground () {
-		document.body.style.background = '';
-	},
-
 	render () {
-		let {className} = this.props;
+		let {className, imgUrl, children} = this.props;
 		return (
-			<div {...this.props} className={cx('gradient-bg', className)} />
+			<div {...this.props} className={cx('gradient-bg', className)}>
+				{imgUrl && <img src={imgUrl} className="gradient-bg-img"/>}
+				{children}
+			</div>
 		);
 	}
 });
