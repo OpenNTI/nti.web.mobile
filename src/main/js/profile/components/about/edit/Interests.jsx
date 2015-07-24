3bdc5b2ed94c60c471b229679da8c47c56fa40b2
Ray Hatfield <ray.hatfield@nextthought.com>
2015-07-24 16:37:28 -0500

    cosmetic and interaction tweaks to interest editing on user profile.

--- src/main/js/profile/components/about/edit/Interests.jsx
+++ src/main/js/profile/components/about/edit/Interests.jsx
@@ -17,7 +17,8 @@ export default React.createClass({
 		if (value.length > 0 && items.indexOf(value) === -1) {
 			items.push(value);
 			this.itemChanged();
-			// this.forceUpdate();
+			event.target.value = '';
+			event.target.focus();
 		}
 	},
 
@@ -28,7 +29,7 @@ export default React.createClass({
 		return (
 			<div className="interests-edit">
 				{items.map((item, index) => <div key={`interest-item-${index}`} className="string-item">{item}<b onClick={this.removeEntry.bind(this, index)} className="remove icon-bold-x" /></div>)}
-				<div className="string-input"><input onBlur={this.maybeAddItem}/></div>
+				<div className="string-item input"><input onBlur={this.maybeAddItem}/></div>
 			</div>
 		);
 	}
