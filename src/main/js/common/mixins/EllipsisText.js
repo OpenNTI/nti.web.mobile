3567dc3698a678642163b0d054da74eacdd57087
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-07-29 14:22:49 -0500

    share SharedExecution class

--- src/main/js/common/mixins/EllipsisText.js
+++ src/main/js/common/mixins/EllipsisText.js
@@ -1,70 +1,7 @@
 import React from 'react';
 import setTextContent from 'react/lib/setTextContent';
 
-/**
- * Goal: Group dom reads/writes into single passes.
- */
-class SharedExecution {
-	static isInterrupted (task) {
-		return task.skip || (task.parent && this.isInterrupted(task.parent));
-	}
-
-	static clear(t) { if(t) { t.skip = true; } }
-
-	static schedual(fn) {
-		let me = this.instance || (this.instance = new SharedExecution());
-
-		let task = {fn};
-
-		me.add(task);
-		me.start();
-
-		return task;
-	}
-
-
-	get () {
-		return (this.tasks || []).filter(x => !SharedExecution.isInterrupted(x) && !x.run);
-	}
-
-
-	add (task) {
-		this.tasks = this.get();
-		this.tasks.push(task);
-	}
-
-
-	start () {
-		if (!this.timeout) {
-			this.timeout = setTimeout(() => { this.stop(); this.run(); }, 1);
-		}
-	}
-
-	stop () {
-		if (this.timeout) {
-			clearTimeout(this.timeout);
-			delete this.timeout;
-		}
-	}
-
-
-	run () {
-		for (let task of this.get()) {
-			try {
-				let child = task.fn.call();
-				if (child) {
-					child.parent = task;
-				}
-			} catch(e) {
-				console.error(e);
-			}
-			task.run = true;
-		}
-
-		this.tasks = this.get();
-		console.debug('Ellipse Pass... remain: ', this.tasks.length);
-	}
-}
+import SharedExecution from '../utils/SharedExecution';
 
 
 
