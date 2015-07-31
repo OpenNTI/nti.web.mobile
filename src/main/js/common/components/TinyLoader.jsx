596247adf1d7df6989e856186742f01346d75925
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-07-31 11:05:31 -0500

    allow additional classes

--- src/main/js/common/components/TinyLoader.jsx
+++ src/main/js/common/components/TinyLoader.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import cx from 'classnames';
 
 export default React.createClass({
 	displayName: 'TinyLoader',
 
+	propTypes: {
+		className: React.PropTypes.string
+	},
+
 	render () {
 		return (
-			<ul className="tinyloader">
+			<ul className={cx('tinyloader', this.props.className)}>
 				<li /><li /><li />
 			</ul>
 		);
