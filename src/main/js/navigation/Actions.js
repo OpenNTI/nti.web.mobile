aeadf48ac02123bf5828f1761519732642855099
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-08-03 15:39:28 -0500

    setup a way to known when to block nav and prompt

--- src/main/js/navigation/Actions.js
+++ src/main/js/navigation/Actions.js
@@ -3,6 +3,9 @@ import AppDispatcher from 'dispatcher/AppDispatcher';
 import {
 	SET_CONTEXT,
 	SET_PAGE_SOURCE,
+
+	CLEAR_NAV_GUARDED,
+	SET_NAV_GUARDED
 } from './Constants';
 
 export function setContext (context) {
@@ -15,6 +18,17 @@ export function setPageSource (pageSource, currentPage, context) {
 	context.resolveContext().then(path=>
 		dispatch(SET_PAGE_SOURCE, {pageSource, currentPage, context, path}));
 }
+
+
+export function activateNavigationGuard (getMessageCallback) {
+	dispatch(SET_NAV_GUARDED, getMessageCallback);
+}
+
+
+export function deactivateNavigationGuard () {
+	dispatch(CLEAR_NAV_GUARDED);
+}
+
 
 function dispatch (key, data) {
 	AppDispatcher.handleRequestAction({type: key, response: data});
--- src/main/js/navigation/Constants.js
+++ src/main/js/navigation/Constants.js
@@ -1,2 +1,4 @@
 export const SET_CONTEXT = Symbol('set context');
 export const SET_PAGE_SOURCE = Symbol('set page source');
+export const CLEAR_NAV_GUARDED = Symbol('Unguard navigation');
+export const SET_NAV_GUARDED = Symbol('Setup guarded navigation');
