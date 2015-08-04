2068b71a4223ed934964d316e4ffcd66541d7864
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-08-04 12:38:38 -0500

    ShareWithList was broken, now its resolving names correctly.

--- src/main/js/common/components/SharedWithList.jsx
+++ src/main/js/common/components/SharedWithList.jsx
@@ -62,7 +62,7 @@ export default React.createClass({
 		let {sharedWith=[]} = item;
 		let pending = sharedWith
 			.filter(x => x && x !== EVERYONE)
-			.map(entity=> resolve(this, {entity}).catch(()=>null));
+			.map(entity=> resolve({entity}).catch(()=> null));
 
 
 		Promise.all(pending)
