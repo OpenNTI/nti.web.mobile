7931999085a96aaaac838b04b0a75c5ab7e0773b
Ray Hatfield <ray.hatfield@nextthought.com>
2015-08-12 13:47:35 -0500

    moved search related functionality out of the mixin and into the Users view.

--- src/main/js/contacts/mixins/Mixin.js
+++ src/main/js/contacts/mixins/Mixin.js
@@ -2,9 +2,6 @@ import React from 'react';
 import Api from '../Api';
 import Loading from 'common/components/Loading';
 import Err from 'common/components/Error';
-import {scoped} from 'common/locale';
-
-let t = scoped('CONTACTS');
 
 export default {
 
@@ -51,90 +48,6 @@ export default {
 			.then(store => this.setState({store}));
 	},
 
-	updateSearchQuery (event) {
-		let query = event ? event.target.value.trim() : '';
-		let {store} = this.state;
-
-		this.setState({
-			search: query,
-			searchLoading: query.length > 0,
-			searchResults: null,
-			searchError: null
-		});
-
-		if (store && store.search) {
-			let search = store.search(query);
-			search.catch(reason=> {
-				if (typeof reason !== 'object' || reason.statusCode !== -1) {
-					this.setState({
-						searchError: reason
-					});
-				}
-			});
-			search.then(results => {
-				// search results include communities and friends lists; we only want users
-				let users = results.filter((entity) => entity.isUser);
-				this.setState({
-					searchResults: users,
-					searchLoading: false
-				});
-			});
-		}
-	},
-
-	searchResults () {
-		let {searchLoading, searchError, searchResults} = this.state;
-		if (searchError) {
-			return <Err error={searchError} />;
-		}
-		if (searchLoading) {
-			return <Loading/>;
-		}
-		if (searchResults) {
-			return (
-				<div>
-					<h2>Search Results</h2>
-					<ul className="avatar-grid">
-					{
-						searchResults.map((entity) => this.renderListItem(entity))
-					}
-					</ul>
-				</div>
-			);
-		}
-	},
-
-	resultsSummary (items, searchResults) {
-		return searchResults && (
-			<div className="search-summary">
-				<div>
-					<div>Searching for {this.state.search}:</div>
-					<div>{t('filteredContacts', {count: items.length})}, {t('searchResults', {count: searchResults.length})}.</div>
-				</div>
-			</div>
-		);
-	},
-
-	clearSearch () {
-		this.updateSearchQuery();
-	},
-
-	searchField (items) {
-
-		let {search, searchResults} = this.state;
-
-		return this.hasSearch &&
-			(<div>
-				<div className="search-field">
-					<input type="search" ref="search" onChange={this.updateSearchQuery} value={search} />
-					<div className={'icon ' + (search.length > 0 ? 'clear-search-icon' : 'search-icon')} onClick={this.clearSearch} />
-				</div>
-				<div>
-					{this.resultsSummary(items, searchResults)}
-				</div>
-			</div>);
-	},
-
 	render () {
 
 		let {error, search, store} = this.state;
@@ -156,14 +69,12 @@ export default {
 
 		return (
 			<div>
-				{this.searchField(items)}
+				{this.beforeList && this.beforeList(items)}
 				<div>
 					{this.listName && <h2>{this.listName}</h2>}
 					<ul className={'contacts-list ' + this.props.listClassName}>{items}</ul>
 				</div>
-				<div className="search-results">
-					{this.searchResults()}
-				</div>
+				{this.afterList && this.afterList()}
 			</div>
 		);
 	}
