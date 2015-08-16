181a8b6751ed6ff4add458d9c2c42cfc4609443a
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-08-16 15:43:48 -0500

    make activity content prompts wider

--- src/main/js/catalog/components/ContentAquirePrompt.jsx
+++ src/main/js/catalog/components/ContentAquirePrompt.jsx
@@ -61,7 +61,7 @@ export default React.createClass({
 		let {length} = items;
 
 		return (
-			<div className="grid-container">
+			<div className="grid-container content-aquire-prompt">
 				{length === 1 ? (
 					<div>
 						<h3>You do not have access to this course.</h3>
