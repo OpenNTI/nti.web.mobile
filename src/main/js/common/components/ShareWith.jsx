647bccfc00850a912406e61ee0846165cf2645c1
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-08-20 22:51:20 -0500

    show value

--- src/main/js/common/components/ShareWith.jsx
+++ src/main/js/common/components/ShareWith.jsx
@@ -1,17 +1,55 @@
 import React from 'react';
 
+import DisplayName from './DisplayName';
+
+const KEY = 'defaultValue';
+
 export default React.createClass({
 	displayName: 'ShareWith',
 
+	propTypes: {
+		defaultValue: React.PropTypes.array,
 
-	componentDidMount () {
-		console.log(this.props);
+		scope: React.PropTypes.object
+	},
+
+
+	getInitialState () {
+		return {};
+	},
+
+
+	componentWillMount () {
+		this.setup();
+	},
+
+
+	componentWillReceiveProps (nextProps) {
+		if (nextProps[KEY] !== this.props[KEY]) {
+			this.setup(nextProps);
+		}
+	},
+
+
+	setup (props = this.props) {
+		const stillValid = () => props[KEY] === this.props[KEY];
+
+		this.setState({value: props.defaultValue});
+
+		props.scope.getSharingSuggestions()
+			.then(suggestions => {
+				if (stillValid()) {
+					this.setState({suggestions});
+				}
+			});
 	},
 
 
 	render () {
+		let {value = []} = this.state;
 		return (
 			<div>
+				{value.map(e => (<DisplayName key={e} entity={e}/>))}
 			</div>
 		);
 	}
