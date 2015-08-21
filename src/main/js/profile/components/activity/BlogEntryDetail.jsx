6060c16a6897c2e17eae04a9405d23e5a544c60c
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-08-21 00:06:28 -0500

    let the TopicView handle this... its got yo back.

--- src/main/js/profile/components/activity/BlogEntryDetail.jsx
+++ src/main/js/profile/components/activity/BlogEntryDetail.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import Loading from 'common/components/Loading';
 import TopicView from 'forums/components/TopicView';
-import {getObject} from 'forums/Api';
 
 export default React.createClass({
 	displayName: 'BlogEntryDetail',
@@ -10,37 +8,10 @@ export default React.createClass({
 		id: React.PropTypes.string.isRequired
 	},
 
-	getInitialState () {
-		return {
-			loading: true,
-			item: null
-		};
-	},
-
-	componentWillMount () {
-		this.getItem(this.props.id);
-	},
-
-	getItem (id) {
-		let get = getObject(id);
-		get.then(result => {
-			this.setState({
-				item: result,
-				loading: false
-			});
-		});
-	},
-
 	render () {
-
-		let {loading, item} = this.state;
-
-		if(loading) {
-			return <Loading />;
-		}
 		return (
 			<div className="profile-forums forums-wrapper">
-				<TopicView topicId={item.getID()} />
+				<TopicView topicId={this.props.id} />
 			</div>
 		);
 	}
