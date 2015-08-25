db43b141e2e15338e222213c4ed597fd4cc94e5b
Ray Hatfield <ray.hatfield@nextthought.com>
2015-08-25 11:23:40 -0500

    Restore remove/undo labels on distribution list detail.

--- src/main/js/contacts/components/ListDetail.jsx
+++ src/main/js/contacts/components/ListDetail.jsx
@@ -151,7 +151,7 @@ export default React.createClass({
 								:
 								<div>
 									<AddPeopleButton onClick={this.addPeople} />
-									<Selectables entities={(list.friends || []).slice()} onChange={this.toggleMembership} />
+									<Selectables entities={(list.friends || []).slice()} onChange={this.toggleMembership} labels={{selected: 'Remove', unselected: 'Undo'}}/>
 								</div>
 							}
 						</div>
