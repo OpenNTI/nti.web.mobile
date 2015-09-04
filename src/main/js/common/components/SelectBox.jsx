1556dcd890a1050b93b59c360a0e5cce03db896f
Ray Hatfield <ray.hatfield@nextthought.com>
2015-09-04 10:15:19 -0500

    make SelectBox work. needs paint.

--- src/main/js/common/components/SelectBox.jsx
+++ src/main/js/common/components/SelectBox.jsx
@@ -4,17 +4,64 @@ export default React.createClass({
 	displayName: 'SelectBox',
 
 	propTypes: {
-		options: React.PropTypes.array.isRequired
+		options: React.PropTypes.array.isRequired,
+		value: React.PropTypes.any,
+		onChange: React.PropTypes.func
 	},
 
-	onClick () {
+	getInitialState () {
+		return {
+			isOpen: false
+		};
+	},
 
+	componentWillMount () {
+		let {value} = this.props;
+		this.setSelected(value);
+	},
+
+	setSelected (value) {
+		let {options, onChange} = this.props;
+		let selectedOption = value ? options.find(option => option.value === value) : options[0];
+		this.setState({
+			selectedOption
+		});
+		if(onChange) {
+			onChange(value);
+		}
+	},
+
+	onClick (value) {
+		this.setSelected(value);
+		this.close();
+	},
+
+	open () {
+		this.setState({
+			isOpen: true
+		});
+	},
+
+	close () {
+		this.setState({
+			isOpen: false
+		});
 	},
 
 	render () {
+
+		let {isOpen, selectedOption} = this.state;
+
+		if (isOpen) {
+			return (
+				<ul className="select-box">
+					{this.props.options.map((option, index) => <li key={index} onClick={this.onClick.bind(this, option.value || option.label)}>{option.label}</li>)}
+				</ul>
+			);
+		}
 		return (
-			<ul>
-				{this.props.options.map((option, index) => <li key={index} onClick={this.onClick.bind(this, option.value || option.label)}>{option.label}</li>)}
+			<ul className="select-box">
+				<li onClick={this.open}>{selectedOption.label}</li>
 			</ul>
 		);
 	}
