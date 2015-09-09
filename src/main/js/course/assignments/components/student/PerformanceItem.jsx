f713c18e1e99591f6bc8face458a326256413c22
Ray Hatfield <ray.hatfield@nextthought.com>
2015-09-09 11:18:35 -0500

    bold the currently sorted column values

--- src/main/js/course/assignments/components/student/PerformanceItem.jsx
+++ src/main/js/course/assignments/components/student/PerformanceItem.jsx
@@ -9,28 +9,36 @@ export default React.createClass({
 	displayName: 'PerformanceItem',
 
 	propTypes: {
-		assignment: React.PropTypes.object.isRequired
+		assignment: React.PropTypes.object.isRequired,
+		sortedOn: React.PropTypes.string
 	},
 
 	render () {
 
-		let {assignment} = this.props;
+		let {assignment, sortedOn} = this.props;
 		let completed = assignment.hasLink('History');
 		let score = '';
 
 		let completedClasses = cx('completed', {
 			'yes': completed,
 			'no': !completed,
-			'icon-check': completed
+			'icon-check': completed,
+			'sorted': sortedOn === 'completed'
 		});
 
 		return (
 			<div className="performance-item">
-				<a href={`../${encodeForURI(assignment.getID())}/`}><div className="assignment-title">{assignment.title}</div></a>
-				<div className="assigned"><DateTime format={DATE_FORMAT} date={assignment.available_for_submission_beginning} /></div>
-				<div className="due"><DateTime format={DATE_FORMAT} date={assignment.available_for_submission_ending} /></div>
+				<a href={`../${encodeForURI(assignment.getID())}/`}>
+					<div className={cx('assignment-title', {'sorted': sortedOn === 'title'})}>{assignment.title}</div>
+				</a>
+				<div className={cx('assigned', {'sorted': sortedOn === 'available_for_submission_beginning'})}>
+					<DateTime format={DATE_FORMAT} date={assignment.available_for_submission_beginning} />
+				</div>
+				<div className={cx('due', {'sorted': sortedOn === 'available_for_submission_ending'})}>
+					<DateTime format={DATE_FORMAT} date={assignment.available_for_submission_ending} />
+				</div>
 				<div className={completedClasses}></div>
-				<div className="score">{score}</div>
+				<div className={cx('score', {'sorted': sortedOn === 'score'})}>{score}</div>
 			</div>
 		);
 	}
