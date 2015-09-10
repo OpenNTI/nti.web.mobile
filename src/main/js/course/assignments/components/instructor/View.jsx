7b2d8c83415ba5ddfe24c1c4808dbbcafb0b122f
Ray Hatfield <ray.hatfield@nextthought.com>
2015-09-10 10:53:31 -0500

    Move Tabs up for use by both student and instructor views.

--- src/main/js/course/assignments/components/instructor/View.jsx
+++ src/main/js/course/assignments/components/instructor/View.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Tabs from '../Tabs';
 
 export default React.createClass({
 	displayName: 'Assignments:Instructor:View',
 
 	render () {
 		return (
-			<div>(instructor)</div>
+			<div>
+				(instructor)
+				<Tabs className="assignments-nav button-group filters" />
+			</div>
 		);
 	}
 });
