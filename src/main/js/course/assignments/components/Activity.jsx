950f34094068342d3701762dc59258d163dfb92f
Ray Hatfield <ray.hatfield@nextthought.com>
2015-09-29 11:28:20 -0500

    stubbed out assignments activity view using course activity data

--- src/main/js/course/assignments/components/Activity.jsx
+++ src/main/js/course/assignments/components/Activity.jsx
@@ -1,11 +1,82 @@
 import React from 'react';
+import Loading from 'common/components/Loading';
+import ActivityBucket from 'course/components/ActivityBucket';
 
 export default React.createClass({
 	displayName: 'Activity',
 
+	propTypes: {
+		course: React.PropTypes.object.isRequired
+	},
+
+	getInitialState () {
+		return {
+		};
+	},
+
+	componentDidMount () {
+		this.setUp();
+	},
+
+	componentWillReceiveProps (nextProps) {
+		let {course, filterParams} = nextProps;
+
+		if(course !== this.props.course || filterParams !== nextProps.filterParams) {
+			this.setUp(nextProps);
+		}
+	},
+
+	componentWillUnmount () {
+		let {stream} = this.state;
+		if (stream) {
+			stream.removeListener('change', this.onStoreChange);
+		}
+	},
+
+
+	componentWillUpdate (_, nextState) {
+		let {stream} = this.state;
+		let nextStream = nextState.stream;
+
+		if (stream && stream !== nextStream) {
+			stream.removeListener('change', this.onStoreChange);
+		}
+
+		if (nextStream && nextStream !== stream) {
+			nextStream.addListener('change', this.onStoreChange);
+
+			if (!nextStream.loading) {
+				console.log('Wut?');
+			}
+		}
+	},
+
+	setUp (props = this.props) {
+		let {course} = props;
+		this.setState({
+			stream: course.getActivity()
+		});
+	},
+
+	onStoreChange () {
+		this.forceUpdate();
+	},
+
 	render () {
+
+		let {stream} = this.state;
+
+		if (!stream || stream.loading) {
+			return <Loading />;
+		}
+
 		return (
-			<div>Activity</div>
+			<div>
+				<div className="assignments-activity">
+					{stream && stream.map((bucket, index) => <ActivityBucket key={`bucket-${index}`} bucket={bucket} />)}
+				</div>
+			</div>
+
 		);
 	}
 });
