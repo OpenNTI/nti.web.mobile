ff17e08eec7f559e3f1cb74c84d6812b253d3e3f
Ray Hatfield <ray.hatfield@nextthought.com>
2015-10-01 16:34:39 -0500

    introduced ScrollTrigger component for triggering callbacks when reaching the bottom of the view

---
+++ src/main/js/common/components/ScrollTrigger.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+
+export default React.createClass({
+	displayName: 'ScrollTrigger',
+
+	propTypes: {
+		onEnterView: React.PropTypes.func,
+		children: React.PropTypes.any
+	},
+
+	inView () {
+		return isElementInViewport(React.findDOMNode(this));
+	},
+
+	componentDidMount () {
+		window.addEventListener('scroll', this.onScroll);
+		if (this.inView()) {
+			this.onEnterView();
+		}
+	},
+
+	componentWillUnmount () {
+		window.removeEventListener('scroll', this.onScroll);
+	},
+
+	onScroll () {
+		this.checkInView();
+	},
+
+	checkInView () {
+		let {inView} = this.state;
+		let newInView = this.inView();
+		if (inView !== newInView) {
+			let handler = newInView ? this.onEnterView : this.onLeaveView;
+			handler();
+		}
+	},
+
+	onLeaveView () {
+		console.debug('leave view');
+		this.setState({
+			inView: false
+		});
+	},
+
+	onEnterView () {
+		console.debug('enter view');
+		if (typeof this.props.onEnterView === 'function') {
+			this.props.onEnterView();
+		}
+		this.setState({
+			inView: true
+		});
+	},
+
+	render () {
+		return (
+			<div className="scrollTrigger">{this.props.children}</div>
+		);
+	}
+});
+
+function isElementInViewport (el) {
+
+	let rect = el.getBoundingClientRect();
+
+	return (
+		rect.top >= 0 &&
+		rect.left >= 0 &&
+		Math.floor(rect.bottom) <= (window.innerHeight || document.documentElement.clientHeight) &&
+		rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
