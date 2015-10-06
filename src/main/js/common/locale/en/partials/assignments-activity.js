e9e96c27edf770fa6dc5d752278d2588d224fccc
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-10-06 15:30:24 -0500

    fix string interpolation variable

--- src/main/js/common/locale/en/partials/assignments-activity.js
+++ src/main/js/common/locale/en/partials/assignments-activity.js
@@ -3,6 +3,6 @@ export default {
 	'late-assignment': 'Assignment Past Due:',
 	'new-assignment': 'New Assignment:',
 	'submitted-assignment': 'Assignment Submitted:',
-	'they-feedback': '{name} left feedback on',
-	'you-feedback': 'You commented on'
+	'they-feedback': '%(name)s left feedback on:',
+	'you-feedback': 'You commented on:'
 };
