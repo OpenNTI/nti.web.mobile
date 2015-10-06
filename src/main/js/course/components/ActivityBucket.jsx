db0b9f1c51ce1ce680a1455188d0e98569c2a117
Ray Hatfield <ray.hatfield@nextthought.com>
2015-10-06 09:02:03 -0500

    Format activity bucket dates

--- src/main/js/course/components/ActivityBucket.jsx
+++ src/main/js/course/components/ActivityBucket.jsx
@@ -3,6 +3,8 @@ import hasItems from 'profile/components/activity/HasItems';
 import Card from 'profile/components/Card';
 import DateTime from 'common/components/DateTime';
 
+const startDateFormat = 'MMMM D';
+
 export default React.createClass({
 	displayName: 'Course:ActivityBucket',
 	mixins: [hasItems],
@@ -13,9 +15,10 @@ export default React.createClass({
 	render () {
 
 		let {bucket} = this.props;
+		let endDateFormat = bucket.start.getMonth() === bucket.end.getMonth() ? 'D' : startDateFormat;
 		return (
 			<Card>
-				<div className="header"><DateTime date={bucket.start}/> - <DateTime date={bucket.end} /></div>
+				<div className="header"><DateTime date={bucket.start} format={startDateFormat} /> — <DateTime date={bucket.end} format={endDateFormat} /></div>
 				{this.renderItems(bucket)}
 			</Card>
 		);
