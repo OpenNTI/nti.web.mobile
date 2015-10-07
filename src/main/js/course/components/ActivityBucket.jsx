dd9b0f86c278eb80262268f24ed381b185374ea0
Ray Hatfield <ray.hatfield@nextthought.com>
2015-10-07 16:46:11 -0500

    added react keys

--- src/main/js/course/components/ActivityBucket.jsx
+++ src/main/js/course/components/ActivityBucket.jsx
@@ -140,9 +140,9 @@ export default React.createClass({
 		return (
 			<Card>
 				<div className="header"><DateTime date={bucket.start} format={startDateFormat} /> - <DateTime date={bucket.end} format={endDateFormat} /></div>
-				{columns.map(col => (
-					<div className="col">
-						{col.map(items => <div className="bucketed-items">{this.renderItems(items)}</div>)}
+				{columns.map((col, index) => (
+					<div key={`col-${index}`} className="col">
+						{col.map((items, idx) => <div key={`item-${idx}`} className="bucketed-items">{this.renderItems(items)}</div>)}
 					</div>
 				) )}
 				{/*<div className="bucketed-items">{this.renderItems(bucket, {className: 'bucketed-item'})}</div>*/}
