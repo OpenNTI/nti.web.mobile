3e388e409ed5a53f0e900ba64e5bd9bb6b68bd44
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-10-07 17:48:22 -0500

    checkpoint ...rendering a graph

--- src/main/js/assessment/components/aggregated/part-types/Matching.jsx
+++ src/main/js/assessment/components/aggregated/part-types/Matching.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import {BarChart} from 'react-d3-components';
+
 import CommonParts from './CommonParts';
 
 export default React.createClass({
@@ -13,9 +15,49 @@ export default React.createClass({
 	},
 
 	render () {
+
+		let data = [
+			{
+				label: 'somethingA',
+				values: [
+					{x: 'SomethingA', y: 10},
+					{x: 'SomethingB', y: 4},
+					{x: 'SomethingC', y: 3}
+				]
+			},
+			{
+				label: 'somethingB',
+				values: [
+					{x: 'SomethingA', y: 6},
+					{x: 'SomethingB', y: 8},
+					{x: 'SomethingC', y: 5}
+				]
+			},
+			{
+				label: 'somethingC',
+				values: [
+					{x: 'SomethingA', y: 6},
+					{x: 'SomethingB', y: 8},
+					{x: 'SomethingC', y: 5}
+				]
+			}
+		];
+
+		const tooltip = (x, y0, y/*, total*/) => y.toString();
+
+
+
+
 		return (
 			<div>
-				Matching
+			<BarChart
+				data={data}
+				width={400}
+				height={400}
+				margin={{top: 10, bottom: 50, left: 50, right: 10}}
+				tooltipHtml={tooltip}
+				tooltipMode={'element'}
+				/>
 			</div>
 		);
 	}
