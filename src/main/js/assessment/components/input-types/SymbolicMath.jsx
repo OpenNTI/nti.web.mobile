ed298d7328b55f3e28f5556d7076371ffc651f62
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-10-15 11:50:23 -0500

    ensure mathquill is loaded when a symbolic math question input part mounts

--- src/main/js/assessment/components/input-types/SymbolicMath.jsx
+++ src/main/js/assessment/components/input-types/SymbolicMath.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import Mixin from './Mixin';
 
 import isEmpty from 'fbjs/lib/isEmpty';
+
+import Loading from 'common/components/TinyLoader';
+import Error from 'common/components/Error';
+import ExternalLibraryManager from 'common/mixins/ExternalLibraryManager';
+import {clearLoadingFlag, setError} from 'common/utils/react-state';
+
+import Mixin from './Mixin';
+
 
 /**
 * This input type represents Symbolic Math
 */
 export default React.createClass({
 	displayName: 'SymbolicMath',
-	mixins: [Mixin],
+	mixins: [Mixin, ExternalLibraryManager],
 
 	statics: {
 		inputType: [
@@ -16,10 +23,28 @@ export default React.createClass({
 		]
 	},
 
-	render () {
+	componentWillMount () {
+		this.setState({ loading:true });
+	},
 
-		let value = this.state.value;
-		let submitted = this.isSubmitted();
+	componentDidMount () {
+		this.ensureExternalLibrary('mathquill')
+			.then(()=> clearLoadingFlag(this))
+			.catch(e => setError(this, e));
+	},
+
+	render () {
+		const {state: {value, loading, error}} = this;
+		const submitted = this.isSubmitted();
+
+		if (loading) {
+			return ( <Loading/> );
+		}
+
+		if (error) {
+			return ( <Error error="There was an error loading this question."/> );
+		}
+
 
 		return (
 			<form className="free-response">
