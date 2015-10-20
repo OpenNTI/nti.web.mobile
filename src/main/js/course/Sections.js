e42e74a556ab277dafb33b076e6d109e016700eb
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-10-20 15:35:04 -0500

    put course activity access behind "course-activity" flag

--- src/main/js/course/Sections.js
+++ src/main/js/course/Sections.js
@@ -1,8 +1,14 @@
-export default {
-	// ACTIVITY: 'activity/',
-	ASSIGNMENTS: 'assignments/',
-	DISCUSSIONS: 'discussions/',
-	LESSONS: 'lessons/',
-	INFO: 'info',
-	VIDEOS: 'videos/'
-};
+import {isFlag} from 'common/utils';
+
+const SECTIONS = Object.assign({},
+	isFlag('course-activity') ? {ACTIVITY: 'activity/'} : {},
+	{
+		// ACTIVITY: 'activity/',
+		ASSIGNMENTS: 'assignments/',
+		DISCUSSIONS: 'discussions/',
+		LESSONS: 'lessons/',
+		INFO: 'info',
+		VIDEOS: 'videos/'
+	});
+
+export default SECTIONS;
