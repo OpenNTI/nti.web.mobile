9f09bcfba5e7ab16114491bc85ec31dc78693134
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-10-21 18:16:58 -0500

    add failing test suite for content viewer... wip: defining the tests.

---
+++ src/main/js/content/components/__test__/Viewer.spec.js
@@ -0,0 +1,20 @@
+
+fdescribe('Content Viewer', ()=> {
+
+	describe('Router Behavior', () => {
+
+		it('should understand special routes', () => {
+			expect(false).toBeTruthy();
+		});
+
+	});
+
+	describe('Context / Pager', () => {
+
+		it('should have the correct context', () => {
+			expect(false).toBeTruthy();
+		});
+
+	});
+
+});
