80b823ff2ff6fa43f4596dfa2708beb734b22d42
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-10-23 18:02:12 -0500

    disable the viewer spec for now.

--- src/main/js/content/components/__test__/Viewer.spec.js
+++ src/main/js/content/components/__test__/Viewer.spec.js
@@ -1,10 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Locations, Location} from 'react-router-component';
 
-describe('Content Viewer', ()=> {
+import Viewer from '../Viewer';
+
+xdescribe('Content Viewer', ()=> {
+	let container;
+
+	beforeEach(()=> {
+		container = document.createElement('div');
+		container.id = 'content';
+		document.body.appendChild(container);
+	});
+
+	afterEach(()=> {
+		ReactDOM.unmountComponentAtNode(container);
+		document.body.removeChild(container);
+	});
+
+	function renderReaderAtPage (page) {
+		ReactDOM.render(
+			<Locations path={page}>
+				<Location path="/:rootId" handler={Viewer}/>
+			</Locations>,
+			container
+		);
+	}
 
 	describe('Router Behavior', () => {
 
 		it('should understand special routes', () => {
-			expect(false).toBeTruthy();
+
+			renderReaderAtPage('/foo');
+
 		});
 
 	});
@@ -12,7 +40,7 @@ describe('Content Viewer', ()=> {
 	describe('Context / Pager', () => {
 
 		it('should have the correct context', () => {
-			expect(false).toBeTruthy();
+
 		});
 
 	});
