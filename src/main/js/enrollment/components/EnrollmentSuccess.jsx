2e7479782b36e42bf3e1a0ccd5e9a6ec1b19633c
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-11-05 16:42:48 -0600

    render the thankyou iframe for normal enrollments.

--- src/main/js/enrollment/components/EnrollmentSuccess.jsx
+++ src/main/js/enrollment/components/EnrollmentSuccess.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
 
+import Loading from 'common/components/Loading';
 import BasePathAware from 'common/mixins/BasePath';
+
+import LibraryAccessor from 'library/mixins/LibraryAccessor';
+
 
 export default React.createClass({
 	displayName: 'EnrollmentSuccess',
-	mixins: [BasePathAware],
+	//The LibraryAccessor mixin gives us the 'getLibrary' method.
+	mixins: [BasePathAware, LibraryAccessor],
 
 	propTypes: {
 		courseTitle: React.PropTypes.string
 	},
 
 	render () {
-		let basePath = this.getBasePath();
-		let {courseTitle} = this.props;
-		let verbiage = 'Go to my courses';
-		let href = basePath + 'library/courses/';
+		const {props: {courseTitle}, state: {loading}} = this;
 
-		console.log(this.props);
-		const thankYouURL = null;
+		const verbiage = 'Go to my courses';
+		const href = this.getBasePath() + 'library/courses/';
+		const library = this.getLibrary();
+
+		//If the library is loading, or reloading this will be true.
+		if (loading) {
+			return ( <Loading/> );
+		}
+
+		//We DEPEND on the library being reloaded by the enrollment proccess.
+		//If the library has not be triggered to reload by the time this
+		//component is mounted/rendered, then the getLastEnrolledCourse will
+		//return the WRONG value.
+
+		const {VendorThankYouPage: {thankYouURL} = {}} = library.getLastEnrolledCourse();
 
 		return (
 			<div className="enrollment-success">
