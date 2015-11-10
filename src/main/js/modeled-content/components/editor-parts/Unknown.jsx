ea4c7b98f3821259e5a2c32186f65a0a356862d8
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-11-10 15:55:00 -0600

    normalize body-divider class. add Unknown

---
+++ src/main/js/modeled-content/components/editor-parts/Unknown.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+const UnknownThumbnail = React.createClass({
+	displayName: 'UnknownThumbnail',
+
+	statics: {
+
+		renderIcon (data) {
+			return Promise.resolve(
+				ReactDOMServer.renderToStaticMarkup(
+					React.createElement(UnknownThumbnail, { data })));
+		}
+
+	},
+
+	propTypes: {
+		data: React.PropTypes.object.isRequired
+	},
+
+	render () {
+		let {data} = this.props;
+
+		data = JSON.stringify(data) || '';
+
+		return (
+			<object contentEditable={false} className="body-divider unknown" unselectable="on">
+				<div className="unknown-icon" unselectable="on">
+					<div className="fill" unselectable="on"/>
+					<script type="application/json" dangerouslySetInnerHTML={{__html: data}}/>
+				</div>
+			</object>
+		);
+	}
+});
+
+export default UnknownThumbnail;
