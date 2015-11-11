d5441fc092a7261d9037f2668363a43982b90da2
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-11-11 11:10:11 -0600

    cleanup store-enrollment actions syntax.

--- src/main/js/enrollment/store-enrollment/Actions.js
+++ src/main/js/enrollment/store-enrollment/Actions.js
@@ -1,5 +1,13 @@
 import AppDispatcher from 'dispatcher/AppDispatcher';
-import * as Constants from './Constants';
+import {
+	EDIT,
+	RESET,
+	UPDATE_COUPON,
+	GIFT_PURCHASE_DONE,
+	SUBMIT_STRIPE_PAYMENT,
+	VERIFY_BILLING_INFO
+} from './Constants';
+
 
 function dispatch (type, payload) {
 	AppDispatcher.handleRequestAction({type, payload});
@@ -9,30 +17,32 @@ function dispatch (type, payload) {
 //FIXME: This is not how to do "Actions".  Actions should DO the work and dispatch results.
 
 export function verifyBillingInfo (stripePublicKey, formData) {
-	dispatch(Constants.VERIFY_BILLING_INFO, { stripePublicKey, formData });
+	dispatch(VERIFY_BILLING_INFO, { stripePublicKey, formData });
 }
 
 
 export function submitPayment (formData) {
-	dispatch(Constants.SUBMIT_STRIPE_PAYMENT, {formData});
+	dispatch(SUBMIT_STRIPE_PAYMENT, {formData});
 }
+
 
 //XXX: Doesn't seem to be referenced.
 export function giftPurchaseDone () {
-	dispatch(Constants.GIFT_PURCHASE_DONE);
+	console.error('Called??');
+	dispatch(GIFT_PURCHASE_DONE);
 }
 
 
 export function updateCoupon (purchasable, coupon) {
-	dispatch(Constants.UPDATE_COUPON, {purchasable, coupon});
+	dispatch(UPDATE_COUPON, {purchasable, coupon});
 }
 
 
 export function resetProcess (options) {
-	dispatch(Constants.RESET, { options: options });
+	dispatch(RESET, { options });
 }
 
 
 export function edit (mode) {
-	dispatch(Constants.EDIT, {mode});
+	dispatch(EDIT, {mode});
 }
