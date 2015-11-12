a467d74ad7eb88c88a04d62e15e6b42a8bcc3d57
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-11-12 13:28:53 -0600

    oops

--- karma.conf.js
+++ karma.conf.js
@@ -100,7 +100,7 @@ module.exports = function (config) {
 		// - Safari (only Mac)
 		// - PhantomJS
 		// - IE (only Windows)
-		browsers: ['Chrome'],
+		browsers: ['PhantomJS'],
 
 		// other possible values: 'dots', 'progress', 'junit', 'html', 'coverage'
 		reporters: ['mocha'],
