24520a6f600a4ecf90104247347ca4a547870232
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-11-17 18:04:31 -0600

    use dynamic imports pattern here

--- src/main/js/notifications/components/kinds/index.js
+++ src/main/js/notifications/components/kinds/index.js
@@ -1,38 +1,9 @@
 import React from 'react';
 
-import Unknown from './Unknown';
-import Chat from './Chat';
-import ChatGroup from './ChatGroup';
-import Contact from './Contact';
-import Badge from './Badge';
-import Grade from './Grade';
-import Feedback from './Feedback';
-import Note from './Note';
-import BlogEntry from './BlogEntry';
-import BlogEntryPost from './BlogEntryPost';
-import BlogComment from './BlogComment';
-import ForumComment from './ForumComment';
-import ForumTopic from './ForumTopic';
-
-const KINDS = [
-	Unknown,
-
-	Chat,
-	ChatGroup,
-	Contact,
-	Badge,
-	Grade,
-	Feedback,
-
-	Note,
-
-	BlogEntry,
-	BlogEntryPost,
-	BlogComment,
-
-	ForumTopic,
-	ForumComment
-];
+//`require.context` is a little WebPack magic :)
+const req = require.context('./', false, /.jsx$/);
+const Unknown = req('./Unknown.jsx');
+const KINDS = req.keys().map(req);
 
 export function getNotificationItem (item, index) {
 	let Item = Unknown;
