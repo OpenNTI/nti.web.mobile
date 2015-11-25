b9b44de186759c79cdf9ca09e770228aff3d96bf
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-11-25 11:15:18 -0600

    unreferenced, but fix exports anyways

--- src/main/js/common/constants/HttpStatusCodes.js
+++ src/main/js/common/constants/HttpStatusCodes.js
@@ -3,40 +3,39 @@
  * @class HTTP_STATUS_CODES
  */
 
-export default {
-	/**
-	* @property NO_CONTENT (204)
-	* @static
-	* @final
-	*/
-	NO_CONTENT: 204,
 
-	/**
-	* IE8 mangles 204 responses from ajax requests into 1223.
-	* @property NO_CONTENT_IE8 (1223)
-	* @static
-	* @final
-	*/
-	NO_CONTENT_IE8: 1223,
+/**
+ * @property NO_CONTENT (204)
+ * @static
+ * @final
+ */
+export const NO_CONTENT = 204;
 
-	/**
-	* @property UNAUTHORIZED (401)
-	* @static
-	* @final
-	*/
-	UNAUTHORIZED: 401,
+/**
+ * IE8 mangles 204 responses from ajax requests into 1223.
+ * @property NO_CONTENT_IE8 (1223)
+ * @static
+ * @final
+ */
+export const NO_CONTENT_IE8 = 1223;
 
-	/**
-	* @property NOT_FOUND (404)
-	* @static
-	* @final
-	*/
-	NOT_FOUND: 404,
+/**
+ * @property UNAUTHORIZED (401)
+ * @static
+ * @final
+ */
+export const UNAUTHORIZED = 401;
 
-	/**
-	* @property SERVER_ERROR (500)
-	* @static
-	* @final
-	*/
-	SERVER_ERROR: 500
-};
+/**
+ * @property NOT_FOUND (404)
+ * @static
+ * @final
+ */
+export const NOT_FOUND = 404;
+
+/**
+ * @property SERVER_ERROR (500)
+ * @static
+ * @final
+ */
+export const SERVER_ERROR = 500;
