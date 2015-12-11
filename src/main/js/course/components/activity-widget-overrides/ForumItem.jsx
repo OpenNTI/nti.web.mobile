4674ff544db83f8df9dc5df497016a9ffc3eaee8
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-12-11 13:47:11 -0600

    show the forum item for course activity

--- src/main/js/course/components/activity-widget-overrides/ForumItem.jsx
+++ src/main/js/course/components/activity-widget-overrides/ForumItem.jsx
@@ -1,18 +1,43 @@
 import React from 'react';
 
+import Breadcrumb from 'common/components/BreadcrumbPath';
+import LuckyCharms from 'common/components/LuckyCharms';
+
+import TopicHeadline from 'forums/components/TopicHeadline';
+// import ActionsComp from 'forums/components/Actions';
+
+
+const PREFIX = [];
+
 export default React.createClass({
 	displayName: 'ForumItem',
 
 	statics: {
 		handles (item) {
-			// const {MimeType = ''} = item;
-			// return /topic$/i.test(MimeType);
+			const {MimeType = ''} = item;
+			return /topic$/i.test(MimeType);
 		}
 	},
 
+
+	propTypes: {
+		item: React.PropTypes.any.isRequired
+	},
+
+
 	render () {
+		const {props: {item}} = this;
+
 		return (
-			<div>ForumItem</div>
+			<div>
+				<Breadcrumb item={item} breadcrumb={PREFIX} splicePaths={1}/>
+				<div className="body">
+					<LuckyCharms item={item} />
+					<TopicHeadline item={item.headline || item} />
+				</div>
+				{/*<ActionsComp item={item} /> */}
+			</div>
 		);
+
 	}
 });
