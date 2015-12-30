309083b5296283cfe9b5c2cf1fc5b5ba507bf759
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-12-30 16:55:33 -0600

    NTI-102 - Working on input UX

--- src/main/js/assignments/components/instructor/GradeBox.jsx
+++ src/main/js/assignments/components/instructor/GradeBox.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cx from 'classnames';
 
 import {PropType as NTIID} from 'nti-lib-interfaces/lib/utils/ntiids';
 
@@ -15,49 +16,105 @@ export default React.createClass({
 
 		userId: React.PropTypes.string.isRequired,
 
-		assignmentId: NTIID
+		assignmentId: NTIID.isRequired
 	},
 
+
+	componentWillMount () {
+		this.onItemChanged();
+	},
+
+
+	componentWillReceiveProps (nextProps) {
+		const {grade: newGrade} = nextProps;
+		const {grade: oldGrade} = this.props;
+
+		if (!oldGrade && newGrade) {
+			console.log('Got new Grade!', nextProps.grade.value);
+		}
+
+		if (oldGrade !== newGrade) {
+			this.onItemChanged(nextProps);
+		}
+	},
+
+
+	getInitialState () {
+		return {
+			value: ''
+		};
+	},
+
+
 	getItem (props = this.props) { return props.grade; },
+
+
+	onItemChanged (props = this.props) {
+		const {value = ''} = props.grade || {};
+		this.setState({value});
+	},
+
 
 	onFocus (e) {
 		e.target.select();
 	},
 
+
 	onBlur (e) {
 		const value = e.target.value.trim();
-		const grade = this.getItem() || {value: ''};
-
-		if (grade.value !== value) {
-			this.gradeChanged(value);
-		}
+		this.maybeSetGrade(value);
 	},
 
-	componentWillReceiveProps (nextProps) {
-		if (!this.props.grade && nextProps.grade) {
-			console.log('Got new Grade!', nextProps.grade.value);
+
+	onChange (e) {
+		const value = e.target.value.trim();
+
+		if (this.state.busy) {
+			e.preventDefault();
+			e.stopPropagation();
+			return;
 		}
+
+		this.setState({value});
+
+		clearTimeout(this.timerGradeChanging);
+		this.timerGradeChanging = setTimeout(()=> this.maybeSetGrade(value), 500);
 	},
 
-	gradeChanged (newValue) {
+
+	onKeyDown (e) {
+		console.log(e.key);
+	},
+
+
+	maybeSetGrade (newValue) {
 		const {assignmentId, userId, grade} = this.props;
 		const collection = this.getAssignments();
+		const currentValue = grade && grade.value;
 
+		if (currentValue === newValue || (!currentValue && !newValue)) {
+			return;
+		}
 
+		this.setState({busy: true});
 		collection.setGrade(grade || assignmentId, userId, newValue)
 			.then(
 				( ) => console.log('Success'),
 				(e) => console.error( e ? (e.stack || e.message || e) : 'Error')
-			);
+			)
+			.then(()=> this.setState({busy: false}));
 	},
 
+
 	render () {
-		const {value} = this.getItem() || {};
+		const {busy, value} = this.state;
 		return (
-			<input className="grade-box"
-					defaultValue={value}
-					onBlur={this.onBlur}
-					onFocus={this.onFocus}
+			<input className={cx('grade-box', {busy})}
+				value={value}
+				onBlur={this.onBlur}
+				onChange={this.onChange}
+				onFocus={this.onFocus}
+				onKeyDown={this.onKeyDown}
 				/>
 		);
 	}
