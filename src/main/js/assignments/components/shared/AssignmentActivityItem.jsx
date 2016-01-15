4aed06ee48810a9a7b7b7c8e67b8a5e30d7eacba
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-01-15 15:17:19 -0600

    NTI-121 - wip

--- src/main/js/assignments/components/shared/AssignmentActivityItem.jsx
+++ src/main/js/assignments/components/shared/AssignmentActivityItem.jsx
@@ -14,6 +14,10 @@ import {join} from 'path';
 const scope = 'COURSE.ASSIGNMENTS.ACTIVITY';
 const t = scoped(scope);
 
+const hasName = RegExp.prototype.test.bind(/^(user|they)/i);
+
+const linkToStudentView = (user, type) => user && hasName(type);
+
 export default React.createClass({
 	displayName: 'ActivityItem',
 
@@ -25,19 +29,22 @@ export default React.createClass({
 		const {event} = this.props;
 		const {date, title, type, suffix, unread, user, assignment} = event;
 		const today = new Date((new Date()).setHours(0, 0, 0, 0));
-		const hasName = type === 'they-feedback';
 
 		let format = 'MMM D'; // "Jan 2" ... Short month, Day of month without zero padding
 		if (date > today) {
 			format = 'h:mm a'; // "8:05 pm" ...Hours without zero padding, ":", minutes with zero padding, lower-case "am/pm"
 		}
 
-		const href = join('..', encodeForURI(assignment.getID()));
+		//TODO: Limit linkToStudentView to only apply IF instructor view...
+		//feedback events look the same to both student and instructors... so this breaks feedback for students.
+		const href = linkToStudentView(user, type)
+			? join('..', encodeForURI(assignment.getID()), 'students', encodeURIComponent(user))
+			: join('..', encodeForURI(assignment.getID()));
 
 		return (
 			<div className={cx('item', {unread})}>
 				<DateTime date={date} format={format}/>
-				{hasName ? (
+				{hasName(type) ? (
 					<DisplayName entity={user} usePronoun localeKey={`${scope}.${type}`}/>
 				) : (
 					<span className="type">{t(type)}</span>
