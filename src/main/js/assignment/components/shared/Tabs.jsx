899652ac5ad81e05ac32eb9f5020c6e360600c43
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-02-18 12:06:53 -0600

    NTI-321 - Disable Grades & Performance tab... ...IFF the course is "Admin" but does not have a GradeBook.

--- src/main/js/assignment/components/shared/Tabs.jsx
+++ src/main/js/assignment/components/shared/Tabs.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
+
 import ActiveState from 'common/components/ActiveState';
 import ActiveStateSelector from 'common/mixins/ActiveStateSelector';
 import Header from 'common/components/TopicHeader';
 
+import Accessor from '../../mixins/AssignmentCollectionAccessor';
+
 export default React.createClass({
 	displayName: 'Tabs',
-	mixins: [ActiveStateSelector],
+	mixins: [Accessor, ActiveStateSelector],
+
+	propTypes: {
+		course: React.PropTypes.object.isRequired
+	},
 
 	render () {
+		const course = this.getCourse();
+
+		const disablePerfView = course.isAdministrative && !course.GradeBook;
+
+		const PerformanceViewTag = disablePerfView ? 'a' : ActiveState;
+		const PerformanceViewProps = disablePerfView ? {className: 'disabled'} : {tag: 'a', href:'/performance/', hasChildren: true};
+
 		return (
 			<div className="assignments-nav">
 				<Header>Assignments</Header>
 				<ul className="filters">
 					<li><ActiveState tag="a" href="/" hasChildren>Assignments</ActiveState></li>
-					<li><ActiveState tag="a" href="/performance/" hasChildren>Grades &amp; Performance</ActiveState></li>
+					<li><PerformanceViewTag {...PerformanceViewProps}>Grades &amp; Performance</PerformanceViewTag></li>
 					<li><ActiveState tag="a" href="/activity/">Activity</ActiveState></li>
 				</ul>
 			</div>
