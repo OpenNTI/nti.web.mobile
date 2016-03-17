76b507c5c1952470de76a9f1a242dbe61f02578e
Ray Hatfield <ray.hatfield@nextthought.com>
2016-03-17 14:46:38 -0500

    Use passed-in props, not this.props

--- src/main/js/profile/components/group/Controls.jsx
+++ src/main/js/profile/components/group/Controls.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 import MembershipButton from './MembershipButton';
 
-export default function GroupControls () {
+export default function GroupControls (props) {
 	return (
 		<ul>
 			<li>
 				<a className="gear-button"/>
 				<ul className="menu"/>
 			</li>
-			<li><MembershipButton {...this.props} /></li>
+			<li><MembershipButton {...props} /></li>
 		</ul>
 	);
 }
