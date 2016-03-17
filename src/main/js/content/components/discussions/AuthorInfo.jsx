240a251413b47c5c87e9571d684046daeb80636c
Ray Hatfield <ray.hatfield@nextthought.com>
2016-03-17 14:47:20 -0500

    rewrite as function component

--- src/main/js/content/components/discussions/AuthorInfo.jsx
+++ src/main/js/content/components/discussions/AuthorInfo.jsx
@@ -7,40 +7,35 @@ import LuckyCharms from 'common/components/LuckyCharms';
 import RepliedTo from 'common/components/RepliedTo';
 import SharedWithList from 'common/components/SharedWithList';
 
-export default React.createClass({
-	displayName: 'AuthorInfo',
+export default function AuthorInfo ({item, lite}) {
+	const {creator, title} = item;
+	const date = item.getCreatedTime();
+	const isReply = item.isReply();
 
-	propTypes: {
-		item: React.PropTypes.object.isRequired,
-		lite: React.PropTypes.bool
-	},
-
-	render () {
-		const {props: {item, lite}} = this;
-		const {creator, title} = item;
-		const date = item.getCreatedTime();
-		const isReply = item.isReply();
-
-		return (
-			<div className="author-info">
-				<Avatar entity={creator}/>
-				<div className="meta">
-					<LuckyCharms item={item}/>
-					{isReply ? null : ( <h1 className="title">{title}</h1> )}
-					{isReply ? (
-						<div className="reply-name-wrapper">
-							{lite ? (<DisplayName entity={creator}/>) : (<RepliedTo item={item}/>)}
-							<DateTime date={date} relative/>
-						</div>
-					) : (
-						<div className="name-wrapper">
-							<DisplayName entity={creator} localeKey={lite ? void 0 : 'DISCUSSIONS.postedBy'}/>
-							<DateTime date={date} relative/>
-							<SharedWithList item={item}/>
-						</div>
-					)}
-				</div>
+	return (
+		<div className="author-info">
+			<Avatar entity={creator}/>
+			<div className="meta">
+				<LuckyCharms item={item}/>
+				{isReply ? null : ( <h1 className="title">{title}</h1> )}
+				{isReply ? (
+					<div className="reply-name-wrapper">
+						{lite ? (<DisplayName entity={creator}/>) : (<RepliedTo item={item}/>)}
+						<DateTime date={date} relative/>
+					</div>
+				) : (
+					<div className="name-wrapper">
+						<DisplayName entity={creator} localeKey={lite ? void 0 : 'DISCUSSIONS.postedBy'}/>
+						<DateTime date={date} relative/>
+						<SharedWithList item={item}/>
+					</div>
+				)}
 			</div>
-		);
-	}
-});
+		</div>
+	);
+}
+
+AuthorInfo.propTypes = {
+	item: React.PropTypes.object.isRequired,
+	lite: React.PropTypes.bool
+};
