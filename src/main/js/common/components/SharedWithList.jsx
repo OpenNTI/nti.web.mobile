0b8784694bf6558cb4987de0585a056bea0f8e60
Ray Hatfield <ray.hatfield@nextthought.com>
2016-03-17 16:29:50 -0500

    lint: added react key

--- src/main/js/common/components/SharedWithList.jsx
+++ src/main/js/common/components/SharedWithList.jsx
@@ -113,7 +113,7 @@ export default React.createClass({
 				'Only Me';
 
 		let names = (state ? [state] : [])
-			.concat(users.map(x => <DisplayName entity={x}/> ))
+			.concat(users.map((x, i) => <DisplayName key={i} entity={x}/> ))
 			//Only show " N Others" when there are more than 0 others, AND we have at least one resolved name.
 			.concat(others === 0 || users.length === 0 ? [] : t('UNITS.others', {count: others}));
 
