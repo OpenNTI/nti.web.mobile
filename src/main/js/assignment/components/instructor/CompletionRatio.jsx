42ded21d57f779919dadbccd0d30c4b7a2727bee
Ray Hatfield <ray.hatfield@nextthought.com>
2016-03-17 12:00:39 -0500

    rewrite CompletionRatio as a pure function component

--- src/main/js/assignment/components/instructor/CompletionRatio.jsx
+++ src/main/js/assignment/components/instructor/CompletionRatio.jsx
@@ -1,23 +1,16 @@
 import React from 'react';
 
-export default React.createClass({
-	displayName: 'CompletionRatio',
+export default function CompletionRatio ({course, assignment}) {
+	return (
+		<div className="completion-ratio">{assignment.submittedCount}/{course.enrolledTotalCount}</div>
+	);
+}
 
-	propTypes: {
-		course: React.PropTypes.shape({
-			enrolledTotalCount: React.PropTypes.number
-		}).isRequired,
-		assignment: React.PropTypes.shape({
-			submittedCount: React.PropTypes.number
-		}).isRequired
-	},
-
-	render () {
-
-		let {course, assignment} = this.props;
-
-		return (
-			<div className="completion-ratio">{assignment.submittedCount}/{course.enrolledTotalCount}</div>
-		);
-	}
-});
+CompletionRatio.propTypes = {
+	course: React.PropTypes.shape({
+		enrolledTotalCount: React.PropTypes.number
+	}).isRequired,
+	assignment: React.PropTypes.shape({
+		submittedCount: React.PropTypes.number
+	}).isRequired
+};
