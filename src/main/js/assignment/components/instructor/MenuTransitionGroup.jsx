f1acfe945b021db5ed7db5240e0c32aa3cd56f1b
Ray Hatfield <ray.hatfield@nextthought.com>
2016-03-17 12:24:18 -0500

    rewrite MenuTransitionGroup as a pure function component

--- src/main/js/assignment/components/instructor/MenuTransitionGroup.jsx
+++ src/main/js/assignment/components/instructor/MenuTransitionGroup.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import TransitionGroup from 'react-addons-css-transition-group';
 
-export default React.createClass({
-	displayName: 'MenuTransitionGroup',
-
-	render () {
-		return (
-			<TransitionGroup
-				transitionName="fadeOutIn"
-				transitionAppear
-				transitionAppearTimeout={500}
-				transitionEnterTimeout={500}
-				transitionLeaveTimeout={500}
-				{...this.props}
-			/>
-		);
-	}
-});
+export default function MenuTransitionGroup (props) {
+	return (
+		<TransitionGroup
+			transitionName="fadeOutIn"
+			transitionAppear
+			transitionAppearTimeout={500}
+			transitionEnterTimeout={500}
+			transitionLeaveTimeout={500}
+			{...props}
+		/>
+	);
+}
