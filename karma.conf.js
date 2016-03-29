facfe291f5de9023fdcbccc5d2f9a8ecfe9288e3
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-03-29 10:57:05 -0500

    show progress on webpack while unittesting

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,9 @@
 /*eslint no-var: 0 strict: 0*/
 'use strict';
 var baseConfig = require('nti-unittesting-clientside');
+var Progress = require('nti-unittesting-clientside/progress');
 
 module.exports = function (config) {
+	baseConfig.webpack.plugins.push(Progress.getPlugin());
 	config.set(baseConfig);
 };
