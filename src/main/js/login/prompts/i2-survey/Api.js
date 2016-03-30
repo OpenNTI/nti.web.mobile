1537bca1e99f65786a9de540ae1c41cc21634b5c
Ray Hatfield <ray.hatfield@nextthought.com>
2016-03-30 17:20:18 -0500

    i2-survey - start transforming server data into form

--- src/main/js/login/prompts/i2-survey/Api.js
+++ src/main/js/login/prompts/i2-survey/Api.js
@@ -1,12 +1,68 @@
 import {getAppUser} from 'common/utils';
 
-import r1 from './registration.json';
+// import r1 from './registration.json';
 import s1 from './survey.json';
+import sample from './sampledata.json';
 
 const SUBMIT_REGISTRATION = 'SubmitRegistration';
+const REGISTRATION_RULES = 'RegistrationRules';
+
+function getGrades (rules) {
+	let result = {};
+	// for each school in rules
+	Object.keys(rules).forEach(school => {
+		// for each grade in school
+		Object.keys(rules[school]).forEach(grade => {
+			// 		// add grade if it doesn't already exist in result
+			if (!result[grade]) {
+				result[grade] = {
+					value: grade,
+					requirement: []
+				};
+			}
+			// add school=[school] to grade requirements
+			result[grade].requirement.push('school=' + school);
+		});
+	});
+
+	return Object.keys(result).map(k => result[k]);
+}
+
+function registrationViewToFormJson (input) {
+
+	return {
+		elements: [
+			{
+				type: 'select',
+				name: 'school',
+				label: 'Where do you teach?',
+				required: true,
+				options: Object.keys(input[REGISTRATION_RULES]).map(s => ({label: s}))
+			},
+			{
+				type: 'radio',
+				name: 'grade',
+				label: 'What grade?',
+				required: true,
+				options: getGrades(input[REGISTRATION_RULES])
+			},
+			{
+				type: 'select',
+				name: 'course',
+				label: 'Select your course',
+				required: true,
+				options: []
+			}
+		]
+	};
+}
 
 function getRegistration () {
-	return Promise.resolve(r1);
+
+	const form = registrationViewToFormJson(sample);
+
+
+	return Promise.resolve(form);
 }
 
 function getSurvey () {
