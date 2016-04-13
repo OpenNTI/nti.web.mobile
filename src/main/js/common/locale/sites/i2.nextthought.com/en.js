d75f01284e8ff86cb589e6ca5a7e5c3946fe43f3
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-04-13 10:37:06 -0500

    add i2 mappings & locale

---
+++ src/main/js/common/locale/sites/i2.nextthought.com/en.js
@@ -0,0 +1,15 @@
+export default {
+	COURSE: {
+		CONTACTINFO: {
+			label: 'Support',
+			LINK0: {
+				label: 'Program and Curriculum',
+				link: 'mailto:mhiggins@i2learning.org'
+			},
+			LINK1: {
+				label: 'Tech Support',
+				link: 'mailto:support@nextthought.com'
+			}
+		}
+	}
+};
