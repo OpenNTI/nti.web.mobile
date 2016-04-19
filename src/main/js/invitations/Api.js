873c6cb25685001f17a83cea1e08ff61fcc9e7e9
Ray Hatfield <ray.hatfield@nextthought.com>
2016-04-19 11:35:21 -0500

    NTI-451 - reload library upon successful invitation redemption.

--- src/main/js/invitations/Api.js
+++ src/main/js/invitations/Api.js
@@ -1,10 +1,17 @@
 import getLink from 'nti-lib-interfaces/lib/utils/getlink';
+
 import {getService} from 'common/utils';
+import AppDispatcher from 'dispatcher/AppDispatcher';
+import {RELOAD as RELOAD_LIBRARY} from 'library/Constants';
 
 export function redeem (code) {
 	return getService().then(service => {
 		const collection = service.getCollection('Invitations', 'Invitations');
 		const href = getLink(collection, 'accept-course-invitations');
-		return service.postParseResponse(href, {'invitation_codes': code});
+		return service.postParseResponse(href, {'invitation_codes': code})
+			.then(response => {
+				AppDispatcher.handleViewAction({type: RELOAD_LIBRARY});
+				return response;
+			});
 	});
 }
