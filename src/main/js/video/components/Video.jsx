934855c3a72962c98b34e0062896cdf32b64a754
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-04-21 14:11:02 -0500

    remove old comment...

--- src/main/js/video/components/Video.jsx
+++ src/main/js/video/components/Video.jsx
@@ -148,9 +148,6 @@ export default React.createClass({
 			logger.error('TODO: Move the rest of this method to be passed as an event factory');
 		}
 
-		//XXX: Do not fix this line of lint by adding these props to this elements PropTypes, nor making eslint ignore.
-		//To fix it, do the above mentioned "FIXME".
-		/*eslint "react/prop-types": 1*/ //Force this to be a warning for now.
 		let {context, courseId, transcript} = this.props;
 
 		let analyticsEvent = new WatchVideoEvent(
