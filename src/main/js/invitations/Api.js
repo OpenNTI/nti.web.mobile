ca80f372213af859605c2656723bdc5354dd3a57
Ray Hatfield <ray.hatfield@nextthought.com>
2016-04-22 13:46:34 -0500

    minor cleanup

--- src/main/js/invitations/Api.js
+++ src/main/js/invitations/Api.js
@@ -4,10 +4,13 @@ import {getService} from 'nti-web-client';
 import AppDispatcher from 'dispatcher/AppDispatcher';
 import {RELOAD as RELOAD_LIBRARY} from 'library/Constants';
 
+const SEND_LINK = 'SendCourseInvitations';
+const ACCEPT_LINK = 'accept-course-invitations';
+
 export function accept (code) {
 	return getService().then(service => {
 		const collection = service.getCollection('Invitations', 'Invitations');
-		const href = getLink(collection, 'accept-course-invitations');
+		const href = getLink(collection, ACCEPT_LINK);
 		return service.postParseResponse(href, {'invitation_codes': code})
 			.then(response => response && response.waitForPending ? response.waitForPending() : response)
 			.then(response => {
@@ -18,11 +21,14 @@ export function accept (code) {
 }
 
 export function send (course, name, email) {
-	const href = course.getLink('SendCourseInvitations');
+	const href = course.getLink(SEND_LINK);
+	if (!href) {
+		return Promise.reject('No link');
+	}
 	return getService()
 		.then(service => service.postParseResponse(href, { name, email }));
 }
 
 export function canSend (course) {
-	return course && course.hasLink && course.hasLink('SendCourseInvitations');
+	return course && course.hasLink && course.hasLink(SEND_LINK);
 }
