60b3b463c4b15988fe1b050b127fae4f822eed98
Ray Hatfield <ray.hatfield@nextthought.com>
2016-05-17 10:21:02 -0500

    NTI-960 - removed lifelong learner phrasing and course start date

--- src/main/js/enrollment/components/enrollment-status-widgets/EnrolledLifelongLearner.jsx
+++ src/main/js/enrollment/components/enrollment-status-widgets/EnrolledLifelongLearner.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
 
-import DateTime from 'common/components/DateTime';
-
-export default function EnrolledLifelongLearner (props) {
-	const {catalogEntry} = props;
-
+export default function EnrolledLifelongLearner () {
 	return (
 		<div className="enrollment-status-lifelong">
-			<div>
-				<div className="heading">You're Enrolled as a Lifelong Learner</div>
-				<div className="content">
-					Class begins <DateTime date={catalogEntry.getStartDate()} /> and will be conducted fully online.
-				</div>
-			</div>
 			<div className="status">
 				<span className="registered">You are registered</span>
 			</div>
