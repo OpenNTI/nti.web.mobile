bd77104f5f1705cc4b679535564ff8cba04aa204
Ray Hatfield <ray.hatfield@nextthought.com>
2016-05-17 17:20:13 -0500

    DateField renders defaultValue when provided

--- src/main/js/forms/components/DateField.jsx
+++ src/main/js/forms/components/DateField.jsx
@@ -30,7 +30,35 @@ export default React.createClass({
 	propTypes: {
 		name: React.PropTypes.string,
 		onChange: React.PropTypes.func,
-		field: React.PropTypes.object
+		field: React.PropTypes.object,
+		defaultValue: React.PropTypes.string
+	},
+
+	componentWillMount () {
+		this.setUp();
+	},
+
+	componentWillReceiveProps (nextProps) {
+		this.setUp(nextProps);
+	},
+
+	setUp (props = this.props) {
+		const {defaultValue} = props;
+		if (defaultValue) {
+			// (yyyy)-(mm)-(dd)
+			const re = /^([\d]{4})\-([\d]{2})\-([\d]{2})$/;
+			try {
+				const [, year, month, day] = defaultValue.match(re);
+				this.setState({
+					year,
+					month,
+					day
+				});
+			}
+			catch (e) {
+				// defaultValue was not in the expected format; don't care
+			}
+		}
 	},
 
 	onSelectChange (event) {
@@ -50,26 +78,29 @@ export default React.createClass({
 	},
 
 	monthSelect () {
+		const {month = ''} = this.state;
 		return (
-			<DateFieldSelect name="month" onChange={this.onSelectChange} className="date-field-month" defaultValue="">
+			<DateFieldSelect name="month" onChange={this.onSelectChange} className="date-field-month" defaultValue={month}>
 			<option key="month" value="" disabled>Month</option>
-				{MONTHS.map( (month, i) => (<option key={month} value={i + 1}>{month}</option>) )}
+				{MONTHS.map( (m, i) => (<option key={m} value={zpad(i + 1)}>{m}</option>) )}
 			</DateFieldSelect>
 		);
 	},
 
 	daySelect () {
+		const {day = ''} = this.state;
 		return (
-			<DateFieldSelect name="day" onChange={this.onSelectChange} className="date-field-day" defaultValue="">
+			<DateFieldSelect name="day" onChange={this.onSelectChange} className="date-field-day" defaultValue={day}>
 				<option key="day" value="" disabled>Day</option>
-				{range(1, 32).map( (n) => (<option key={n} value={n}>{n}</option>) )}
+				{range(1, 32).map( (n) => (<option key={n} value={zpad(n)}>{n}</option>) )}
 			</DateFieldSelect>
 		);
 	},
 
 	yearSelect () {
+		const {year = ''} = this.state;
 		return (
-			<DateFieldSelect name="year" onChange={this.onSelectChange} className="date-field-year" defaultValue="">
+			<DateFieldSelect name="year" onChange={this.onSelectChange} className="date-field-year" defaultValue={year}>
 				<option key="year" value="" disabled>Year</option>
 				{range(1900, (new Date().getFullYear() + 1)).reverse().map((n) => (<option key={n} value={n}>{n}</option>) )}
 			</DateFieldSelect>
