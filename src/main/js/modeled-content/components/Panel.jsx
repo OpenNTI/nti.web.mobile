514b37ab763691c72e9849d670fa1dbabeac27c2
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-05-19 18:54:58 -0500

    cleanup & add preview class

--- src/main/js/modeled-content/components/Panel.jsx
+++ src/main/js/modeled-content/components/Panel.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import cx from 'classnames';
 
 import {getHTMLSnippet, filterContent, processContent} from 'nti-lib-content-processing';
 
@@ -48,14 +49,15 @@ export default React.createClass({
 	displayName: 'ModeledBodyContent',
 
 	propTypes: {
-		body: React.PropTypes.array,
+		className: PropTypes.string,
+		body: PropTypes.array,
 
-		previewMode: React.PropTypes.bool,
-		previewLength: React.PropTypes.number,
+		previewMode: PropTypes.bool,
+		previewLength: PropTypes.number,
 
-		strategies: React.PropTypes.object,
-		widgets: React.PropTypes.object,
-		renderCustomWidget: React.PropTypes.func
+		strategies: PropTypes.object,
+		widgets: PropTypes.object,
+		renderCustomWidget: PropTypes.func
 	},
 
 
@@ -153,12 +155,17 @@ export default React.createClass({
 
 
 	render () {
-		let props = Object.assign({}, this.props, {body: undefined});
-		let {body} = this.state;
+		const {
+			props: {className, previewMode},
+			state: {body}
+		} = this;
+
+		const props = Object.assign({}, this.props, {
+			body: undefined,
+			className: cx('modeled-content', className, {preview: previewMode})
+		});
+
 		let dynamicRenderers = [];
-
-		props.className = (props.className || '') + ' modeled-content';
-
 		if (Array.isArray(body)) {
 			dynamicRenderers = body;
 		}
