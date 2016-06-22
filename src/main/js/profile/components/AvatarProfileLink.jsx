3ec7f86925394703976191e57d595945ebfb87d7
Ray Hatfield <ray.hatfield@nextthought.com>
2016-06-22 10:51:32 -0500

    derp

--- src/main/js/profile/components/AvatarProfileLink.jsx
+++ src/main/js/profile/components/AvatarProfileLink.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Avatar from 'common/components/Avatar';
 import DisplayName from 'common/components/DisplayName';
-import FollowButton from 'nti-web-commons';
+import {FollowButton} from 'nti-web-commons';
 
 import {rawContent} from 'nti-commons/lib/jsx';
 
