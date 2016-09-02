a9aa3802732368b65696979c7e5cc6345425da7e
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-09-02 18:23:56 -0500

    move strings to site packages

--- src/main/js/locale/sites/platform.ou.edu/partials/coming-soon.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	general: 'Coming soon to mobile.\nUntil then, please use a desktop or iPad app to access it.',
-	singular: '%(subject)s is not yet available on mobile. Please use the iPad app or a desktop browser.',
-	plural: '%(subject)s are not yet available on mobile. Please use the iPad app or a desktop browser.'
-};
