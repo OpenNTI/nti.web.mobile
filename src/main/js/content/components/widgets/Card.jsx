e77843a21ad33df1b9f4718e36b0504adf5aa789
Andrew Ligon <andrew.ligon@nextthought.com>
2017-11-16 13:59:44 -0600

    NTI-4312 Use the card context for all related work refs

--- src/main/js/content/components/widgets/Card.jsx
+++ src/main/js/content/components/widgets/Card.jsx
@@ -11,7 +11,7 @@ export default createReactClass({
 	mixins: [Mixin],
 
 	statics: {
-		itemType: /ntirelatedworkref$|nticard$|externallink$/i,
+		itemType: /relatedworkref$|nticard$|externallink$/i,
 
 		interactiveInContext: true
 	},
