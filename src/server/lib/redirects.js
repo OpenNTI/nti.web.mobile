43bcbb6c17f3428addbc22f32f44216a2890ec34
Cory Jones <cory.jones@nextthought.com>
2017-12-07 16:19:06 -0600

    Add redirect for catalog/redeem

--- src/server/lib/redirects.js
+++ src/server/lib/redirects.js
@@ -18,7 +18,7 @@ const SEGMENT_HANDLERS = {
 
 const HANDLERS = {
 	handleObjectRedirects: /(\/id|ntiid)\//i,
-	handleInvitationRedirects: /invitations\/accept/i,
+	handleInvitationRedirects: /invitations\/accept|catalog\/redeem/i,
 	handleLibraryRedirects: /^library/i,
 	//the path may not always start with /app/ but it will always be have one path segment in front.
 	handleLibraryPathRedirects: /^\/[^\/]+\/library/i
