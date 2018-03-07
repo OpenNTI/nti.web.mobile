747700e68aa20724fa525511f9604c4e95b6b9ff
Josh Birdwell <jbirdwell34@gmail.com>
2018-03-07 15:08:23 -0600

    Scorm pass

--- src/main/js/course/Sections.js
+++ src/main/js/course/Sections.js
@@ -2,5 +2,6 @@ export const ACTIVITY = 'activity/';
 export const ASSIGNMENTS = 'assignments/';
 export const DISCUSSIONS = 'discussions/';
 export const LESSONS = 'lessons/';
+export const SCORMCONTENT = 'scormcontent/';
 export const INFO = 'info';
 export const VIDEOS = 'videos/';
