e3f00a02c41687eadfe8088a5500e4515544b90f
Jonathan Grimes <jsg2021@gmail.com>
2018-03-12 11:24:45 -0500

    Use explicit split points instead of magic filenames

---
+++ src/main/js/catalog/components/ViewLoader.jsx
@@ -0,0 +1,7 @@
+import Loadable from 'react-loadable';
+import {Loading} from 'nti-web-commons';
+
+export default Loadable({
+	loader: () => import(/* webpackChunkName: "catalog" */'./View'),
+	loading: Loading.Mask,
+});
