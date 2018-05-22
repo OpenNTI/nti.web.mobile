f8b167dc031925c181f4a6be28b3c0d91b512d69
Andrew Ligon <andrew.ligon@nextthought.com>
2018-05-22 17:51:09 -0500

    NTI-5733 add the transcript tab back

--- src/main/js/profile/components/user/PageFrame.jsx
+++ src/main/js/profile/components/user/PageFrame.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import createReactClass from 'create-react-class';
 import {getAppUsername} from '@nti/web-client';
-import {ActiveState, Background} from '@nti/web-commons';
+import {ActiveState, Background, Flyout} from '@nti/web-commons';
 
 import Page from 'common/components/Page';
 import DisplayName from 'common/components/DisplayName';
@@ -50,6 +50,15 @@ export default createReactClass({
 								<li className="profile-nav-item"><ActiveState hasChildren tag="a" href="/about/">About</ActiveState></li>
 								<li className="profile-nav-item"><ActiveState tag="a" href="/activity/" hasChildren={/^\/(activity|thoughts)/i}>Activity</ActiveState></li>
 								<li className="profile-nav-item"><ActiveState tag="a" href="/memberships/">Memberships</ActiveState></li>
+								<li className="profile-nav-item">
+									<Flyout.Triggered trigger={(<a href="#" className="profile-nav-more-trigger">&middot;&middot;&middot;</a>)} verticalAlign={Flyout.ALIGNMENTS.BOTTOM} horizontalAlign={Flyout.ALIGNMENTS.RIGHT} arrow dark>
+										<ul className="profile-nav-sub-items">
+											{entity && entity.hasLink && entity.hasLink('transcript') && (
+												<li className="profile-nav-item"><ActiveState tag="a" href="/transcripts/">Transcripts</ActiveState></li>
+											)}
+										</ul>
+									</Flyout.Triggered>
+								</li>
 								{/*<li className="profile-nav-item"><ActiveLink href="/achievements/">Achievements</ActiveLink></li>*/}
 							</ul>
 						</Head>
