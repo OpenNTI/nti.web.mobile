a9d52d17ff2353c0fce938f9150fd75d99782877
Cory Jones <cory.jones@nextthought.com>
2018-05-23 09:25:01 -0500

    Update profile imports

--- src/main/js/profile/components/user/Transcripts.jsx
+++ src/main/js/profile/components/user/Transcripts.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Transcripts} from '@nti/web-profiles';
+import {User} from '@nti/web-profiles';
 
+const {Transcripts} = User;
 
 ProfileTranscripts.propTypes = {
 	entity: PropTypes.object
