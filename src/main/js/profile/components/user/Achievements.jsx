4cb3ef0823f7296f3f1ca56205d67a1f2569a655
Cory Jones <cory.jones@nextthought.com>
2018-05-30 09:50:13 -0500

    Add identity check to determine showing other users certificates

--- src/main/js/profile/components/user/Achievements.jsx
+++ src/main/js/profile/components/user/Achievements.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Logger from '@nti/util-logger';
 import {User} from '@nti/web-profiles';
+import {getAppUser} from '@nti/web-client';
+import {Loading} from '@nti/web-commons';
+import {scoped} from '@nti/lib-locale';
 
 import Card from '../Card';
+
+const t = scoped('nti-web-mobile.profile.components.user.Achievements', {
+	noCertificates: 'This user doesn\'t have any visible badges or certificates.'
+});
+
 
 const {ProfileCertificates} = User;
 
@@ -16,18 +24,38 @@ export default class extends React.Component {
 		entity: PropTypes.object.isRequired
 	};
 
+	state = {}
+
 	componentDidMount () {
 		let {entity} = this.props;
 		if (entity) {
 			entity.getAchievements().then(achievements=> logger.debug(achievements));
 		}
+
+		getAppUser().then(user => {
+			this.setState({actingUser: user});
+		});
+	}
+
+	renderEmptyMessage () {
+		return <div className="empty-state">{t('noCertificates')}</div>;
 	}
 
 	render () {
+		const {actingUser} = this.state;
+		const {entity} = this.props;
+
+		if(!actingUser || !entity) {
+			return <Loading.Ellipsis/>;
+		}
+
+		const isMe = actingUser.Username === entity.Username;
+
 		return (
 			<ul className="profile-cards">
 				<Card className="achievements" title="Achievements">
-					<ProfileCertificates entity={this.props.entity} showPreviewFrame={false}/>
+					{isMe && <ProfileCertificates entity={entity} showPreviewFrame={false}/>}
+					{!isMe && this.renderEmptyMessage()}
 				</Card>
 			</ul>
 		);
