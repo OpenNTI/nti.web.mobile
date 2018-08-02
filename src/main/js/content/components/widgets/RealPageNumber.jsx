2aa154032ae3bd45767d5024df36a3954445b0e2
Josh Birdwell <jbirdwell34@gmail.com>
2018-08-02 15:57:23 -0500

    NTI-6303: Real page number scroll update

---
+++ src/main/js/content/components/widgets/RealPageNumber.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import createReactClass from 'create-react-class';
+import { VisibleComponentTracker } from '@nti/web-commons';
+
+import ContextAccessor from 'common/mixins/ContextAccessor';
+
+import Mixin from './Mixin';
+
+
+export default createReactClass({
+	displayName: 'RealPageNumbers',
+	mixins: [Mixin, ContextAccessor],
+
+	propTypes: {
+		item: PropTypes.shape({
+			pageNumber: PropTypes.string.isRequired
+		})
+	},
+
+	statics: {
+		itemType: /realpagenumber/i
+	},
+
+	render () {
+		const { item: { pageNumber } } = this.props;
+		return (
+			<VisibleComponentTracker
+				group="real-page-numbers"
+				data={{ pageNumber }}
+			/>
+		);
+	}
+});
