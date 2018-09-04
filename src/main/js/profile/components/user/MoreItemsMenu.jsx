b5bbde48a138db9ef0bb65bdaec9d1eded90910c
Ray Hatfield <ray.hatfield@nextthought.com>
2018-09-04 16:25:24 -0500

    fix markup

--- src/main/js/profile/components/user/MoreItemsMenu.jsx
+++ src/main/js/profile/components/user/MoreItemsMenu.jsx
@@ -21,9 +21,7 @@ export default class MoreItemsMenu extends React.Component {
 		return (
 			<Flyout.Triggered trigger={(<a href="#" className="profile-nav-more-trigger">&middot;&middot;&middot;</a>)} ref={this.attachFlyoutRef} className="profile-more-items-menu" verticalAlign={Flyout.ALIGNMENTS.BOTTOM} horizontalAlign={Flyout.ALIGNMENTS.RIGHT} arrow dark>
 				<ul className="profile-nav-sub-items" onClick={this.dismissFlyout}>
-					<ul className="profile-nav-sub-items">
-						<li className="profile-nav-item"><ActiveState tag="a" href="/achievements/"><div onClick={this.dismissFlyout}>Achievements</div></ActiveState></li>
-					</ul>
+					<li className="profile-nav-item"><ActiveState tag="a" href="/achievements/"><div onClick={this.dismissFlyout}>Achievements</div></ActiveState></li>
 					{entity && entity.hasLink && entity.hasLink('transcript') && (
 						<li className="profile-nav-item"><ActiveState tag="a" href="/transcripts/"><div onClick={this.dismissFlyout}>Transcripts</div></ActiveState></li>
 					)}
