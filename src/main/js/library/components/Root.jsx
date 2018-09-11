7766b59ab3e4a79983e3fd24dc677509f8c8bd08
Landon Sherwood <landonsherwood23@gmail.com>
2018-09-11 14:42:01 -0500

    fix routing for new library

--- src/main/js/library/components/Root.jsx
+++ src/main/js/library/components/Root.jsx
@@ -1,4 +1,10 @@
+import path from 'path';
+
 import React from 'react';
+import PropTypes from 'prop-types';
+import {View as SearchableLibrary} from '@nti/web-library';
+import {isFlag} from '@nti/web-client';
+import {encodeForURI} from '@nti/lib-ntiids';
 
 import NavigationBar from 'navigation/components/Bar';
 
@@ -8,22 +14,71 @@ import Books from './containers/Books';
 import Features from './containers/Features';
 import Branding from './Branding';
 
-export default function Root () {
-	return (
-		<div>
-			<NavigationBar>
-				<Branding position="left"/>
-			</NavigationBar>
+export default class Root extends React.Component {
+	static contextTypes = {
+		router: PropTypes.object,
+		basePath: PropTypes.string
+	}
 
-			<Features/>
 
-			<Communities/>
+	static childContextTypes = {
+		router: PropTypes.object
+	}
 
-			<Courses/>
 
-			<Courses admin/>
+	getChildContext () {
+		const {router: nav} = this.context;
 
-			<Books/>
-		</div>
-	);
+		return {
+			router: {
+				...(nav || {}),
+				baseroute: nav && nav.makeHref(''),
+				getRouteFor: this.getRouteFor,
+			}
+		};
+	}
+
+	getRouteFor = (obj, context) => {
+		const {context: {basePath}} = this;
+		const getEncodedIDCourse = o => encodeForURI(o.getLinkProperty('CourseInstance', 'ntiid'));
+		const {MimeType: type} = obj || {};
+
+		let route = '';
+
+		if (/course(.*)enrollment/i.test(type)) {
+			route = path.join(basePath, 'course', getEncodedIDCourse(obj));
+		} else if (/contentpackagebundle/i.test(type)) {
+			route = path.join(basePath, 'content', obj.getID());
+		} else if (/community/i.test(type)) {
+			route = path.join(basePath, 'profile', obj.getID());
+		}
+
+		return route;
+	}
+
+	render () {
+		return (
+			<div>
+				<NavigationBar>
+					<Branding position="left"/>
+				</NavigationBar>
+
+				{isFlag('library-searchable') ? (
+					<SearchableLibrary />
+				) : (
+					<>
+						<Features/>
+
+						<Communities/>
+
+						<Courses/>
+
+						<Courses admin/>
+
+						<Books/>
+					</>
+				)}
+			</div>
+		);
+	}
 }
