c9d80ef009c1f637ff4b4c43b84eb5e96546bf2a
Andrew Ligon <andrew.ligon@nextthought.com>
2018-09-17 12:55:23 -0500

    NTI-6635 only add the router context around the navigation

--- src/main/js/course/components/NavigationTabs.jsx
+++ src/main/js/course/components/NavigationTabs.jsx
@@ -1,6 +1,77 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import {Router, Route} from '@nti/web-routing';
 import {Navigation} from '@nti/web-course';
+import {encodeForURI} from '@nti/lib-ntiids';
+
+class CourseNavigationTabs extends React.Component {
+	static propTypes = {
+		course: PropTypes.object.isRequired
+	}
+
+	static contextTypes = {
+		router: PropTypes.object
+	}
+
+	static childContextTypes = {
+		router: PropTypes.shape({
+			getRouteFor: PropTypes.func
+		})
+	}
+
+	getChildContext () {
+		return {
+			router: {
+				...this.context.router,
+				baseroute: this.getBaseRoute(),
+				getRouteFor: (...args) => this.getRouteFor(...args)
+			}
+		};
+	}
+
+
+	getBaseRoute () {
+		const {course} = this.props;
+		const courseID = course.getCourseID ? course.getCourseID() : course.NTIID;
+
+		return `/mobile/course/${encodeForURI(courseID)}/`;
+	}
+
+
+	getRouteFor (obj, context) {
+		if (obj !== this.props.course) { return null; }
+
+		const base = this.getBaseRoute();
+
+		let path = '';
+
+		if (context === 'lessons') {
+			path = 'lessons/';
+		} else if (context === 'assignments') {
+			path = 'assignments/';
+		} else if (context === 'discussions') {
+			path = 'discussions/';
+		} else if (context === 'info') {
+			path = 'info/';
+		} else if (context === 'scorm') {
+			path = 'scormcontent/';
+		} else if (context === 'videos') {
+			path = 'videos/';
+		}
+
+		return `${base}${path}`;
+	}
+
+
+	render () {
+		const {course} = this.props;
+
+		return (
+			<Navigation.Tabs course={course} />
+		);
+	}
+}
 
 export default Router.for([
-	Route({path: '/', component: Navigation.Tabs})
+	Route({path: '/', component: CourseNavigationTabs})
 ]);
--- src/main/js/course/components/Page.jsx
+++ src/main/js/course/components/Page.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {encodeForURI} from '@nti/lib-ntiids';
 
 import Page from 'common/components/Page';
 
@@ -15,60 +14,6 @@ export default class extends React.Component {
 		course: PropTypes.object.isRequired
 	};
 
-	static contextTypes = {
-		router: PropTypes.object
-	}
-
-
-	static childContextTypes = {
-		router: PropTypes.shape({
-			getRouteFor: PropTypes.func
-		})
-	}
-
-
-	getBaseRoute () {
-		const {course} = this.props;
-		const courseID = course.getCourseID ? course.getCourseID() : course.NTIID;
-
-		return `/mobile/course/${encodeForURI(courseID)}/`;
-	}
-
-
-	getChildContext () {
-		return {
-			router: {
-				...this.context.router,
-				baseroute: this.getBaseRoute(),
-				getRouteFor: (...args) => this.getRouteFor(...args)
-			}
-		};
-	}
-
-
-	getRouteFor (obj, context) {
-		if (obj !== this.props.course) { return null; }
-
-		const base = this.getBaseRoute();
-
-		let path = '';
-
-		if (context === 'lessons') {
-			path = 'lessons/';
-		} else if (context === 'assignments') {
-			path = 'assignments/';
-		} else if (context === 'discussions') {
-			path = 'discussions/';
-		} else if (context === 'info') {
-			path = 'info/';
-		} else if (context === 'scorm') {
-			path = 'scormcontent/';
-		} else if (context === 'videos') {
-			path = 'videos/';
-		}
-
-		return `${base}${path}`;
-	}
 
 	render () {
 		const {children, course, ...otherProps} = this.props;
