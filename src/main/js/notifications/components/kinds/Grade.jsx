c102909c11e406959c0046082d6900cda19fc5bf
Cory Jones <cory.jones@nextthought.com>
2018-09-21 14:51:41 -0500

    NTI-6578 Use course info for auto-grade notifications

--- src/main/js/notifications/components/kinds/Grade.jsx
+++ src/main/js/notifications/components/kinds/Grade.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import createReactClass from 'create-react-class';
-import {DateTime} from '@nti/web-commons';
+import {DateTime, Presentation} from '@nti/web-commons';
+import {getService} from '@nti/web-client';
 
 import Avatar from 'common/components/Avatar';
 import DisplayName from 'common/components/DisplayName';
@@ -24,16 +25,37 @@ export default createReactClass({
 		item: PropTypes.object
 	},
 
+	componentDidMount () {
+		const {item: {Item: {creator, CatalogEntryNTIID }}} = this.props;
+
+		if(creator === 'system') {
+			getService().then(service => {
+				service.getObject(CatalogEntryNTIID).then(catalogEntry => {
+					this.setState({
+						catalogEntry
+					});
+				});
+			});
+		}
+		else {
+			this.setState({
+				creator
+			});
+		}
+	},
 
 	render () {
-		const {state: {url}, props: {item: {Item: {creator, CourseName, AssignmentName = 'an assignment'}}}} = this;
+		const {state: {url, catalogEntry, creator}, props: {item: {Item: {CourseName, AssignmentName = 'an assignment'}}}} = this;
 
 		return (
 			<li className="notification-item">
 				<a href={url}>
-					<Avatar entity={creator} width="32" height="32" suppressProfileLink/>
+					{creator && <Avatar entity={creator} width="32" height="32" suppressProfileLink/>}
+					{catalogEntry && <Presentation.AssetBackground className="avatar" contentPackage={catalogEntry} type="thumb"/>}
 					<div className="wrap">
-						<DisplayName entity={creator} suppressProfileLink/> graded {AssignmentName}{CourseName ? ` in ${CourseName}` : ''}
+						{creator && <DisplayName entity={creator} suppressProfileLink/>}
+						{catalogEntry && <span>{catalogEntry.Title}</span>}
+						<span> graded {AssignmentName}{CourseName ? ` in ${CourseName}` : ''}</span>
 						<DateTime date={this.getEventTime()} relative />
 					</div>
 				</a>
