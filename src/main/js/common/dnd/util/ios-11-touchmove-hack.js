36b03ab9f56aedd97919a046623e6a39053698fd
Ray Hatfield <ray.hatfield@nextthought.com>
2018-10-24 18:11:48 -0500

    [NTI-6821] - workaround for touchmove events in iOS 11.3+

---
+++ src/main/js/common/dnd/util/ios-11-touchmove-hack.js
@@ -0,0 +1,56 @@
+import {default as isTouch, PointerEvents} from '@nti/util-detection-touch';
+import iOSVersion from '@nti/util-ios-version';
+import {
+	addEventListener as _addEventListener,
+	removeEventListener as _removeEventListener,
+} from '@nti/lib-dom';
+
+const [major, minor] = iOSVersion() || [];
+const isBroken = isTouch && major > 11 || (major === 11 && minor >= 3);
+
+// iOS 11.3+ doesn't allow non-passive dynamically added touchmove events,
+// meaning that event listeners added when a drag starts are unable to preventDefault
+// on the event, which we need to prevent document scrolling.
+// see: https://bugs.webkit.org/show_bug.cgi?id=184250
+//
+// This solution is based on the react-beautiful-dnd workaround
+// https://github.com/atlassian/react-beautiful-dnd/blob/74562f733fbd8795c4f41b8f222ce8eb9dc19150/src/view/drag-handle/sensor/create-touch-sensor.js#L138
+
+export default !isBroken ? {} : {
+	componentDidMount () {
+		const options = {
+			passive: false,
+			capture: false
+		};
+
+		const handler = safariTouchMoveHandler.bind(this);
+
+		_addEventListener(global,
+			PointerEvents.pointerMove,
+			handler,
+			options
+		);
+
+		this.removeSafariHack = () => _removeEventListener(
+			global,
+			PointerEvents.pointerMove,
+			handler
+		);
+	},
+
+	componentWillUnmount () {
+		if (this.removeSafariHack) {
+			this.removeSafariHack();
+		}
+	},
+};
+
+function safariTouchMoveHandler (e) {
+	const {dragging} = this.state;
+
+	if (!dragging || e.defaultPrevented) {
+		return;
+	}
+
+	e.preventDefault();
+}
