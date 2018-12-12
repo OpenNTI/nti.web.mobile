e8b9898cd8e329959d61e2110aea871d144ca8b8
Ray Hatfield <ray.hatfield@nextthought.com>
2018-12-12 13:46:08 -0600

    remember selected tab in side drawer

--- src/main/js/app/right-overlay/tabs/View.jsx
+++ src/main/js/app/right-overlay/tabs/View.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {scoped} from '@nti/lib-locale';
 import {Switch} from '@nti/web-commons';
+import Storage from '@nti/web-storage';
 import cx from 'classnames';
 
 import Notifications from './Notifications';
@@ -10,6 +12,8 @@ import Contacts from './contacts';
 const NOTIFICATIONS = 'notifications';
 const CALENDAR = 'calendar';
 const CONTACTS = 'contacts';
+
+const storageKey = 'app:right-overlay:tabs:selected-tab';
 
 const t = scoped('app.user-overlay.tabs', {
 	[NOTIFICATIONS]: 'Notifications',
@@ -32,17 +36,35 @@ const TABS = {
 	},
 };
 
+class Tab extends React.Component {
+	static propTypes = {
+		item: PropTypes.string,
+		label: PropTypes.string,
+		children: PropTypes.any,
+		className: PropTypes.string
+	}
+
+	rememberSelectedTab = () => Storage.setItem(storageKey, this.props.item)
+
+	render () {
+		const {children, className, ...other} = this.props;
+
+		return (
+			<Switch.Trigger
+				className={cx('tab-label', className)}
+				onClick={this.rememberSelectedTab}
+				{...other}
+			>
+				{children}
+			</Switch.Trigger>
+		);
+	}
+}
+
 export default class View extends React.Component {
 
 	renderTrigger = ([key, {label}]) => (
-		<Switch.Trigger
-			className={cx('tab-label', key)}
-			key={key}
-			item={key}
-			title={label}
-		>
-			{label}
-		</Switch.Trigger>
+		<Tab key={key} item={key} className={key} title={label}>{label}</Tab>
 	)
 
 	renderItem = ([key, {label, component}]) => (
@@ -55,8 +77,10 @@ export default class View extends React.Component {
 	)
 
 	render () {
+		const active = Storage.getItem(storageKey) || Object.keys(TABS)[0];
+
 		return (
-			<Switch.Panel className="nti-mobile-drawer-tab-panel" active={Object.keys(TABS)[0]}>
+			<Switch.Panel className="nti-mobile-drawer-tab-panel" active={active}>
 				<Switch.Controls className="nti-mobile-drawer-tabs">
 					{Object.entries(TABS).map(this.renderTrigger)}
 				</Switch.Controls>
