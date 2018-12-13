5c6a02eb0ccc76e8e9a1c8504afc62a0d34c1fc3
Ray Hatfield <ray.hatfield@nextthought.com>
2018-12-13 12:32:03 -0600

    add chevron to back link in calendar header

--- src/main/js/calendar/components/BackButton.jsx
+++ src/main/js/calendar/components/BackButton.jsx
@@ -4,6 +4,6 @@ import {closeDialog} from '../../util';
 
 export default function BackButton () {
 	return (
-		<div className="calendar-back-button" onClick={closeDialog}>Back</div>
+		<div className="calendar-back-button" onClick={closeDialog}><i className="icon-chevron-left" /> Back</div>
 	);
 }
