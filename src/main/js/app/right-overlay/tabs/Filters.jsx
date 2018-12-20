70fae50955326b2d3d9ed3f0620303eb20d7fffb
Ray Hatfield <ray.hatfield@nextthought.com>
2018-12-20 16:11:49 -0600

    add svg filter for coloring active tab

---
+++ src/main/js/app/right-overlay/tabs/Filters.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {HOC} from '@nti/web-commons';
+
+export default
+@HOC.SingleInstance
+class Filters extends React.Component {
+	render () {
+		return (
+			<svg className="svg-filter">
+				<filter id="active-tab-filter">
+					<feFlood result="flood" floodColor="red" />
+					<feComposite in="flood" in2="SourceAlpha" operator="atop" result="maskedflood" />
+				</filter>
+			</svg>
+		);
+	}
+}
