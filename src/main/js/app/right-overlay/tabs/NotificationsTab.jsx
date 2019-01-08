4a8f0d365c1b6fdc1edc688a1f73dea92244d2f3
Cory Jones <cory.jones@nextthought.com>
2019-01-08 11:52:59 -0600

    NTI-7188 Remove badge for now until we have a more robust solution

--- src/main/js/app/right-overlay/tabs/NotificationsTab.jsx
+++ src/main/js/app/right-overlay/tabs/NotificationsTab.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import {Badge} from '@nti/web-commons';
 import cx from 'classnames';
-
-import {getCount} from 'notifications/Api';
 
 import {TAB_ICON_CLASSNAME} from './util';
 import ICON from './assets/notifications.svg';
@@ -11,28 +8,11 @@ export default class NotificationsTab extends React.Component {
 
 	state = {}
 
-	componentDidMount () {
-		this.setUp();
-	}
-
-	componentWillUnmount () {
-		this.unmounted = true;
-	}
-
-	async setUp () {
-		const count = await getCount();
-		if (!this.unmounted) {
-			this.setState({count});
-		}
-	}
-
 	render () {
-		const {count} = this.state;
-
 		return (
-			<Badge badge={count} position={Badge.POSITIONS.CENTER_RIGHT}>
+			<div onClick={this.markViewed}>
 				<img src={ICON} className={cx(TAB_ICON_CLASSNAME, 'notifications-icon')} />
-			</Badge>
+			</div>
 		);
 	}
 }
