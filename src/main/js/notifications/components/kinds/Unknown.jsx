eecb4346085f387fb45755ad4b87de3175e520ab
Andrew Ligon <andrew.ligon@nextthought.com>
2019-01-31 21:07:15 -0600

    NTI-7349 ignore unknown events

--- src/main/js/notifications/components/kinds/Unknown.jsx
+++ src/main/js/notifications/components/kinds/Unknown.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import createReactClass from 'create-react-class';
 
 import NoteableMixin from '../mixins/Noteable';
@@ -8,21 +8,6 @@ export default createReactClass({
 	mixins: [NoteableMixin],
 
 	render () {
-		const {item} = this.state;
-		//console.debug('Unhandled Notification Kind: %o', this.state.change);
-		//console.debug(this.state.item);
-
-		if(item.MimeType === 'application/vnd.nextthought.change' && !item.Item) {
-			// this is a special case of Unknown, where we have a 'change' item but no sub-item in it to know how it should behave
-			return (
-				<li className="notification-item"/>
-			);
-		}
-
-
-		let type = item.MimeType.replace('application/vnd.nextthought.', '');
-		return (
-			<li className="notification-item">Unknown {type}</li>
-		);
+		return null;
 	}
 });
