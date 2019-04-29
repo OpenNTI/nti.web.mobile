d2b40ed889a4925da5d426633ca187ad62ca80a9
Andrew Ligon <andrew.ligon@nextthought.com>
2019-04-29 15:45:03 -0500

    pull in the topic component

--- src/main/js/course/components/items/overrides/topic/View.jsx
+++ src/main/js/course/components/items/overrides/topic/View.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+
+import Topic from 'forums/components/Topic';
 
 import Page from '../Page';
 import Registry from '../Registry';
+
+function getTopicId (location) {
+	const {item} = location || {};
+
+	return item && item.target;
+}
 
 const MIME_TYPES = {
 	'application/vnd.nextthought.discussionref': true,
@@ -18,12 +26,19 @@ const handles = (obj) => {
 export default
 @Registry.register(handles)
 class CourseItemAssignment extends React.Component {
+	static propTypes = {
+		location: PropTypes.object
+	}
+
 	render () {
+		const {location} = this.props;
+		const topicId = getTopicId(location);
+
 		return (
 			<Page {...this.props}>
-				<h1>
-					Topic
-				</h1>
+				<div className="forums-wrapper">
+					<Topic topicId={topicId} />
+				</div>
 			</Page>
 		);
 	}
