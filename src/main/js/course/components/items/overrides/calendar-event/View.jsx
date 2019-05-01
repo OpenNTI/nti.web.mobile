942661def9d45f71f50ce7cb390edc9661eb3a43
Andrew Ligon <andrew.ligon@nextthought.com>
2019-05-01 15:56:04 -0500

    add events to the course items

--- src/main/js/course/components/items/overrides/calendar-event/View.jsx
+++ src/main/js/course/components/items/overrides/calendar-event/View.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import {Event} from '@nti/web-calendar';
+import classnames from 'classnames/bind';
 
 import Page from '../Page';
 import Registry from '../Registry';
+
+import Styles from './View.css';
+
+const cx = classnames.bind(Styles);
 
 const MIME_TYPES = {
 	'application/vnd.nextthought.nticalendareventref': true
@@ -18,12 +24,19 @@ const handles = (obj) => {
 export default
 @Registry.register(handles)
 class CourseItemAssignment extends React.Component {
+	static propTypes = {
+		location: PropTypes.shape({
+			item: PropTypes.object
+		})
+	}
+
 	render () {
+		const {location} = this.props;
+		const {item: {CalendarEvent: event} = {}} = location || {};
+
 		return (
 			<Page {...this.props}>
-				<h1>
-					Calendar Event
-				</h1>
+				<Event.View event={event} className={cx('course-item-event')} nonDialog noControls />
 			</Page>
 		);
 	}
