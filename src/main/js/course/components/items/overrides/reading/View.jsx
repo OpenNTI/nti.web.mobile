5119367dbf4a087e979180e87aa68a40450d5d02
Andrew Ligon <andrew.ligon@nextthought.com>
2019-05-06 18:01:23 -0500

    NTI-7927 show the current page number

--- src/main/js/course/components/items/overrides/reading/View.jsx
+++ src/main/js/course/components/items/overrides/reading/View.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames/bind';
 import {encodeForURI} from '@nti/lib-ntiids';
+import {scoped} from '@nti/lib-locale';
 
 import ContentView from 'content/components/ViewerLoader';
 
 import Page from '../Page';
 import Registry from '../Registry';
+
+import Styles from './View.css';
+
+const cx = classnames.bind(Styles);
+
+const t = scoped('mobile.course.items.overrides.reading.View',  {
+	page: 'Page %(current)s of %(total)s'
+});
 
 function getPageId (location) {
 	const {item} = location || {};
@@ -57,11 +67,13 @@ class CourseItemReading extends React.Component {
 
 	render () {
 		const {course, location, ...otherProps} = this.props;
+		const {currentPage, totalPages} = location;
 		const pageId = getPageId(location);
 		const rootId = getRootId(pageId, location);
 
 		return (
 			<Page {...this.props}>
+				{this.renderPage(currentPage, totalPages)}
 				<ContentView
 					{...otherProps}
 					pageId={pageId}
@@ -70,6 +82,16 @@ class CourseItemReading extends React.Component {
 					noNavigation
 				/>
 			</Page>
+		);
+	}
+
+	renderPage (current, total) {
+		if (!total || total === 1) { return null; }
+
+		return (
+			<div className={cx('page')}>
+				{t('page', {current: current + 1, total})}
+			</div>
 		);
 	}
 }
