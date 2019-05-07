5094801ce23ea7865e59ffcf363ad025f066c72e
Ray Hatfield <ray.hatfield@nextthought.com>
2019-05-07 16:48:39 -0500

    NTI-7944 - use UserCompletionCount for ratio when available

--- src/main/js/assignment/components/instructor/CompletionRatio.jsx
+++ src/main/js/assignment/components/instructor/CompletionRatio.jsx
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+const AorB = (a, b) => typeof a === 'number' ? a : b;
+
 CompletionRatio.propTypes = {
 	course: PropTypes.shape({
 		enrolledTotalCount: PropTypes.number
@@ -11,11 +13,11 @@ CompletionRatio.propTypes = {
 };
 
 export default function CompletionRatio ({course, assignment}) {
-	const {submittedCount, submittedCountTotalPossible: totalPossible} = assignment;
-
+	const {submittedCount, UserCompletionCount, submittedCountTotalPossible: totalPossible} = assignment;
+	const totalSubmissions = AorB(UserCompletionCount, submittedCount);
 	const total = typeof totalPossible === 'number' ? totalPossible : course.enrolledTotalCount;
 
 	return (
-		<div className="completion-ratio">{submittedCount || 0}/{total}</div>
+		<div className="completion-ratio">{totalSubmissions || 0}/{total}</div>
 	);
 }
