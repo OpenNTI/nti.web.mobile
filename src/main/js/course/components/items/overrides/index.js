0fbb24420355a2961976046f4c3d0f0917ffff69
Andrew Ligon <andrew.ligon@nextthought.com>
2019-06-17 16:01:02 -0500

    NTI-8052 wrap scorm in the course item page

--- src/main/js/course/components/items/overrides/index.js
+++ src/main/js/course/components/items/overrides/index.js
@@ -3,6 +3,7 @@ import Registry from './Registry';
 import './assignment';
 import './calendar-event';
 import './reading';
+import './scorm';
 import './timeline';
 import './topic';
 import './video';
---
+++ src/main/js/course/components/items/overrides/scorm/View.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Content} from '@nti/web-course';
+
+import Page from '../Page';
+import Registry from '../Registry';
+
+const {SCORM} = Content.Viewer.ContentTypes;
+
+const MIME_TYPES = {
+	'application/vnd.nextthought.scormcontentref': true
+};
+
+const handles = (obj) => {
+	const {location} = obj || {};
+	const {item} = location || {};
+
+	return item && MIME_TYPES[item.MimeType];
+};
+
+export default
+@Registry.register(handles)
+class CourseItemSCORM extends React.Component {
+	render () {
+		return (
+			<Page {...this.props}>
+				<SCORM {...this.props} />
+			</Page>
+		);
+	}
+}
