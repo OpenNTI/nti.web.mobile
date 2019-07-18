bd62530ba8f99aea947bb608938ae66c7afb618a
Andrew Ligon <andrew.ligon@nextthought.com>
2019-07-18 13:42:16 -0500

    NTI-8160 match the future assignment message from the webapp

--- src/main/js/assessment/components/TimeLockedPlaceholder.jsx
+++ src/main/js/assessment/components/TimeLockedPlaceholder.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {DateTime} from '@nti/web-commons';
+import {scoped} from '@nti/lib-locale';
 
 import Placeholder from './Placeholder';
+
+const t = scoped('mobile.assessment.components.TimeLockedPlaceholder', {
+	header: 'Currently Unavailable',
+	date: 'Your assignment will be available on %(date)s.',
+	noDate: 'This assignment has not opened yet. Come back later.'
+});
 
 export default class extends React.Component {
 	static displayName = 'TimeLockedPlaceholder';
@@ -22,11 +30,13 @@ export default class extends React.Component {
 
 	render () {
 		const {props:{assignment}} = this;
+		const available = assignment.getAvailableForSubmissionBeginning();
+		const date = available && DateTime.format(available, 'dddd, MMMM D [at] h:mmA z');
 		const props = {
 			assignment,
-			message: 'This assignment has not opened yet. Come back later.',
+			message: date ? t('date', {date}) : t('noDate'),
 			buttonLabel: 'Back',
-			pageTitle: 'Not Available Yet',
+			pageTitle: t('header'),
 			onConfirm: this.onBack
 		};
 
