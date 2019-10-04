e4fdb52ed44dedb7a9f0e08351e033e54de5b458
Andrew Ligon <andrew.ligon@nextthought.com>
2019-10-04 17:38:44 -0500

    set up route for toc search hits

--- src/main/js/content/components/TableOfContentsView.jsx
+++ src/main/js/content/components/TableOfContentsView.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import createReactClass from 'create-react-class';
 import {TableOfContents} from '@nti/web-content';
 import {Mixins} from '@nti/web-commons';
+import {Router, Route} from '@nti/web-routing';
+import {encodeForURI} from '@nti/lib-ntiids';
 
 import ContextSender from 'common/mixins/ContextSender';
 
@@ -12,7 +14,7 @@ import ContextSender from 'common/mixins/ContextSender';
 // 	chapter: 'h3'
 // };
 
-export default createReactClass({
+const TableOfContentsWrapper = createReactClass({
 	displayName: 'TableOfContentsView',
 	mixins: [
 		Mixins.BasePath,
@@ -25,12 +27,26 @@ export default createReactClass({
 		item: PropTypes.object
 	},
 
+	getRouteFor (obj) {
+		if (obj.isContentUnitSearchHit) {
+			return this.makeHref(encodeForURI(obj.Containers[0]));
+		}
+	},
 
 	render () {
 		const {contentPackage} = this.props;
 
 		return (
-			<TableOfContents.View contentPackage={contentPackage} banner showLastPage />
+			<Router.RouteForProvider getRouteFor={this.getRouteFor}>
+				<TableOfContents.View contentPackage={contentPackage} banner showLastPage />
+			</Router.RouteForProvider>
 		);
 	}
 });
+
+export default Router.for([
+	Route({
+		path: '/',
+		component: TableOfContentsWrapper
+	})
+]);
