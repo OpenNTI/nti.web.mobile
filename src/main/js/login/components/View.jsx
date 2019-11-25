cd5a14ca06411e48d171fc21864a4134095918a2
Andrew Ligon <andrew.ligon@nextthought.com>
2019-11-25 17:42:43 -0600

    NTI-8764 keep the return param when redirecting to login

--- src/main/js/login/components/View.jsx
+++ src/main/js/login/components/View.jsx
@@ -3,8 +3,18 @@ import React from 'react';
 import Redirect from 'navigation/components/Redirect';
 
 export default function LoginView () {
+	const href = global.location?.href;
+	const url = new URL(href);
+	const returnParam = url.searchParams.get('return');
+
+	let redirect = '/login/';
+
+	if (returnParam) {
+		redirect = `${redirect}?return=${encodeURIComponent(returnParam)}`;
+	}
+
 	return (
-		<Redirect location="/login/" force />
+		<Redirect location={redirect} force />
 	);
 }
 
