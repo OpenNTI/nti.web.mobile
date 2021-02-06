91545a02597c915354427cc238eebb1161293a0f
Andrew Ligon <andrew.ligon@nextthought.com>
2021-02-05 22:52:47 -0600

    NTI-10216 add a mask if the assignment has been submitted but doesn't have any parts

---
+++ src/main/js/assessment/components/SubmissionMaskedPlaceholder.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {scoped} from '@nti/lib-locale';
+
+import Placeholder from './Placeholder';
+
+const t = scoped('mobile.assessment.components.SubmissionMaskedPlaceholder', {
+	header: 'You\'re all set!',
+	message: 'Your answers have been submitted.'
+});
+
+TimeLockedPlaceholder.propTypes = {
+	assignment: PropTypes.object
+};
+
+export default function TimeLockedPlaceholder ({assignment}) {
+
+	const onConfirm = React.useCallback((e) => {
+		if (e) {
+			e.preventDefault();
+			e.stopPropagation();
+		}
+
+		//Temp:
+		global.history.go(-1);
+	}, []);
+
+	return (
+		<Placeholder
+			assignment={assignment}
+			buttonLabel={'Back'}
+			message={t('message')}
+			onConfirm={onConfirm}
+			pageTitle={t('header')}
+		/>
+	);
+}
