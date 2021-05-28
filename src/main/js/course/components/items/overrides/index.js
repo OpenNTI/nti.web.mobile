3bb4e4505cf67a8e90f76129b84d9dd381701bae
Jonathan Grimes <jsg2021@gmail.com>
2021-05-28 15:25:03 -0500

    auto-wrap event

--- src/main/js/course/components/items/overrides/index.js
+++ src/main/js/course/components/items/overrides/index.js
@@ -1,12 +1,28 @@
 import Registry from './Registry';
+import Page from './Page';
 
 import './Assignment';
-import './CalendarEvent';
 import './Reading';
-import './Scorm';
 import './Timeline';
 import './Topic';
 import './Video';
+
+//TODO: These two look like they can be deleted now that the below auto-wrapping handler is in place
+import './Scorm';
 import './Webinar';
 
 export default Registry.getInstance();
+
+Registry.register((item, registration) => {
+	if (registration && !registration.default) {
+		if (!Registry.getItem(item)) {
+			const Handler = registration.handler;
+			// console.log('New Wrapped component', Handler);
+			Registry.register(registration.key, props => (
+				<Page {...props}>
+					<Handler {...props} />
+				</Page>
+			));
+		}
+	}
+}, null);
