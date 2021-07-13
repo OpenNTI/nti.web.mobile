183c207d3bf61be7c259fa06436901f75439a22a
Jonathan Grimes <jsg2021@gmail.com>
2021-07-13 12:47:42 -0500

    NTI-10831 - fix wrapped Page impl to actually work

--- src/main/js/course/components/items/overrides/Registry.js
+++ src/main/js/course/components/items/overrides/Registry.js
@@ -1,3 +1,27 @@
 import { Registry } from '@nti/lib-commons';
 
-export default class CourseItemsOverrides extends Registry.Handler {}
+import Page from './Page';
+
+const wrap = X =>
+	X &&
+	(props => (
+		<Page {...props}>
+			<X {...props} />
+		</Page>
+	));
+
+class CourseItemsOverrides extends Registry.Handler {
+	static register(matcher, item) {
+		// If the parent class returns truthy, it has not registered anything and is depending on
+		// the decorator returned function do perform the registration. (If it gets more than 1
+		// arg it will register whatever value is in argument position 1, using arg 0 as the key.)
+		if (super.register.apply(this, [matcher, wrap(item)].filter(Boolean))) {
+			// Return a "decorator" function to perform the registering.
+			return Component => {
+				this.registerItem(matcher, wrap(Component));
+			};
+		}
+	}
+}
+
+export default CourseItemsOverrides;
--- src/main/js/course/components/items/overrides/index.js
+++ src/main/js/course/components/items/overrides/index.js
@@ -1,5 +1,4 @@
 import Registry from './Registry';
-import Page from './Page';
 
 import './Assignment';
 import './CalendarEvent';
@@ -13,17 +12,3 @@ import './Scorm';
 import './Webinar';
 
 export default Registry.getInstance();
-
-Registry.register((item, registration) => {
-	if (registration && !registration.default) {
-		if (!Registry.getItem(item)) {
-			const Handler = registration.handler;
-			// console.log('New Wrapped component', Handler);
-			Registry.register(registration.key, props => (
-				<Page {...props}>
-					<Handler {...props} />
-				</Page>
-			));
-		}
-	}
-}, null);
